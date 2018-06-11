3952a5cc2b8f9d8599b98c89af56ab5bf53f9269
ravindra35 <36989030+ravindra35@users.noreply.github.com>
2018-06-11 15:23:32 +0530

    #216 issue resolved

---
+++ sfcs_app/app/fsp/fsp_menus/PSD_wise_Summary.php
@@ -0,0 +1,15 @@
+<html>
+<script language="javascript">
+
+function popup(Site)
+{
+window.open(Site,'PopupName','toolbar=no,statusbar=yes,menubar=yes,location=no,scrollbars=yes,resizable=yes,width=775,height=700');
+}
+</script>
+<?php 
+include($_SERVER['DOCUMENT_ROOT']."/sfcs_app/common/config/config.php");
+?>
+<body onload="popup('<?php echo $dnr_adr_sp_chain; ?>/prj_rmw_0001/PRJ_RMW_0001_S_0010_REP_FORMAT_1.cshtml');">
+
+</body>
+</html>
