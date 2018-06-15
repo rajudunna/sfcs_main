4ecbdceef1f692b2afc0a4d26291de2076e06dbf
ravindra35 <36989030+ravindra35@users.noreply.github.com>
2018-06-15 17:11:35 +0530

    #257 issue resolved

--- sfcs_app/app/warehouse/controllers/ajax_alloc_qty_label.php
+++ sfcs_app/app/warehouse/controllers/ajax_alloc_qty_label.php
@@ -1,27 +1,37 @@
 <?PHP
 // include("dbconf.php");
+include($_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/config/config_ajax.php');
+// $bai_rm_pj1="bai_rm_pj1";
+// $host="192.168.0.110:3326";
 
-$bai_rm_pj1="bai_rm_pj1";
-$host="192.168.0.110:3326";
-
-$user="baiall";
-$password="baiall";
+// $user="baiall";
+// $password="baiall";
 
 
-$link= ($GLOBALS["___mysqli_ston"] = mysqli_connect($host, $user, $password)) or die("Could not connect1: ".mysqli_error($GLOBALS["___mysqli_ston"]));
-mysqli_select_db($link, $bai_rm_pj1) or die("Error in selecting the database:".mysqli_error($GLOBALS["___mysqli_ston"]));
+// $link= ($GLOBALS["___mysqli_ston"] = mysqli_connect($host, $user, $password)) or die("Could not connect1: ".mysqli_error($GLOBALS["___mysqli_ston"]));
+// mysqli_select_db($link, $bai_rm_pj1) or die("Error in selecting the database:".mysqli_error($GLOBALS["___mysqli_ston"]));
 
 // echo 'hai';
 $label_id=$_POST["label"];
-$sql="SELECT qty_allocated FROM $bai_rm_pj1.store_in WHERE tid='".$label_id."'";
+$sql="select * FROM $bai_rm_pj1.store_in WHERE tid='".$label_id."'";
 $result1=mysqli_query($link, $sql);
 $sql_rows=mysqli_num_rows($result1);
 if($sql_rows>0)
-while($row1=mysqli_fetch_array($result1))
 {
-	echo $qty_allocated=$row1["qty_allocated"];
-	
-}
+		while($row12=mysqli_fetch_array($result1))
+		{
+			if($row12['allotment_status']==0)
+			{				
+				$balance=$row12['qty_rec']+$row12['qty_ret']-($row12['qty_issued']+$row12['partial_appr_qty']);
+			}
+			else
+			{
+				$balance=0;
+			}
+			
+		}
+		echo $balance;
+}		
 else
 {
 	echo 'false';
--- sfcs_app/common/config/config_ajax.php
+++ sfcs_app/common/config/config_ajax.php
@@ -4,9 +4,26 @@
 $host="192.168.0.110:3326";
 $user="baiall";
 $pass="baiall";
+
+$central_administration_sfcs='central_administration_sfcs';
+$tbl_view_view_menu="tbl_view_view_menu";
+$view_menu_role="view_menu_role";
+
 $bai_pro3="bai_pro3";
+$bai_pro="bai_pro";
+$bai_pro4="bai_pro4";
+$bai_pro2="bai_pro2";
+$bai_rm_pj1="bai_rm_pj1";
+$bai_rm_pj2="bai_rm_pj2";
+$brandix_bts="brandix_bts";
+$bai_pack="bai_pack";
+$bai_kpi="bai_kpi";
+$brandix_bts_uat="brandix_bts_uat";
+$m3_inputs="m3_inputs";
+$m3_bulk_ops_rep_db="m3_bulk_ops_rep_db";
+$temp_pool_db="temp_pool_db";
 
 $link= ($GLOBALS["___mysqli_ston"] = mysqli_connect($host, $user, $pass)) or die("Could not connect21: ".mysqli_error($GLOBALS["___mysqli_ston"]));
-mysqli_select_db($link, $bai_pro3) or die("Error in selecting the database:".mysqli_error($GLOBALS["___mysqli_ston"]));
+
 
 ?>
