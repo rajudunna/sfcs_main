4405c7511f675ef0bd60a88438b687b512d4422a
Dev <kalyan@schemaxtech.com>
2018-06-20 13:22:37 +0530

    Commit SSPI User Access enabled

--- template/helper.php
+++ template/helper.php
@@ -147,7 +147,7 @@ function hasmenupermission()
 
 function hasviewpermission($r)
 {
-    $user = getrbac_user();
+    //$user = getrbac_user();
     GLOBAL $link_ui;
     $r = base64_decode($r);
     $r = "/".trim($r, "/");
@@ -181,7 +181,7 @@ function hasviewpermission($r)
     updated at : 17-06-2018.
 */
 function haspermission($r){
-    $user = getrbac_user();
+    //$user = getrbac_user();
     GLOBAL $link_ui;
     $r = base64_decode($r);
     $r = "/".trim($r, "/");
@@ -218,7 +218,7 @@ function getrbac_user(){
     //$username_list=explode('\\',$_SERVER['REMOTE_USER']);
     //$user=$username_list[1];
 
-    $user = 'kiran';
+   // $user = 'kiran';
     return $user;
 }
 
