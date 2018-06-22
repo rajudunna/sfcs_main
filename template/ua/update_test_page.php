850b89bc41dc0eefbdd375a3842e768d061d4386
anil <anilt@schemaxtech.com>
2018-06-22 14:56:00 +0530

    modal added for update permmisions

--- template/ua/update_test_page.php
+++ template/ua/update_test_page.php
@@ -2,24 +2,27 @@
 
 <?php 
 
+   
     include('../dbconf.php');
-
+   
     $role_menu_id = $_POST['role_menu_id'];
-    $new_permissions = $_POST['permission_ids'];
+    $new_permissions = json_decode($_POST['permission_ids']);
     $old_records = $_POST['ids'];
-
+    $old_records = json_decode($old_records);
+    
     // Delete Old Permission Records
 
     foreach ($old_records as $key => $rid) {
         $sql_insert_query = "DELETE FROM rbac_role_menu_per WHERE role_menu_per_id = '$rid'";
-        $query_result = mysqli_query($link, $sql_insert_query) or exit("Sql Error1=".mysqli_error($GLOBALS["___mysqli_ston"]));
+        $query_result = mysqli_query($link_ui, $sql_insert_query) or exit("Sql Error1=".mysqli_error($GLOBALS["___mysqli_ston"]));
     }
 
     // Insert New Permissions
 
     foreach ($new_permissions as $key => $pid) {
+        $pid = $pid->permission_id;
         $sql_insert_query = "insert into rbac_role_menu_per (role_menu_id,permission_id) values ('$role_menu_id','$pid')";
-        $query_result = mysqli_query($link, $sql_insert_query) or exit("Sql Error2=".mysqli_error($GLOBALS["___mysqli_ston"]));
+        $query_result = mysqli_query($link_ui, $sql_insert_query) or exit("Sql Error2=".mysqli_error($GLOBALS["___mysqli_ston"]));
     }
 
     $_SESSION["msg"]='Permissions updated successfully';
