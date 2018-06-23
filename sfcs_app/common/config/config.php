cb932df735a5fb9142578580e6cc54c16e39f501
ChandrasekharaPhaniraju Tammana <iamchandu.1992@gmail.com>
2018-06-23 18:38:09 +0530

    user access bug fixes

--- sfcs_app/common/config/config.php
+++ sfcs_app/common/config/config.php
@@ -1,8 +1,13 @@
 <?php 
 //Without LDAP , With LDAP Unblock LDAP Code
 //$username="sfcsproject1";
-$mysql_details = get_config_values(getmysqldb);
-$mssql_odbc_details = get_config_values(mssql-odbc);
+// echo $_SERVER['DOCUMENT_ROOT'];
+error_reporting(0);
+require_once($_SERVER['DOCUMENT_ROOT']."/configuration/API/confr.php");
+$conf1 = new confr($_SERVER['DOCUMENT_ROOT']."/configuration/config-builder/saved_fields/fields.json");
+
+$mysql_details = $conf1->getDBConfig();
+$mssql_odbc_details = $conf1->get('mssql-odbc');
 $username_list=explode('\\',$_SERVER['REMOTE_USER']);
 $user=$username=strtolower($username_list[1]);
 $remove_user_name = true; // set false for static username removing 
@@ -13,15 +18,15 @@ $user=$mysql_details['db_user'];
 $pass=$mysql_details['db_pass'];
 
 //MSSql Configurations
-$ms_sql_odbc_host = get_config_values(mssql-odbc);
-$ms_sql_odbc_user = get_config_values(mssql-user-name);
-$ms_sql_odbc_pass = get_config_values(mssql-password);
+$ms_sql_odbc_host = $conf1->get('mssql-odbc');
+$ms_sql_odbc_user = $conf1->get('mssql-user-name');
+$ms_sql_odbc_pass = $conf1->get('mssql-password');
 
 //MY SQL host
-$ms_sql_odbc_host = get_config_values(mysql-odbc);
+$ms_sql_odbc_host = $conf1->get('mysql-odbc');
 
 //To Facilitate SFCS Filters
-$global_facility_code=get_config_values(plantcode);
+$global_facility_code=$conf1->get('plantcode');
 
 //User access code
 $server_soft=$_SERVER['SERVER_SOFTWARE'];
@@ -56,20 +61,20 @@ echo "<script language=\"javascript\">
 </script>";
 }
 $dnr_adr_sp_chain = "http://192.168.0.110:8002"; 
-$fab_uom=get_config_values(uom);
+$fab_uom=$conf1->get('uom');
 $sizes_array=array('s01','s02','s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24','s25','s26','s27','s28','s29','s30','s31','s32','s33','s34','s35','s36','s37','s38','s39','s40','s41','s42','s43','s44','s45','s46','s47','s48','s49','s50');
 
 $sizes_code=array('01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50');
 
 $sizes_title=array('S01','S02','S03','S04','S05','S06','S07','S08','S09','S10','S11','S12','S13','S14','S15','S16','S17','S18','S19','S20','S21','S22','S23','S24','S25','S26','S27','S28','S29','S30','S31','S32','S33','S34','S35','S36','S37','S38','S39','S40','S41','S42','S43','S44','S45','S46','S47','S48','S49','S50');
 
-$shifts_array = get_config_values(shifts);
+$shifts_array = $conf1->get('shifts');
 
 $mod_names = array("1","2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40");
 
-$plant_name = get_config_values(plantname);
+$plant_name = $conf1->get('plantname');
 
-$in_categories = get_config_values('category-display-dashboard');
+$in_categories = implode(',',$conf1->get('category-display-dashboard'));
 
 //Central Administraion Group ID's
 $group_id_sfcs=8;
