20bcf05013121b4e878db945b8d9b95b5957ca44
sudheer <sudheer@schemaxtech.com>
2018-06-25 16:14:47 +0530

    config items

--- configuration/API/testing.php
+++ configuration/API/testing.php
@@ -4,6 +4,6 @@ include "confr.php";
 
 $conf = new confr("../config-builder/saved_fields/fields.json");
 
-var_dump(implode(',',$conf->get("category-display-dashboard")));
+var_dump($conf->get("m3database"));
 //var_dump( $conf->getDBConfig() );
 //var_dump( $conf->getConnection() );
