818dc8b320ac80960f1b8f8f4505d126f1ff380d
sudheer <sudheer@schemaxtech.com>
2018-06-26 12:33:13 +0530

    mailcommits

--- configuration/API/testing.php
+++ configuration/API/testing.php
@@ -4,6 +4,6 @@ include "confr.php";
 
 $conf = new confr("../config-builder/saved_fields/fields.json");
 
-var_dump($conf->get("layplanmail"));
+var_dump($conf->get("packing_team_head"));
 //var_dump( $conf->getDBConfig() );
 //var_dump( $conf->getConnection() );
