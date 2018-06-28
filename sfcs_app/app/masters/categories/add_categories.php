40e32b1f3e2e69bdcccdc26ab7fffa73479897af
Ramani <ramani@schemaxtech.com>
2018-06-28 15:31:43 -0700

    commit categories form changes

--- sfcs_app/app/masters/categories/add_categories.php
+++ sfcs_app/app/masters/categories/add_categories.php
@@ -5,12 +5,12 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title></title>
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    
+    <h1>Add Category</h1></br>
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
