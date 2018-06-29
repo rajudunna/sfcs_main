53185a122969180435f45295fd9a9d398cc9ba61
anil <anilt@schemaxtech.com>
2018-06-29 19:37:10 +0530

    log changes

--- logs/inc/classes/Sentinel.php
+++ logs/inc/classes/Sentinel.php
@@ -31,7 +31,7 @@ class Sentinel
 					if ( ! csrf_verify() ) {
 						$attempt = $_POST['attempt'];
 						$error   = 2;
-						include_once PML_BASE . '/inc/login.inc.php';
+						// include_once PML_BASE . '/inc/login.inc.php';
 						self::release();
 						die();
 					}
@@ -46,7 +46,7 @@ class Sentinel
 					else { // error while signing in
 						$attempt = $_POST['attempt'];
 						$error   = 1;
-						include_once PML_BASE . '/inc/login.inc.php';
+						// include_once PML_BASE . '/inc/login.inc.php';
 						self::release();
 						die();
 					}
@@ -56,7 +56,7 @@ class Sentinel
 
 					$attempt = ( isset( $_GET['attempt'] ) ) ? $_GET['attempt'] : $_SERVER['REQUEST_URI'] . '?' . $_SERVER['QUERY_STRING'];
 					$error   = 0;
-					include_once PML_BASE . '/inc/login.inc.php';
+					// include_once PML_BASE . '/inc/login.inc.php';
 					self::release();
 					die();
 				}
@@ -68,7 +68,7 @@ class Sentinel
 				else { // send form
 					$attempt = $_SERVER['REQUEST_URI'] . '?' . $_SERVER['QUERY_STRING'];
 					$error   = 0;
-					include_once PML_BASE . '/inc/login.inc.php';
+					// include_once PML_BASE . '/inc/login.inc.php';
 					self::release();
 					die();
 				}
@@ -87,7 +87,7 @@ class Sentinel
 					else { // No anonymous access, redirect to login page
 						$error   = 3;
 						$attempt = '?';
-						include_once PML_BASE . '/inc/login.inc.php';
+						// include_once PML_BASE . '/inc/login.inc.php';
 					}
 
 					die();
