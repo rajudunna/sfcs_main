6eb5eec5dae2456c386b94ccc7d8952a991d03ec
unknown <theja@schemaxtech.com>
2018-07-02 11:10:06 +0530

    Git issue #596 : displayed input job number

--- sfcs_app/app/production/controllers/rework_update.php
+++ sfcs_app/app/production/controllers/rework_update.php
@@ -469,7 +469,7 @@ if ($_POST['submit11'])
 
 		echo '<div class="table-responsive"><table class="table table-bordered"		style="color:black; border: 1px solid red;">';
 		echo "<tr class=\"new\"><th>Mod#</th>";
-		echo "<th>Style</th><th>Schedule</th><th>Color</th><th>Cut#</th><th>Size</th><th>Input</th><th>Output</th><th>Balance</th>";
+		echo "<th>Style</th><th>Schedule</th><th>Color</th><th>Cut#</th><th>Input Job#</th><th>Size</th><th>Input</th><th>Output</th><th>Balance</th>";
 		// echo "<th>QTY</th><th>SMV</th><th>SMO</th><th>Status</th>";
 		echo "<th>Rework Qty</th><th>Remarks</th></tr>";
 		
@@ -583,7 +583,7 @@ if ($_POST['submit11'])
 						
 						if($rowcount_check==1)
 						{
-							echo "<td>".$sql_row12['ims_style']."</td><td>".$sql_row12['ims_schedule']."</td><td>".$sql_row12['ims_color']."</td><td>".chr($color_code).leading_zeros($cutno,3)."</td><td>".strtoupper($size_value)."</td><td>".$sql_row12['ims_qty']."</td><td>".$sql_row12['ims_pro_qty']."</td><td>".($sql_row12['ims_qty']-$sql_row12['ims_pro_qty'])."</td>";
+							echo "<td>".$sql_row12['ims_style']."</td><td>".$sql_row12['ims_schedule']."</td><td>".$sql_row12['ims_color']."</td><td>".chr($color_code).leading_zeros($cutno,3)."</td><td>J".leading_zeros($sql_row12['input_job_no_ref'],3)."</td><td>".strtoupper($size_value)."</td><td>".$sql_row12['ims_qty']."</td><td>".$sql_row12['ims_pro_qty']."</td><td>".($sql_row12['ims_qty']-$sql_row12['ims_pro_qty'])."</td>";
 							$balance=$sql_row12['ims_qty']-$sql_row12['ims_pro_qty'];
 							// echo '<td><input type="text" name="qty[]" autocomplete="off" size="8" onchange="if(check(this.value, '.($sql_row12['ims_qty']-$sql_row12['ims_pro_qty']).')==1010){ this.value=0;}" value="0" tabindex="'.$j.'" style="background-color:green; color=white;"></td>';
 							
@@ -593,7 +593,7 @@ if ($_POST['submit11'])
 						{
 							echo "<tr>";
 							
-							echo "<td>".$sql_row12['ims_style']."</td><td>".$sql_row12['ims_schedule']."</td><td>".$sql_row12['ims_color']."</td><td>".chr($color_code).leading_zeros($cutno,3)."</td><td>".strtoupper($size_value)."</td><td>".$sql_row12['ims_qty']."</td><td>".$sql_row12['ims_pro_qty']."</td><td>".($sql_row12['ims_qty']-$sql_row12['ims_pro_qty'])."</td>";
+							echo "<td>".$sql_row12['ims_style']."</td><td>".$sql_row12['ims_schedule']."</td><td>".$sql_row12['ims_color']."</td><td>".chr($color_code).leading_zeros($cutno,3)."</td><td>J".leading_zeros($sql_row12['input_job_no_ref'],3)."</td><td>".strtoupper($size_value)."</td><td>".$sql_row12['ims_qty']."</td><td>".$sql_row12['ims_pro_qty']."</td><td>".($sql_row12['ims_qty']-$sql_row12['ims_pro_qty'])."</td>";
 							$balance=$sql_row12['ims_qty']-$sql_row12['ims_pro_qty'];
 							// echo '<td><input type="text" name="qty[]" autocomplete="off" size="8" onchange="if(check(this.value, '.($sql_row12['ims_qty']-$sql_row12['ims_pro_qty']).')==1010){ this.value=0;}" value="" tabindex="'.$j.'" style="background-color:green; color=white;"></td>';
 						}
