23e2b06a18b38ed91b5b274df9726aa67f2753ed
ram G <ramkumar@schemaxtech.com>
2018-07-03 15:07:48 +0530

    Ticket#625 git push

--- sfcs_app/app/dashboards/controllers/rms/fab_pps_dashboard_v2.php
+++ sfcs_app/app/dashboards/controllers/rms/fab_pps_dashboard_v2.php
@@ -1026,10 +1026,12 @@ while($sql_rowx=mysqli_fetch_array($sql_resultx))
 		$club_c_code=array();
 		$club_docs=array();
 		$colors_db=array();
+		
 		if($sql_row1['clubbing']>0)
 		{
 			$total_qty=0;
-			$sql11="select order_col_des,color_code,doc_no,(p_xs+p_s+p_m+p_l+p_xl+p_xxl+p_xxxl+p_s06+p_s08+p_s10+p_s12+p_s14+p_s16+p_s18+p_s20+p_s22+p_s24+p_s26+p_s28+p_s30)*p_plies as total from $bai_pro3.order_cat_doc_mk_mix where category in ('".implode("','",$in_categories)."') and order_del_no=$schedule and clubbing=".$sql_row1['clubbing']." and acutno=".$sql_row1['acutno'];
+			$sql11="select order_col_des,color_code,doc_no,(p_xs+p_s+p_m+p_l+p_xl+p_xxl+p_xxxl+p_s06+p_s08+p_s10+p_s12+p_s14+p_s16+p_s18+p_s20+p_s22+p_s24+p_s26+p_s28+p_s30)*p_plies as total from $bai_pro3.order_cat_doc_mk_mix where category in ('".$in_categories."') and order_del_no=$schedule and clubbing=".$sql_row1['clubbing']." and acutno=".$sql_row1['acutno'];
+			//echo "</br>RMS tool Tip:".$sql11."</br>";
 			$sql_result11=mysqli_query($link, $sql11) or exit("Sql Error 22".mysqli_error($GLOBALS["___mysqli_ston"]));
 			while($sql_row11=mysqli_fetch_array($sql_result11))
 			{
