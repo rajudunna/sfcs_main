cf9f5b9bb012302ed48d3ab1ec30e2499d40ecb6
ram G <ramkumar@schemaxtech.com>
2018-07-03 16:01:38 +0530

    ticket #625 git commit

--- sfcs_app/app/dashboards/controllers/rms/fab_pps_dashboard_v2.php
+++ sfcs_app/app/dashboards/controllers/rms/fab_pps_dashboard_v2.php
@@ -1,3 +1,4 @@
+
 <!--
 Core Module:In this interface we can get module wise fabric allocation details.
 
@@ -1030,7 +1031,7 @@ while($sql_rowx=mysqli_fetch_array($sql_resultx))
 		if($sql_row1['clubbing']>0)
 		{
 			$total_qty=0;
-			$sql11="select order_col_des,color_code,doc_no,(p_xs+p_s+p_m+p_l+p_xl+p_xxl+p_xxxl+p_s06+p_s08+p_s10+p_s12+p_s14+p_s16+p_s18+p_s20+p_s22+p_s24+p_s26+p_s28+p_s30)*p_plies as total from $bai_pro3.order_cat_doc_mk_mix where category in ('".$in_categories."') and order_del_no=$schedule and clubbing=".$sql_row1['clubbing']." and acutno=".$sql_row1['acutno'];
+			$sql11="select order_col_des,color_code,doc_no,(p_xs+p_s+p_m+p_l+p_xl+p_xxl+p_xxxl+p_s01+p_s02+p_s03+p_s04+p_s05+p_s06+p_s07+p_s08+p_s09+p_s10+p_s11+p_s12+p_s13+p_s14+p_s15+p_s16+p_s17+p_s18+p_s19+p_s20+p_s21+p_s22+p_s23+p_s24+p_s25+p_s26+p_s27+p_s28+p_s29+p_s30+p_s31+p_s32+p_s33+p_s34+p_s35+p_s36+p_s37+p_s38+p_s39+p_s40+p_s41+p_s42+p_s43+p_s44+p_s45+p_s46+p_s47+p_s48+p_s49+p_s50)*p_plies as total from $bai_pro3.order_cat_doc_mk_mix where UPPER(category) in (".$in_categories.") and order_del_no=$schedule and clubbing=".$sql_row1['clubbing']." and acutno=".$sql_row1['acutno'];
 			//echo "</br>RMS tool Tip:".$sql11."</br>";
 			$sql_result11=mysqli_query($link, $sql11) or exit("Sql Error 22".mysqli_error($GLOBALS["___mysqli_ston"]));
 			while($sql_row11=mysqli_fetch_array($sql_result11))
