530d99e2eeb34b5ceafd3e698b16cd55f5a123a7
Ramesh Kalla <rameshk@schemaxtech.com>
2018-07-08 11:09:31 +0530

    Order Summary Report

--- sfcs_app/app/planning/reports/export_excel.php
+++ sfcs_app/app/planning/reports/export_excel.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-type: application/octet-stream");
-header("Content-Disposition: attachment; filename=\""."Production Status Report".".csv\"");
+$filename=$_POST['csvname'];
+header("Content-Disposition: attachment; filename=\"".$filename.".csv\"");
 $data=stripcslashes($_REQUEST['csv_text']);
 echo $data; 
 ?>
