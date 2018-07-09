3dcdd53235b400715e105f151cdcd71343bc61c6
unknown <theja@schemaxtech.com>
2018-07-09 20:32:53 +0530

    added cut numbers in report

--- sfcs_app/app/production/controllers/sewing_job/input_job_mix_ch_report.php
+++ sfcs_app/app/production/controllers/sewing_job/input_job_mix_ch_report.php
@@ -45,7 +45,9 @@ echo '<br>
        
         echo "<a class='btn btn-warning' href='$url1?schedule=$schedule' onclick=\"return popitup2('$url1?schedule=$schedule')\" target='_blank'>Print Sewing Job Sheet - Job Wise</a>      ";
 
-        echo "<a class='btn btn-warning' href='$url2?schedule=$schedule' onclick=\"return popitup2('$url2?schedule=$schedule')\" target='_blank'>Print Sewing Job Sheet - Split Wise</a><br><br>";
+        echo "<a class='btn btn-warning' href='$url2?schedule=$schedule' onclick=\"return popitup2('$url2?schedule=$schedule')\" target='_blank'>Print Sewing Job Sheet - Split Wise</a>";
+
+        echo "<a class='btn btn-info' href='#'>Number of Cartons: $no_of_cartons Per Sewing Job</a><br><br>";
 
     echo '</div>';
 
@@ -55,7 +57,6 @@ echo '<br>
             echo '<input type="hidden" name="job_qty" value="'.$job_qty.'">';
 
             echo "<div class='row'>
-                    <b>Number of Cartons: <font color='red'>$no_of_cartons</font> Per Sewing Job</b>
                 <table class='table table-bordered'>";
                     echo "<tr>";
                     echo "<th>Schedule</th>";
@@ -70,7 +71,7 @@ echo '<br>
                     mysqli_query($link, $sql1x) or exit("Sql Error88 = $sql".mysqli_error($GLOBALS["___mysqli_ston"]));
 
 
-                    $sql="SELECT type_of_sewing,input_job_no_random,sch_mix,input_job_no,GROUP_CONCAT(DISTINCT tid ORDER BY tid) AS tid,GROUP_CONCAT(DISTINCT doc_no_ref ORDER BY doc_no) AS doc_no_ref,GROUP_CONCAT(DISTINCT m3_size_code order by m3_size_code) AS size_code,group_concat(distinct order_col_des order by order_col_des) as order_col_des,doc_no,group_concat(distinct order_del_no) as order_del_no,GROUP_CONCAT(DISTINCT CONCAT(acutno) ORDER BY doc_no SEPARATOR '<br>' ) AS acutno,SUM(carton_act_qty) AS carton_act_qty FROM (SELECT DISTINCT(SUBSTRING_INDEX(order_joins,'J',-1)) AS sch_mix,order_del_no,input_job_no,input_job_no_random,tid,doc_no,doc_no_ref,m3_size_code,order_col_des,acutno,SUM(carton_act_qty) AS carton_act_qty,type_of_sewing FROM $bai_pro3.packing_summary_input WHERE order_del_no in ($schedule) $exp_query GROUP BY order_col_des,order_del_no,input_job_no_random,acutno,m3_size_code order by field(order_del_no,$schedule),field(m3_size_code,'s01','s02','s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24','s25','s26','s27','s28','s29','s30','s31','s32','s33','s34','s35','s36','s37','s38','s39','s40','s41','s42','s43','s44','s45','s46','s47','s48','s49','s50')) AS t GROUP BY input_job_no_random ORDER BY input_job_no*1,field(m3_size_code,'s01','s02','s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24','s25','s26','s27','s28','s29','s30','s31','s32','s33','s34','s35','s36','s37','s38','s39','s40','s41','s42','s43','s44','s45','s46','s47','s48','s49','s50')";
+                    $sql="SELECT type_of_sewing,input_job_no_random,sch_mix,input_job_no,GROUP_CONCAT(DISTINCT tid ORDER BY tid) AS tid,GROUP_CONCAT(DISTINCT doc_no_ref ORDER BY doc_no) AS doc_no_ref,GROUP_CONCAT(DISTINCT m3_size_code order by m3_size_code) AS size_code,group_concat(distinct order_col_des order by order_col_des) as order_col_des,doc_no,group_concat(distinct order_del_no) as order_del_no,GROUP_CONCAT(DISTINCT CONCAT(order_col_des,'$',acutno) ORDER BY doc_no SEPARATOR ',') AS acutno,SUM(carton_act_qty) AS carton_act_qty FROM (SELECT DISTINCT(SUBSTRING_INDEX(order_joins,'J',-1)) AS sch_mix,order_del_no,input_job_no,input_job_no_random,tid,doc_no,doc_no_ref,m3_size_code,order_col_des,acutno,SUM(carton_act_qty) AS carton_act_qty,type_of_sewing FROM $bai_pro3.packing_summary_input WHERE order_del_no in ($schedule) $exp_query GROUP BY order_col_des,order_del_no,input_job_no_random,acutno,m3_size_code order by field(order_del_no,$schedule),field(m3_size_code,'s01','s02','s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24','s25','s26','s27','s28','s29','s30','s31','s32','s33','s34','s35','s36','s37','s38','s39','s40','s41','s42','s43','s44','s45','s46','s47','s48','s49','s50')) AS t GROUP BY input_job_no_random ORDER BY input_job_no*1,field(m3_size_code,'s01','s02','s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24','s25','s26','s27','s28','s29','s30','s31','s32','s33','s34','s35','s36','s37','s38','s39','s40','s41','s42','s43','s44','s45','s46','s47','s48','s49','s50')";
                     // echo $sql."<br>";
                     $temp=0;
                     $job_no=0;
@@ -99,14 +100,22 @@ echo '<br>
                         {
                             $order_tid=$sql_row4["order_tid"];
                         }
-
-                        $sql4="select color_code from $bai_pro3.bai_orders_db where order_del_no=\"".$schedule."\"";
-                        $sql_result4=mysqli_query($link, $sql4) or exit("Sql Error44 $sql4".mysqli_error($GLOBALS["___mysqli_ston"]));
-                        while($sql_row4=mysqli_fetch_array($sql_result4))
+                        $total_cuts=explode(",",$sql_row['acutno']);
+                        $cut_jobs_new='';
+                        for($ii=0;$ii<sizeof($total_cuts);$ii++)
                         {
-                            $color_code=$sql_row4["color_code"];
+                            $arr = explode("$", $total_cuts[$ii], 2);;
+                           // $col = $arr[0];
+                            $sql4="select color_code from $bai_pro3.bai_orders_db_confirm where order_del_no=\"".$schedule."\" and order_col_des='".$arr[0]."'";
+                            //echo $sql4."<br>";
+                            $sql_result4=mysqli_query($link, $sql4) or exit("Sql Error44 $sql4".mysqli_error($GLOBALS["___mysqli_ston"]));
+                            while($sql_row4=mysqli_fetch_array($sql_result4))
+                            {
+                                $color_code=$sql_row4["color_code"];
+                            }
+                             $cut_jobs_new .= chr($color_code).leading_zeros($arr[1], 3)."<br>";
+                             unset($arr);
                         }
-
                         $doc_tag=$sql_row["doc_no"];
 
                         $sql_des="select destination,group_concat(distinct size_code ORDER BY size_code) as size_code from $bai_pro3.packing_summary_input where doc_no=\"$doc_tag\" and input_job_no='".$sql_row['input_job_no']."'";
@@ -120,7 +129,7 @@ echo '<br>
                         echo "<tr bgcolor='$bg_color'>";
                             echo "<td>".$sql_row['order_del_no']."</td>";
                             echo "<td>".$sql_row['order_col_des']."</td>";
-                            echo "<td>".chr($color_code).leading_zeros($sql_row['acutno'], 3)."</td>";
+                            echo "<td>".$cut_jobs_new."</td>";
                             echo "<td>".strtoupper($size_codes)."</td>";
                             echo "<td>".$sql_row['carton_act_qty']."</td>";
                             echo "<td>";
