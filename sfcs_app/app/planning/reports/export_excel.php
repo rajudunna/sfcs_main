3a310fffbcbd78f95289a9ddf42e28f7f6e5ed0e
Ramani <ramani@schemaxtech.com>
2018-07-09 11:49:05 -0700

    #614 excel issue resloved

--- sfcs_app/app/planning/reports/export_excel.php
+++ sfcs_app/app/planning/reports/export_excel.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-type: application/octet-stream");
-$filename=$_POST['csvname'];
-header("Content-Disposition: attachment; filename=\"".$filename.".csv\"");
+//$filename=$_POST['csvname'];
+header("Content-Disposition: attachment; filename=\""."Production Status Report".".csv\"");
 $data=stripcslashes($_REQUEST['csv_text']);
 echo $data; 
 ?>
