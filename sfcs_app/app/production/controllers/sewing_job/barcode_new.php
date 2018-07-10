6664c766f987cb503070e8ad4540a22ca52eb32c
Ravi Shankar <ravishankar@schemaxtech.com>
2018-07-10 16:18:03 +0530

    barcode sticker print changes

--- sfcs_app/app/production/controllers/sewing_job/barcode_new.php
+++ sfcs_app/app/production/controllers/sewing_job/barcode_new.php
@@ -9,7 +9,7 @@
 
 	$mpdf = new \Mpdf\Mpdf([
 		'mode' => 'utf-8', 
-		'format' => [65, 105], 
+		'format' => [45, 100], 
 		'orientation' => 'L'
 	]);
 
@@ -76,7 +76,7 @@
 			<table><tr><td>Style:</td><td>'.$barcode_rslt['order_style_no'].'</td><td>Schedule:</td><td>'.$schedule.'</td></tr>
 					 <tr><td>input#:</td><td>'.$input_job.'</td><td>Size#</td><td>'.$barcode_rslt['size_code'].'</td><td>B#:</td><td>'.$barcode.'</td></tr>
 				 	 <tr><td>color# </td><td colspan=3>'.$barcode_rslt['order_col_des'].'</td></tr>
-					 </table></div><br><br>';
+					 </table></div><br><br><br><br><br><br>';
 			$operation_det="select operation_name from $brandix_bts.tbl_style_ops_master where style='$style' AND color='$color'";
 			$sql_result1=mysqli_query($link, $operation_det) or exit("Sql Error1".mysqli_error($GLOBALS["___mysqli_ston"]));
 			while($ops = mysqli_fetch_array($sql_result1))
@@ -86,9 +86,9 @@
 				$html.= '<div><table><tr><td colspace="4"><barcode code="'.$barcode.'" type="C39"/ height="0.80" size="1.1"
 				 text="1"></td><td></td></tr></table>';
 				$html.= '<table><tr><td>Style:</td><td>'.$barcode_rslt['order_style_no'].'</td><td>Schedule:</td><td>'.$schedule.'</td></tr>
-					 <tr><td>input#:</td><td>'.$input_job.'</td><td>Size#</td><td>'.$barcode_rslt['size_code'].'</td><td>B#:</td><td>'.$barcode.'</td></tr>
+					 <tr><td colspan=4>input#:'.$input_job.' Size# '.$barcode_rslt['size_code'].' B#:'.$barcode.' ops#:'.$operations.'</td></tr>
 				 	 <tr><td>color# </td><td colspan=3>'.$barcode_rslt['order_col_des'].'</td></tr>
-					 </table></div>';			 
+					 </table></div><br><br><br><br><br>';			 
 			}
 		}
 	$html.='
