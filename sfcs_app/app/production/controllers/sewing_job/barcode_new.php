3378a6dcacc95b5376d10da1acb66f182508d6ec
unknown <theja@schemaxtech.com>
2018-07-11 13:24:37 +0530

    bundle barcodes edit

--- sfcs_app/app/production/controllers/sewing_job/barcode_new.php
+++ sfcs_app/app/production/controllers/sewing_job/barcode_new.php
@@ -57,20 +57,21 @@
 			$cutno=$barcode_rslt['acutno'];
 			$color_code=echo_title("$bai_pro3.bai_orders_db_confirm","color_code","order_col_des='".$color."' and order_del_no",$schedule,$link);
 			$html.= '<div>
-			<table>
-					 <tr rowspan=2><td colspan=2> Stab Here</td><td colspan=2><svg height="25" width="25">
-  <circle cx="10" cy="10" r="8"  />
-</svg></td></tr>	
-					 
-				
-					 <tr><td>Style:</td><td>'.$barcode_rslt['order_style_no'].'</td><td>Schedule:</td><td>'.$schedule.'</td></tr>
-					 
-					 <tr><td>inputjob#:</td><td>'.$input_job.'</td><td>Size#</td><td>'.$barcode_rslt['size_code'].'</td></tr>
-				
-					 <tr><td>B#:</td><td>'.$barcode.'</td><td>Cut#:</td><td>'.chr($color_code).leading_zeros($cutno, 3).'</td></tr>
-					
-				 	 <tr><td>color# </td><td colspan=3>'.trim($barcode_rslt['order_col_des']).'</td></tr>
-					 </table></div><br>';
+						<table>
+							<tr rowspan=2>
+								<td colspan=2><b>Stab Here:</b></td>
+								<td colspan=2>
+									<svg height="25" width="25">
+										<circle cx="10" cy="10" r="8"  />
+									</svg>
+								</td>
+							</tr>	
+							<tr><td><b>Style:</b></td><td>'.$barcode_rslt['order_style_no'].'</td><td><b>Schedule:</b></td><td>'.$schedule.'</td></tr>
+							<tr><td><b>InputJob#:</b></td><td>'.$input_job.'</td><td><b>Size#:</b></td><td>'.$barcode_rslt['size_code'].'</td></tr>
+							<tr><td><b>B#:</b></td><td>'.$barcode.'</td><td><b>Cut#:</b></td><td>'.chr($color_code).leading_zeros($cutno, 3).'</td></tr>
+							<tr><td><b>Color#:</b></td><td colspan=3>'.trim($barcode_rslt['order_col_des']).'</td></tr>
+						 </table>
+					 </div><br>';
 			$operation_det="SELECT tor.operation_name as operation_name,tor.operation_code as operation_code FROM $brandix_bts.tbl_style_ops_master tsm LEFT JOIN $brandix_bts.tbl_orders_ops_ref tor ON tor.id=tsm.operation_name WHERE style='$style ' AND color='$color' and tor.operation_code not in (10,15,200)";
 			$sql_result1=mysqli_query($link, $operation_det) or exit("Sql Error1".mysqli_error($GLOBALS["___mysqli_ston"]));
 			while($ops = mysqli_fetch_array($sql_result1))
@@ -78,13 +79,23 @@
 				$operations=$ops['operation_name'];
 				$opscode=$ops['operation_code'];
 
-				$html.= '<div><table><tr><td colspace="4"><barcode code="'.$barcode.'-'.$opscode.'" type="C39"/ height="0.80" size="0.8"
-				 text="1"></td><td></td></tr></table>';
-				$html.= '<table><tr><td>Style:</td><td>'.$barcode_rslt['order_style_no'].'</td><td>Schedule:</td><td>'.$schedule.'</td></tr>
-					 <tr><td colspan=4>input#:'.$input_job.' Size# '.$barcode_rslt['size_code'].' B#:'.$barcode.'</td></tr>
-				 	 <tr><td>color# </td><td colspan=3>'.trim($barcode_rslt['order_col_des']).'</td></tr>
-				 	 <tr><td colspan=4>ops#:'.trim($operations).' C#: '.chr($color_code).leading_zeros($cutno, 3).'</td></tr>
-					 </table></div><br><br><br><br><br>';			 
+				$html.= '<div>
+							<table>
+								<tr>
+									<td colspace="4"><barcode code="'.$barcode.'-'.$opscode.'" type="C39"/ height="0.80" size="0.8" text="1"></td>
+									<td></td>
+								</tr>
+							</table>
+							<table>
+								<tr>
+									<td><b>Style:</b></td><td>'.$barcode_rslt['order_style_no'].'</td>
+									<td><b>Schedule:</b></td><td>'.$schedule.'</td>
+								</tr>
+								<tr><td colspan=4><b>Input#:</b>'.$input_job.' <b>Size#:</b> '.$barcode_rslt['size_code'].' <b>B#:</b>'.$barcode.'</td></tr>
+								<tr><td><b>Color#: </b></td><td colspan=3>'.trim($barcode_rslt['order_col_des']).'</td></tr>
+								<tr><td colspan=4><b>OPS#:</b>'.trim($operations).' <b>Cut#:</b> '.chr($color_code).leading_zeros($cutno, 3).'</td></tr>
+							</table>
+						</div><br><br><br><br><br>';			 
 			}
 		}
 	$html.='
