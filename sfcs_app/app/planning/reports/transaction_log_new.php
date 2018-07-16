3edf0df5ca3aec5aa81d78e7dacbfdcf2a001112
mounika pentakota <mounika@schemaxtech.com>
2018-07-16 12:59:11 +0530

    commit filter changes in production status report issue #672

--- sfcs_app/app/planning/reports/transaction_log_new.php
+++ sfcs_app/app/planning/reports/transaction_log_new.php
@@ -25,10 +25,10 @@ include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/
 	$module=$_POST['module'];
 ?>
 <!--<div id="page_heading"><span style="float"><h3>Daily Production Status Report</h3></span><span style="float: right; margin-top: -20px"><b>?</b>&nbsp;</span></div>-->
-<div class="panel panel-primary" style='width:1087px;'>
+<div class="panel panel-primary">
 <div class="panel-heading">Production Status Report</div>
 <div class="panel-body">
-<div class="form-group" style='width:1069px;';>
+<div class="form-group">
 <form name="text" method="post" action="index.php?r=<?php echo $_GET['r']; ?>">
 <div class="col-md-12">
 <div class="col-md-2">
@@ -38,7 +38,7 @@ Start : <input data-toggle="datepicker" class="form-control" type="text" id="dem
 End : <input data-toggle="datepicker"  class="form-control" type="text" id="demo2" name="edate" value="<?php  if($edate==""){ echo date("Y-m-d"); } else { echo $edate; } ?>" size="10">
 </div>
 <div class="col-md-1">
-Section: <select name="module" class="form-control">
+Section: <select name="module" id="myModule" class="form-control">
 <option value="0" <?php  if($module=="All")?>selected>All</option>
 <?php
 /*for($i=1;$i<=8; $i++)
@@ -75,14 +75,16 @@ for($i=0;$i<sizeof($sql_mods);$i++)
 }	
 ?>
 </select></div>
-
 <div class="col-md-1">
-Shift: <select name="shift" class="form-control">
+Shift: <select name="shift" id="myshift" class="form-control">
+<option value='All' <?php if($shift=="All"){ echo "selected"; } ?> >All</option>
 <?php 
 for ($i=0; $i < sizeof($shifts_array); $i++) {?>
-<option  <?php echo 'value="'.$shifts_array[$i].'"'; if($shift==$shifts_array[$i]){ echo "selected";}   ?>><?php echo $shifts_array[$i] ?></option>
+<!-- $sf = $sf."'".$shifts_array[$i]."'"; -->
+<option  <?php echo 'value="'.$shifts_array[$i].'"'; ?>><?php echo $shifts_array[$i] ?></option>
 <?php }
 ?>
+<!-- <option  value="<?= $sf ?>" selected><?php echo 'ALL'; ?></option> -->
 </select></div>
 
 <div class="col-md-2">
@@ -171,8 +173,15 @@ echo '<form action="'.getFullURL($_GET["r"],"export_excel.php",'R').'" method ="
 	$module=$_POST['module'];
 	$hour_from=$_POST["hour"];
 	$hour_to=$_POST["hour1"];
+	if($shift_new!='All')
+	{
+		$shift_value="and bac_shift in ('".$shift_new."')";
+	}
+	else {
+		$shift_value="";
+	}
 
-	
+
 	// ob_end_flush();
 	// flush();
 	// usleep(10);
@@ -184,7 +193,6 @@ shift=".str_replace('"','*',$shift)."&module=$module&hour_from=$hour_from&hour_t
 	<input type='submit' name='submit1' class='btn btn-info' value='Export to Excel'></a></right>";*/
 $sql="select * from $bai_pro3.sections_db where sec_id=$module";
 
-//echo $sql;
 $sql_result=mysqli_query($link, $sql) or exit("Sql Error2".mysqli_error($GLOBALS["___mysqli_ston"]));
 //echo $sql;
 $sql_result=mysqli_query($link, $sql) or exit("Sql Error211".mysqli_error($GLOBALS["___mysqli_ston"]));
@@ -197,22 +205,21 @@ while($sql_row=mysqli_fetch_array($sql_result))
 
 
 
-if($hour_from='Day' and $hour_to='Day'){
-	$sql="select tid,bac_no,delivery,bac_sec,bac_date,bac_shift, jobno,bac_Qty,bac_lastup,bac_style,ims_doc_no,ims_tid,ims_table_name,log_time,smv,nop,size_s01,size_s02,size_s03,size_s04,size_s05,size_s06,size_s07,size_s08,size_s09,size_s10,size_s11,size_s12,size_s13,size_s14,size_s15,size_s16,size_s17,size_s18,size_s19,size_s20,size_s21,size_s22,size_s23, size_s24,size_s25,size_s26,size_s27,size_s28,size_s29,size_s30,size_s31,size_s32,size_s33,size_s34,size_s35,size_s36,size_s37,size_s38,size_s39,size_s40,size_s41,size_s42,size_s43,size_s44,size_s45,size_s46,size_s47,size_s48,size_s49,size_s50 from $bai_pro.bai_log where bac_date between \"$sdate\" and \"$edate\" and bac_shift in ('$shift') and bac_no in ($module) and bac_qty>0 order by bac_date desc,hour(bac_lastup)";
+if($hour_from=='Day' and $hour_to=='Day'){
+	$sql="select tid,bac_no,delivery,bac_sec,bac_date,bac_shift, jobno,bac_Qty,bac_lastup,bac_style,ims_doc_no,ims_tid,ims_table_name,log_time,smv,nop,size_s01,size_s02,size_s03,size_s04,size_s05,size_s06,size_s07,size_s08,size_s09,size_s10,size_s11,size_s12,size_s13,size_s14,size_s15,size_s16,size_s17,size_s18,size_s19,size_s20,size_s21,size_s22,size_s23, size_s24,size_s25,size_s26,size_s27,size_s28,size_s29,size_s30,size_s31,size_s32,size_s33,size_s34,size_s35,size_s36,size_s37,size_s38,size_s39,size_s40,size_s41,size_s42,size_s43,size_s44,size_s45,size_s46,size_s47,size_s48,size_s49,size_s50 from $bai_pro.bai_log where bac_date between \"$sdate\" and \"$edate\" ".$shift_value." and bac_no in ($module) and bac_qty>0 order by bac_date desc,hour(bac_lastup)";
 	//$sql="select tid,bac_no,delivery,bac_sec,bac_date,bac_shift, jobno,sum(bac_Qty) as \"bac_Qty\",bac_lastup,bac_style,ims_doc_no,ims_tid,ims_table_name,log_time,smv,nop,sum(size_s01) as \"size_s01\", sum(size_s02) as \"size_s02\", sum(size_s03) as \"size_s03\",sum(size_s04) as \"size_s04\",sum(size_s05) as \"size_s05\",sum(size_s06) as \"size_s06\", sum(size_s07) as \"size_s07\",sum(size_s08) as \"size_s08\",sum(size_s09) as \"size_s09\",sum(size_s10) as \"size_s10\",sum(size_s11) as \"size_s11\", sum(size_s12) as \"size_s12\", sum(size_s13) as \"size_s13\",sum(size_s14) as \"size_s14\", sum(size_s15) as \"size_s15\", sum(size_s16) as \"size_s16\",sum(size_s17) as \"size_s17\", sum(size_s18) as \"size_s18\", sum(size_s19) as \"size_s19\",sum(size_s20) as \"size_s20\", sum(size_s21) as \"size_s21\", sum(size_s22) as \"size_s22\",sum(size_s23) as \"size_s23\", sum(size_s24) as \"size_s24\", sum(size_s25) as \"size_s25\",sum(size_s26) as \"size_s26\",sum(size_s27) as \"size_s27\", sum(size_s28) as \"size_s28\",sum(size_s29) as \"size_s29\", sum(size_s30) as \"size_s30\",sum(size_s31) as \"size_s31\",sum(size_s32) as \"size_s32\",sum(size_s33) as \"size_s33\",sum(size_s34) as \"size_s34\",sum(size_s35) as \"size_s35\",sum(size_s36) as \"size_s36\",sum(size_s37) as \"size_s37\",sum(size_s38) as \"size_s38\",sum(size_s39) as \"size_s39\",sum(size_s40) as \"size_s40\",sum(size_s41) as \"size_s41\",sum(size_s42) as \"size_s42\",sum(size_s43) as \"size_s43\",sum(size_s44) as \"size_s44\",sum(size_s45) as \"size_s45\",sum(size_s46) as \"size_s46\",sum(size_s47) as \"size_s47\",sum(size_s48) as \"size_s48\",sum(size_s49) as \"size_s49\",sum(size_s50) as \"size_s50\" from $bai_pro.bai_log where bac_date between \"$sdate\" and \"$edate\" and bac_shift in ('$shift') and bac_no in ($module) and bac_qty>0 group by bac_date,bac_style,delivery,color,ims_doc_no,bac_sec,bac_no,bac_shift,bac_lastup order by bac_date,bac_no*1,ims_doc_no,bac_shift,bac_lastup";
 }else
 {
 	//$sql="select tid,bac_no,delivery,bac_sec,bac_date,bac_shift, jobno,sum(bac_Qty) as \"bac_Qty\",bac_lastup,bac_style,ims_doc_no,ims_tid,ims_table_name,log_time,smv,nop,sum(size_s01) as \"size_s01\", sum(size_s02) as \"size_s02\", sum(size_s03) as \"size_s03\",sum(size_s04) as \"size_s04\",sum(size_s05) as \"size_s05\",sum(size_s06) as \"size_s06\", sum(size_s07) as \"size_s07\",sum(size_s08) as \"size_s08\",sum(size_s09) as \"size_s09\",sum(size_s10) as \"size_s10\",sum(size_s11) as \"size_s11\", sum(size_s12) as \"size_s12\", sum(size_s13) as \"size_s13\",sum(size_s14) as \"size_s14\", sum(size_s15) as \"size_s15\", sum(size_s16) as \"size_s16\",sum(size_s17) as \"size_s17\", sum(size_s18) as \"size_s18\", sum(size_s19) as \"size_s19\",sum(size_s20) as \"size_s20\", sum(size_s21) as \"size_s21\", sum(size_s22) as \"size_s22\",sum(size_s23) as \"size_s23\", sum(size_s24) as \"size_s24\", sum(size_s25) as \"size_s25\",sum(size_s26) as \"size_s26\",sum(size_s27) as \"size_s27\", sum(size_s28) as \"size_s28\",sum(size_s29) as \"size_s29\", sum(size_s30) as \"size_s30\",sum(size_s31) as \"size_s31\",sum(size_s32) as \"size_s32\",sum(size_s33) as \"size_s33\",sum(size_s34) as \"size_s34\",sum(size_s35) as \"size_s35\",sum(size_s36) as \"size_s36\",sum(size_s37) as \"size_s37\",sum(size_s38) as \"size_s38\",sum(size_s39) as \"size_s39\",sum(size_s40) as \"size_s40\",sum(size_s41) as \"size_s41\",sum(size_s42) as \"size_s42\",sum(size_s43) as \"size_s43\",sum(size_s44) as \"size_s44\",sum(size_s45) as \"size_s45\",sum(size_s46) as \"size_s46\",sum(size_s47) as \"size_s47\",sum(size_s48) as \"size_s48\",sum(size_s49) as \"size_s49\",sum(size_s50) as \"size_s50\" from $bai_pro.bai_log where bac_date between \"$sdate\" and \"$edate\" and bac_shift in ('$shift') and bac_no in ($module) and hour(bac_lastup) between \"$hour_from\" and \"$hour_to\" and bac_qty>0 group by bac_date,bac_style,delivery,color,ims_doc_no,bac_sec,bac_no,bac_shift,bac_lastup order by bac_date,bac_no*1,ims_doc_no,bac_shift,bac_lastup";
 
-	$sql="select tid,bac_no,delivery,bac_sec,bac_date,bac_shift, jobno,sum(bac_Qty) as \"bac_Qty\",bac_lastup,bac_style,ims_doc_no,ims_tid,ims_table_name,log_time,smv,nop,size_s01,size_s02,size_s03,size_s04,size_s05,size_s06,size_s07,size_s08,size_s09,size_s10,size_s11,size_s12,size_s13,size_s14,size_s15,size_s16,size_s17,size_s18,size_s19,size_s20,size_s21,size_s22,size_s23,size_s24,size_s25,size_s26,size_s27,size_s28,size_s29,size_s30,size_s31,size_s32,size_s33,size_s34,size_s35,size_s36,size_s37,size_s38,size_s39,size_s40,size_s41,size_s42,size_s43,size_s44,size_s45,size_s46,size_s47,size_s48,size_s49,size_s50 from $bai_pro.bai_log where bac_date between \"$sdate\" and \"$edate\" and bac_shift in ('$shift') and bac_no in ($module) and hour(bac_lastup) between \"$hour_from\" and \"$hour_to\" and bac_qty>0 order by bac_date,hour(bac_lastup) desc";
+	$sql="select tid,bac_no,delivery,bac_sec,bac_date,bac_shift, jobno,bac_Qty,bac_lastup,bac_style,ims_doc_no,ims_tid,ims_table_name,log_time,smv,nop,size_s01,size_s02,size_s03,size_s04,size_s05,size_s06,size_s07,size_s08,size_s09,size_s10,size_s11,size_s12,size_s13,size_s14,size_s15,size_s16,size_s17,size_s18,size_s19,size_s20,size_s21,size_s22,size_s23,size_s24,size_s25,size_s26,size_s27,size_s28,size_s29,size_s30,size_s31,size_s32,size_s33,size_s34,size_s35,size_s36,size_s37,size_s38,size_s39,size_s40,size_s41,size_s42,size_s43,size_s44,size_s45,size_s46,size_s47,size_s48,size_s49,size_s50 from $bai_pro.bai_log where bac_date between \"$sdate\" and \"$edate\" ".$shift_value." and bac_no in ($module) and hour(bac_lastup) between \"$hour_from\" and \"$hour_to\" and bac_qty>0 order by bac_date,hour(bac_lastup) desc";
 }
-//echo $sql;
 $sql_result=mysqli_query($link, $sql) or exit("Sql Error3".mysqli_error($GLOBALS["___mysqli_ston"]));
 if(mysqli_num_rows($sql_result)>0)
 {
 	echo "<div>";
 echo "<div  class ='table-responsive'>";
-echo "<table id=\"table1\" style='max-width: 89%;'  border=1 class=\"table\" cellpadding=\"0\" cellspacing=\"0\" ><thead>";
+echo "<table id=\"table1\"  border=1 class=\"table\" cellpadding=\"0\" cellspacing=\"0\" style='margin-top:10pt;'><thead>";
 echo "<tr class='tblheading' style='color:white;'><th>Date</th><th>Time<th>Module</th><th>Section</th><th>Shift</th><th>User Style</th><th>Movex Style</th><th>Schedule</th><th>Color</th><th>Cut No</th>";
 //echo "<th>SMV</th><th>NOP</th>";
 echo "<th>Input Job No</th><th>Size</th><th>Quantity</th></tr></tbody>";
@@ -384,7 +391,7 @@ while($sql_row=mysqli_fetch_array($sql_result))
 echo "</table></div></div>";
 }
 else{
-	echo "<div class='alert alert-danger' style='width:942px';>No Data Found</div>";
+	echo "<div class='alert alert-danger' style='width:1000px';>No Data Found</div>";
 	echo "<script>$(document).ready(function(){
 			 $('#table1').css('display','none');
 		 });</script>";
@@ -395,11 +402,14 @@ else{
 <script>
 
 	var table3Filters = {
-	// col_7: "select",
+	rows_counter: true,
+	rows_counter_text: "Total rows: ",
+	btn_reset: true,
 	sort_select: true,
 	display_all_text: "Display all"
 	}
 	setFilterGrid("table1",table3Filters);
+	$('#reset_table1').addClass('btn btn-warning btn-xs');
 	
 </script>
 <script language="javascript">
@@ -452,8 +462,19 @@ function getData(){
 			return true;
 		}
     }
-
+	console.log(document.getElementById('myshift').value);
+	$(document).ready(function(){
+    $('#myshift').val("<?php echo $_POST['shift'];?>");
+    $('#myModule').val("<?php echo $_POST['module'];?>");
+    $('#hour').val("<?php echo $_POST['hour'];?>");
+    $('#hour1').val("<?php echo $_POST['hour1'];?>");
+});
 </script>
+<style>
+.flt{
+	width:100%;
+}
+</style>
 </div>
 </div>
 </body>
