1392c041aedb01bcffe55b6518adf3a0ab250789
unknown <theja@schemaxtech.com>
2018-07-20 18:50:29 +0530

    git issue #688 solved

--- sfcs_app/app/cutting/controllers/lay_plan_preparation/main_interface_5.php
+++ sfcs_app/app/cutting/controllers/lay_plan_preparation/main_interface_5.php
@@ -41,7 +41,7 @@ echo "
 		<th class=\"column-title\"><center>Delete Control</center></th>
 		<th class=\"column-title\"><center>Ratio wise Savings%</center></th>
 		<th class=\"column-title\"><center>Ratio wise CAD Consumption</center></th>
-		<th class=\"column-title\"><center>Used Meters</center></th><th class=\"column-title\"><center>Current Status</center></th><th class=\"column-title\"><center>Remarks</center></th></tr></thead>";
+		<th class=\"column-title\"><center>Used $fab_uom</center></th><th class=\"column-title\"><center>Current Status</center></th><th class=\"column-title\"><center>Remarks</center></th></tr></thead>";
 
 while($sql_row=mysqli_fetch_array($sql_result))
 {
@@ -139,7 +139,8 @@ while($sql_row=mysqli_fetch_array($sql_result))
 			$cutcount1=ceil($sql_row2['plies']/$sql_row2['pliespercut']);
 		}
 				
-		$totalplies1=$sql_row2['allocate_s01']+$sql_row2['allocate_s02']+$sql_row2['allocate_s03']+$sql_row2['allocate_s04']+$sql_row2['allocate_s05']+$sql_row2['allocate_s06']+$sql_row2['allocate_s07']+$sql_row2['allocate_s08']+$sql_row2['allocate_s09']+$sql_row2['allocate_s10']+$sql_row2['allocate_s11']+$sql_row2['allocate_s12']+$sql_row2['allocate_s13']+$sql_row2['allocate_s14']+$sql_row2['allocate_s15']+$sql_row2['allocate_s16']+$sql_row2['allocate_s17']+$sql_row2['allocate_s18']+$sql_row2['allocate_s19']+$sql_row2['allocate_s20']+$sql_row2['allocate_s21']+$sql_row2['allocate_s22']+$sql_row2['allocate_s23']+$sql_row2['allocate_s24']+$sql_row2['allocate_s25']+$sql_row2['allocate_s26']+$sql_row2['allocate_s27']+$sql_row2['allocate_s28']+$sql_row2['allocate_s29']+$sql_row2['allocate_s30']+$sql_row2['allocate_s31']+$sql_row2['allocate_s32']+$sql_row2['allocate_s33']+$sql_row2['allocate_s34']+$sql_row2['allocate_s35']+$sql_row2['allocate_s36']+$sql_row2['allocate_s37']+$sql_row2['allocate_s38']+$sql_row2['allocate_s39']+$sql_row2['allocate_s40']+$sql_row2['allocate_s41']+$sql_row2['allocate_s42']+$sql_row2['allocate_s43']+$sql_row2['allocate_s44']+$sql_row2['allocate_s45']+$sql_row2['allocate_s46']+$sql_row2['allocate_s47']+$sql_row2['allocate_s48']+$sql_row2['allocate_s49']+$sql_row2['allocate_s50'];
+		// $totalplies1=$sql_row2['allocate_s01']+$sql_row2['allocate_s02']+$sql_row2['allocate_s03']+$sql_row2['allocate_s04']+$sql_row2['allocate_s05']+$sql_row2['allocate_s06']+$sql_row2['allocate_s07']+$sql_row2['allocate_s08']+$sql_row2['allocate_s09']+$sql_row2['allocate_s10']+$sql_row2['allocate_s11']+$sql_row2['allocate_s12']+$sql_row2['allocate_s13']+$sql_row2['allocate_s14']+$sql_row2['allocate_s15']+$sql_row2['allocate_s16']+$sql_row2['allocate_s17']+$sql_row2['allocate_s18']+$sql_row2['allocate_s19']+$sql_row2['allocate_s20']+$sql_row2['allocate_s21']+$sql_row2['allocate_s22']+$sql_row2['allocate_s23']+$sql_row2['allocate_s24']+$sql_row2['allocate_s25']+$sql_row2['allocate_s26']+$sql_row2['allocate_s27']+$sql_row2['allocate_s28']+$sql_row2['allocate_s29']+$sql_row2['allocate_s30']+$sql_row2['allocate_s31']+$sql_row2['allocate_s32']+$sql_row2['allocate_s33']+$sql_row2['allocate_s34']+$sql_row2['allocate_s35']+$sql_row2['allocate_s36']+$sql_row2['allocate_s37']+$sql_row2['allocate_s38']+$sql_row2['allocate_s39']+$sql_row2['allocate_s40']+$sql_row2['allocate_s41']+$sql_row2['allocate_s42']+$sql_row2['allocate_s43']+$sql_row2['allocate_s44']+$sql_row2['allocate_s45']+$sql_row2['allocate_s46']+$sql_row2['allocate_s47']+$sql_row2['allocate_s48']+$sql_row2['allocate_s49']+$sql_row2['allocate_s50'];
+		$totalplies1=$sql_row2['pliespercut'];
 
 		$ratiotot=0;
 		for($s=0;$s<sizeof($s_tit);$s++)
@@ -169,7 +170,7 @@ while($sql_row=mysqli_fetch_array($sql_result))
 
 	echo "<td class=\"  \"><center>".$savings1."%</center></td>";
 	echo "<td class=\"  \"><center>".round($cad_consumption,4)."</center></td>";
-	echo "<td class=\"  \"><center>".round($usedFabric,0)."</center></td>";
+	echo "<td class=\"  \"><center>".round($usedFabric,2)."</center></td>";
 		
 	switch ($mk_status1)
 	{
