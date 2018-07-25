dad94bde2b0728a278b1a21d5215e32f1ff81bc8
ashok@schemaxtech.com <ashok@schemaxtech.com>
2018-07-25 14:56:39 +0530

    Load dynamic css url created for gifs

--- template/header_links.php
+++ template/header_links.php
@@ -33,5 +33,109 @@
      
      <!-- Loading Event -->
 
-     <link rel="stylesheet" href="template/loading/loading.css">
+    <style type="text/css">
+
+      #overlay {
+          position: fixed;
+          display: none;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: rgba(255, 255, 255, 0.85);
+          z-index: 2;
+          cursor: pointer;
+      }
+
+      #load1 {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          font-size: 50px;
+          color: white;
+          transform: translate(-50%,-50%);
+          background-image: url(<?php echo "http://". $_SERVER['SERVER_NAME']?>/template/loading/gifs/1.gif);
+          background-size: cover;
+          background-position: center;
+          border: 6px solid white;
+      }
+
+      #load2 {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          font-size: 50px;
+          color: white;
+          transform: translate(-50%,-50%);
+          background-image: url(<?php echo "http://". $_SERVER['SERVER_NAME']?>/template/loading/gifs/2.gif);
+          background-size: cover;
+          background-position: center;
+          border: 6px solid white;
+      }
+
+      #load3 {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          font-size: 50px;
+          color: white;
+          transform: translate(-50%,-50%);
+          background-image: url(<?php echo "http://". $_SERVER['SERVER_NAME']?>/template/loading/gifs/3.gif);
+          background-size: cover;
+          background-position: center;
+          border: 6px solid white;
+      }
+
+      #load4 {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          font-size: 50px;
+          color: white;
+          transform: translate(-50%,-50%);
+          background-image: url(<?php echo "http://". $_SERVER['SERVER_NAME']?>/template/loading/gifs/4.gif);
+          background-size: 400%;
+          background-position: center;
+          border: 6px solid white;
+      }
+
+      #load5 {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          font-size: 50px;
+          color: white;
+          transform: translate(-50%,-50%);
+          background-image: url(<?php echo "http://". $_SERVER['SERVER_NAME']?>/template/loading/gifs/5.gif);
+          background-size: 200%;
+          background-position: center;
+          border: 6px solid white;
+      }
+
+      .circle {
+          position:fixed;
+          height: 100px;
+          width: 100px;
+          display: table-cell;
+          text-align: center;
+          vertical-align: middle;
+          border-radius: 50%;
+          background: white;
+          background-color: white;
+          animation: ripple 0.7s linear infinite;   
+      }
+
+      @keyframes ripple {
+          0% {
+              box-shadow: 0 0 0 0 rgba(221, 221, 221, 0.3), 0 0 0 1em rgba(227, 227, 227, 0.3), 0 0 0 3em rgba(235, 235, 235, 0.3), 0 0 0 5em rgba(255, 255, 255, 0.3);
+          }
+          100% {
+              box-shadow: 0 0 0 1em rgba(253, 253, 253, 0.3), 0 0 0 3em rgba(231, 231, 231, 0.3), 0 0 0 5em rgba(249, 249, 249, 0.3), 0 0 0 8em rgba(101, 255, 120, 0);
+          }
+      }
+
+
+    </style>
         
