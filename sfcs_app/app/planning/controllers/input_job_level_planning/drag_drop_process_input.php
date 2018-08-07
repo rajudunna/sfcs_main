e9434b0ed400a409149589eb2c479679cb37215b
satishkalla <satishkalla@schemaxtech.com>
2018-08-07 16:54:31 +0530

    CR #738 Color clubbing task
    (cherry picked from commit 2996b96e0c15e0fc1478fc91c8acd0db03069e23)

--- sfcs_app/app/planning/controllers/input_job_level_planning/drag_drop_process_input.php
+++ sfcs_app/app/planning/controllers/input_job_level_planning/drag_drop_process_input.php
@@ -77,9 +77,15 @@ $userName = getrbac_user()['uname'];
 			// echo $sql.";<br>";
 			mysqli_query($link, $sql) or exit("Sql Error3".mysqli_error($GLOBALS["___mysqli_ston"]));
 
+			$plan_moduleqry="update $bai_pro3.plandoc_stat_log set plan_module= NULL where doc_no in (".$doc_no_ref_input.")";
+			$plan_moduleqry_result=mysqli_query($link, $plan_moduleqry) or exit("plan_moduleqry update error first".mysqli_error($GLOBALS["___mysqli_ston"]));
+
 			$sql="delete from $bai_pro3.plan_dashboard where doc_no in (".$doc_no_ref_input.")";
 			// echo $sql.";<br>";
 			mysqli_query($link, $sql) or exit("Sql Error3".mysqli_error($GLOBALS["___mysqli_ston"]));
+
+			$plan_moduleqry1="update $bai_pro3.plandoc_stat_log set plan_module= NULL where doc_no in (".implode(",",$org_docs).")";
+			$plan_moduleqry_result1=mysqli_query($link, $plan_moduleqry1) or exit("plan_moduleqry update error second".mysqli_error($GLOBALS["___mysqli_ston"]));
 			
 			$sql="delete from $bai_pro3.plan_dashboard where doc_no in (".implode(",",$org_docs).")";
 			// echo $sql.";<br>";
