7fba09d57de2b49c1427c5e6093246b2667b3aaa
saiyateesh <saiyateesh@gmail.com>
2018-08-08 11:21:24 +0530

    CR #717

--- sfcs_app/app/dashboards/controllers/IMS/edit_delete.php
+++ sfcs_app/app/dashboards/controllers/IMS/edit_delete.php
@@ -37,7 +37,7 @@ function validateQty(event)
 		$operation_code = "";
 		$id = $_GET['id'];
 	
-		$qry = "select * from $brandix_bts.tbl_ims_ops where id=".$id;
+		$qry = "select * from $brandix_bts.tbl_ims_ops where appilication=".$id;
 		//echo $qry;	
 		$res_do_num = mysqli_query($link,$qry);
 		//$row=[];
@@ -47,11 +47,19 @@ function validateQty(event)
 	    }
 
 
-	    if(isset($_POST['submit']))
-	    {
+	    
 
-				$qry_insert1 = "update $brandix_bts.tbl_ims_ops set operation_name='$operation_name',operation_code='$operation_code' where id='$id'";
-				echo $qry_insert1;
+ 	}		
+		if(isset($_POST['submit']))
+	    {
+	    	$details=explode('|',$_POST['opn']);
+			//var_dump($details);
+			$operation_name=$details[1];
+			$operation_code=$details[0];
+			$id = $_POST['app'];
+	  
+				$qry_insert1 = "update $brandix_bts.tbl_ims_ops set operation_name='$operation_name',operation_code='$operation_code' where appilication='$id'";
+				//echo $qry_insert1;
 				$res_do_num1 = mysqli_query($link,$qry_insert1);
 				echo "<h3 style='color:red;text-align:center;'>Please Wait!!!  While Redirecting to page !!!</h3>";
 				//$sql_message = 'Operation Updated Successfully...';
@@ -67,10 +75,7 @@ function validateQty(event)
 					}
 				</script>";
 	
-     	}
-
- 	}		
-			
+     	}	
 	
 		//var_dump($row);
 		?>
@@ -85,30 +90,50 @@ function validateQty(event)
 				<a href="#" class="close" data-dismiss="alert">&times;</a>
 				<strong>Info! </strong><span class="sql_message"></span>
 			</div>
-						<div class="form-group">
-							<form name="test" action="index.php" method="GET">
-							
-								<input type="hidden" id="r" name="r" value= "<?= $_GET['r']; ?>">
-								<div>
-									<b></b><input type='hidden' class='form-control' id='id' name='id' required value = <?php echo $_GET['id'] ?> />
-								</div>
-								<div class="row">
-									<div class="col-sm-2">
-										<b>Operation Name<span data-toggle="tooltip" data-placement="top" title="It's Mandatory field"><font color='red'>*</font></span></b>
-										<input type="text" class="form-control" id="opn" name="opn" value= "<?php echo $row[0]['operation_name']?>" required>
-									</div> 
-									<div class="col-sm-2">
-										<b>Operation code<span data-toggle="tooltip" data-placement="top" title="It's Mandatory field"><font color='red'>*</font></span></b><input type="number" onkeypress="return validateQty(event);"  class="form-control" id="opc" name="opc" value= "<?php echo $row[0]['operation_code']?>" required>
-									</div>
-									
-									<div class="col-sm-2">
-										<br>
-									    <input type="submit" name="submit" id="submit" class="btn btn-success" value="Update">
-									</div>
-								</div>
-							</form>
-						</div>
+						<div class="panel-body">
+					<div class="alert alert-danger" style="display:none;">
+						<a href="#" class="close" data-dismiss="alert">&times;</a>
+						<strong>Info! </strong><span class="sql_message"></span>
 					</div>
+					<div class="form-group">
+						<form name="test" class="form-inline" action="index.php?r=<?php echo $_GET['r']; ?>" method="POST" id='form_submt'>
+							<!-- <div class="row"> -->
+								
+								<div class="form-group">
+                                    <!-- <b>Appilication<span data-toggle="tooltip" data-placement="top" title="It's Mandatory field"><font color='red'></font></span></b>
+									<select class="form-control" id="apn" name="apn" required>
+										<option value="">Select</option>
+										<option value="IPS">IPS</option>
+										<option value="IMS">IMS</option>
+									</select> -->
+
+									<b>Operation Name<span data-toggle="tooltip" data-placement="top" title="It's Mandatory field"><font color='red'></font></span></b>
+									<select class="form-control" id="opn" name="opn" required>
+										<option value="">Select</option>
+									
+									<?php
+										$get_operations="SELECT operation_code,operation_name FROM $brandix_bts.tbl_orders_ops_ref where operation_code not in (10,15,200) group by operation_code order by operation_code";
+										$result=mysqli_query($link,$get_operations);
+										while ($test = mysqli_fetch_array($result))
+										{
+											echo '<option value="'.$test['operation_code'].'|'.$test['operation_name'].'">'.$test['operation_name'].' - '.$test['operation_code'].'</option>';
+										}
+									?>
+									</select>
+									<input type="hidden" class="form-control" id="app" name="app" value="<?=$_GET['id']?>">
+								</div> 
+								<!-- <div class="col-sm-2">
+									<b>Operation code<span data-toggle="tooltip" data-placement="top" title="It's Mandatory field"><font color='red'></font></span></b>
+									<input type="text" onkeypress="return validateQty(event);" class="form-control integer" id="opc" name="opc" required>
+								</div> -->
+								<div class="form-group">
+									<!-- <br> -->
+									<input type="submit" name="submit" id="submit" class="btn btn-success" value="Update">
+								</div>		
+							<!-- </div> -->
+						</form>
+					</div>	
+				</div>
 				</div>
 			</div>
 		<?php
