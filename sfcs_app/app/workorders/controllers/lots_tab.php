43b224480794f67ffaa8dd0ca4347470c6cc61c0
rajesh raj <rajesh.nalam@schemaxtech.com>
2018-08-14 10:45:15 +0530

    supplier claim filters applied

--- sfcs_app/app/workorders/controllers/lots_tab.php
+++ sfcs_app/app/workorders/controllers/lots_tab.php
@@ -57,25 +57,16 @@
                     // $url2 = getFullURL($_GET['r'],'user_assigned_role_updation.php','N');
                     echo "<tr>
                             <td>".$sno."</td>
-                            <td id='lot$i'>".$lot_number."</td>
+                            <td>".$lot_number."</td>
                             <td>".$batch_number."</td>
                             <td>".$qty."</td>
                             <td>".$product_group."</td>
-<<<<<<< HEAD
-                            <td class='btn-group'>     
-                                <a href='#' class='btn btn-primary btn-sm'>Receive</a>
-                                <a href='#' class='btn btn-danger btn-sm'>Delete</a> 
-                                <a href='#' class='btn btn-info btn-sm'>Transfer</a> 
-                                <a href='#' class='btn btn-warning btn-sm'>Inspect</a> 
-                                <a href='#' class='btn btn-success btn-sm'>Claim</a>    
-=======
                             <td class='append_buttons' id='column$i'><div class='btn-group'>
                                 <a href='#' class='btn btn-primary btn-sm'>Receive</a>
                                 <a href='#' class='btn btn-danger btn-sm'>Delete</a>
                                 <a href='#' class='btn btn-info btn-sm'>Transfer</a>
                                 <a href='#' class='btn btn-warning btn-sm'>Inspect</a>
                                 <a href='#' class='btn btn-success btn-sm'>Claim</a> </div>   
->>>>>>> 0b51f32c8a0b62d6b8b7b900c3b6064610faa07e
                             </td>
                         </tr>";
                 }
@@ -89,8 +80,6 @@
         ?>
     </tbody>
 </table>
-<<<<<<< HEAD
-=======
       
 <script>
     $(document).ready(function() {
@@ -121,7 +110,6 @@
         //     }
         // });
     });
->>>>>>> 0b51f32c8a0b62d6b8b7b900c3b6064610faa07e
 
     function afterAjax(){
         $('.append_something').each(function(){
