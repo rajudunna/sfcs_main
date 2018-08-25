b45cad2e08f4b5a4f0c83943985e26b177c0859f
ashok@schemaxtech.com <ashok@schemaxtech.com>
2018-08-25 17:10:31 +0530

    Merge Request complted

--- sfcs_app/app/production_xpparel/production_index.php
+++ sfcs_app/app/production_xpparel/production_index.php
@@ -12,15 +12,6 @@
     </ul>
 </div>
 
-<<<<<<< HEAD
-=======
-<div class="btn-group pull-right" role="group" aria-label="..." myattribute="production_body">
-    <div class='pull-left' style='padding-top:7px'><font style='font-size:15px'>WorkArea :&nbsp</label></div>
-    <a href="?r=L3NmY3NfYXBwL2FwcC9kYXNoYm9hcmRzL2NvbnRyb2xsZXJzL0lQUy90bXNfZGFzaGJvYXJkX2lucHV0X3YyMi5waHANCg==&modal=yes" class="btn btn-default" myattribute="production_body" id="sewing">Sewing</a>
-    <div class="btn-group" role="group">
-    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cutting<span class="caret"></span></button><ul class="dropdown-menu"><li><a href="?r=L3NmY3NfYXBwL2FwcC9kYXNoYm9hcmRzL2NvbnRyb2xsZXJzL2Nwcy9mYWJfcHJpb3JpdHlfZGFzaGJvYXJkLnBocA==&modal=yes" myattribute="production_body" id="cut">Cut</a></li><li><a href="?r=<?= base64_encode('/sfcs_app/app/dashboards/controllers/RTS/fab_pps_recut_dashboard_v4.php').'&modal=yes';?>" myattribute="production_body" id="recut">Re-Cut</a></li></ul></div>
-    <div class="btn-group" role="group"><button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Material<span class="caret"></span></button><ul class="dropdown-menu"><li><a href="?r=L3NmY3NfYXBwL2FwcC9kYXNoYm9hcmRzL2NvbnRyb2xsZXJzL3Rtcy90bXNfZGFzaGJvYXJkX2lucHV0LnBocA==&modal=yes" myattribute="production_body" id="trims">Trims</a></li><li><a href="?r=L3NmY3NfYXBwL2FwcC9kYXNoYm9hcmRzL2NvbnRyb2xsZXJzL3Jtcy9mYWJfcHBzX2Rhc2hib2FyZF92Mi5waHA=&modal=yes" myattribute="production_body" id="fabric">Fabric</a></li></ul></div>
->>>>>>> af0cb5c6944cad0e4334b469fc522f263f9118ef
 
 <div class="row">
     <div class="col-md-12">
