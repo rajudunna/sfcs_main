56ab289be41874c9b2fde71162f5ca3cb1cadc46
saiyateesh <saiyateesh@gmail.com>
2018-08-28 17:48:02 +0530

    #830 cr

--- sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/pre_input_job_scanning.php
+++ sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/pre_input_job_scanning.php
@@ -4,6 +4,18 @@
 	include(getFullURLLevel($_GET['r'],'common/config/config.php',5,'R'));
 	$has_permission=haspermission($_GET['r']);
 // error_reporting (0);
+if(isset($_GET['operation_id']))
+	{
+		$status='&sidemenu=true';
+		$input_job_no_random_ref=$_GET['input_job_no_random_ref'];
+		$operation_code=$_GET['operation_id'];
+		$style=$_GET['style'];
+		$schedule=$_GET['schedule'];
+		$module=$_GET['module'];
+		// $operation_name = echo_title("$brandix_bts.tbl_orders_ops_ref","operation_name","operation_code",$operation_code,$link).- $operation_code;
+		// $color = echo_title("$bai_pro3.packing_summary_input","order_col_des","input_job_no_random",$input_job_no_random_ref,$link);
+		$barcode_generation=$_GET['barcode_generation'];
+	}		
 
 $url = getFullURL($_GET['r'],'scan_input_jobs.php','N');
 //echo $url;
@@ -26,11 +38,18 @@ $configuration_bundle_print_array = ['0'=>'Bundle Level','1'=>'Sewing Job Level'
 // if($scanning_methods == 'Bundle Level')
 // $configuration_bundle_print_array = ['1'=>'Sewing Job Level'];
 ?>
-<form method ='POST' action='<?php echo $url ?>'>
+<form method ='POST' action='<?php echo $url."$status" ?>'>
 	<div class="panel panel-primary">
 		<div class="panel-heading"><strong>Input Job Scanning</strong></div>
 		<div class="panel-body">
 			<div class="row">
+				<input type='hidden' id='input_job_no_random_ref' name='input_job_no_random_ref1' value='<?php echo $input_job_no_random_ref  ?>'>
+				<input type='hidden' id='operation_id' name='operation_id1' value='<?php echo $operation_code ?>'>
+				<input type='hidden' id='style' name='style1' value='<?php echo $style ?>'>
+				<input type='hidden' id='schedule' name='schedule1' value='<?php echo $schedule ?>'>
+				<input type='hidden' id='module' name='module1' value='<?php echo $module ?>'>
+
+
 				<div class="form-group col-md-2">
 					<label for="title">Select Operation:<span data-toggle="tooltip" data-placement="top" title="It's Mandatory field"><font color='red'>*</font></span></label>
 						<select class="form-control select2"  name="operation" id="operation" required>
@@ -137,7 +156,9 @@ $(document).ready(function(){
 					$.each(response, function(key,value) {
 						
 							$('select[name="operation"]').append('<option value="'+ key +'">'+value+' - '+key+'</option>');
-					});		    					
+					});
+					$('#operation option[value=<?= $operation_code ?>]').prop('selected', true);
+					$('#operation').prop('disabled', true);		    					
 				
 															  
 			},
@@ -164,3 +185,32 @@ $(document).ready(function(){
 
 
 </script>
+<style>
+.hidden_class,hidden_class_for_remarks{
+	display:none;
+}
+
+</style>
+	
+<?php
+if(isset($_GET['sidemenu'])){
+
+	echo "<style>
+          .left_col,.top_nav{
+          	display:none !important;
+          }
+          .right_col{
+          	width: 100% !important;
+    margin-left: 0 !important;
+          }
+	</style>";
+}
+?>
+<script>
+$(document).ready(function(){
+	
+	$('#barcode_generation option[value=<?= $barcode_generation ?>]').prop('selected', true);
+	$('#barcode_generation').prop('disabled', true);
+})
+</script>	
+
