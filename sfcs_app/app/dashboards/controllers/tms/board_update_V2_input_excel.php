02e4fec8953d3cf27cb12d9cd0207f7a097ac074
santoshkumar <santosh@schemaxtech.com>
2018-09-08 15:21:54 +0530

    modified queries

--- sfcs_app/app/dashboards/controllers/tms/board_update_V2_input_excel.php
+++ sfcs_app/app/dashboards/controllers/tms/board_update_V2_input_excel.php
@@ -1,8 +1,11 @@
 <?php
-    // include (getFullURLLevel($_GET['r'],'common/config/config.php',4,'R'));
-    // include (getFullURLLevel($_GET['r'],'common/config/functions.php',4,'R'));
     include ("../../../../common/config/config.php");
     include ("../../../../common/config/functions.php");
+//     // Report all PHP errors (see changelog)
+// error_reporting(E_ALL);
+
+// // Report all PHP errors
+// error_reporting(-1);
     ?>
     <style>
     table, th, td {
@@ -27,86 +30,76 @@
         array_push($mod_ary,$row2x['module_id']);
     }
     $mod = implode(", ",$mod_ary);
-    $mod_qry = "SELECT DISTINCT(input_module)AS input_module,input_trims_request_time FROM plan_dashboard_input WHERE 
-    input_module IN ($mod) AND input_trims_status= 4";
-    
+    $mod_qry = "SELECT DISTINCT(input_module)AS input_module FROM plan_dashboard_input WHERE 
+    input_module IN ($mod) AND input_trims_status<> 4";
+   
     $result_mod=mysqli_query($link, $mod_qry) or die("Error = ".mysqli_error($GLOBALS["___mysqli_ston"]));
     while($row2 = mysqli_fetch_array($result_mod)){
-        $request_time = $row2['input_trims_request_time'];
+        // $request_time = $row2['input_trims_request_time'];
         array_push($dmod_ary,$row2['input_module']);
     }
     $imp_mod = implode(", ",$dmod_ary);
-    $doc_qry = "SELECT doc_no FROM plandoc_stat_log WHERE plan_module IN($imp_mod)";
-    $doc_result =mysqli_query($link, $doc_qry) or die("Error = ".mysqli_error($GLOBALS["___mysqli_ston"]));
-    while($row2x=mysqli_fetch_array($doc_result))
-    {
-        $i++;
-        $doc_no=$row2x["doc_no"];
-        $sql11x="select order_tid,acutno from $bai_pro3.plandoc_stat_log where doc_no=\"".$doc_no."\"";
-        // echo $sql11x;
-        $sql_result11x=mysqli_query($link, $sql11x) or die("Error1 = ".mysqli_error($GLOBALS["___mysqli_ston"]));
-        while($row11x=mysqli_fetch_array($sql_result11x))
-        {
-            $order_tidx=$row11x["order_tid"];
-            $cut_nosx=$row11x["acutno"];
-        }
-        $sql21x="select order_style_no,order_del_no,order_col_des,order_div,color_code from $bai_pro3.bai_orders_db where order_tid=\"".$order_tidx."\"";
-        // echo $sql21x;
+    if($imp_mod!=''){
 
-        $sql_result21x=mysqli_query($link, $sql21x) or die("Error2 = ".mysqli_error($GLOBALS["___mysqli_ston"]));
-        while($row21x=mysqli_fetch_array($sql_result21x))
-        {
-            $stylex=$row21x["order_style_no"];
-            $schedulex=$row21x["order_del_no"];
-            $colorx=$row21x["order_col_des"];
-            $buyerx=$row21x["order_div"];
-            $color_codex=$row21x["color_code"];
-        }
-        
-        $zeros="00";
-        
-        if($cut_nosx > 9)
-        {
-            $zeros="0";
-        }
-        $report_data_ary[$i]['style']=$stylex;
-        $report_data_ary[$i]['schedule'] = $schedulex;
-        $report_data_ary[$i]['color']=$colorx;
-        $report_data_ary[$i]['docket'] = $doc_no;
-        $report_data_ary[$i]['job_no'] = chr($color_codex).$zeros.$cut_nosx;
-        // $report_data_ary[$i]['req_time']=$row2x["trims_req_time"];
-        // $report_data_ary[$i]['issue_time']=$row2x["trims_issued_time"];
-        $trims_status="";	
-        
-        if($status == 0)
-        {
-            $trims_status="Trims Applied";
-        }
-        
-        if($status == 1)
-        {
-            $trims_status="Stock In Pool";
-        }
-        
-        if($status == 2)
-        {
-            $trims_status="Trims Issued";
-        }
-        
-        if($row2x["trims_req_time"] == "0000-00-00 00:00:00")
-        {
-            $trims_status="Need To Apply For Trims";
-        }
-        // $report_data_ary[$i]['tstatus'] = $trims_status;
-    }
+        $doc_qry = "SELECT doc_no FROM plandoc_stat_log WHERE plan_module IN($imp_mod)";
+        $doc_result =mysqli_query($link, $doc_qry) or die("Error = ".mysqli_error($GLOBALS["___mysqli_ston"]));
     
-    $output = '';
+        while($row2x=mysqli_fetch_array($doc_result))
+        {
+            $doc_no=$row2x["doc_no"];
+            // echo $doc_no;
+            $count_qry = "SELECT ps.input_job_no,ps.order_style_no,ps.order_del_no,ps.order_col_des,ps.acutno,ps.doc_no,
+            pd.input_trims_request_time,pd.log_time,pd.input_trims_status
+            FROM $bai_pro3.packing_summary_input ps
+            LEFT JOIN  plan_dashboard_input pd ON ps.input_job_no_random = pd.input_job_no_random_ref  WHERE ps.doc_no= $doc_no";
+            //  echo $count_qry."<br>";
+
+            $doc_result1=mysqli_query($link, $count_qry) ;
+            while($jobsresult=mysqli_fetch_array($doc_result1)){
+                $report_data_ary[$i]['style'] = $jobsresult['order_style_no'];
+                $report_data_ary[$i]['schedule'] = $jobsresult['order_del_no'];
+                $report_data_ary[$i]['color']=$jobsresult['order_col_des'];
+                $report_data_ary[$i]['docket'] = $jobsresult['doc_no'];
+                $report_data_ary[$i]['job_no'] = $jobsresult['input_job_no'];;
+                $report_data_ary[$i]['req_time']=$jobsresult["input_trims_request_time"];
+                $report_data_ary[$i]['issue_time']=$jobsresult["log_time"];
+                $status = $jobsresult['input_trims_status'];
+
+                $trims_status="";	
+                
+                if($status == 0)
+                {
+                    $trims_status="Trims Applied";
+                }
+                
+                if($status == 1)
+                {
+                    $trims_status="Stock In Pool";
+                }
+                
+                if($status == 2)
+                {
+                    $trims_status="Trims Issued";
+                }
+                
+                if($jobsresult["input_trims_request_time"] == "0000-00-00 00:00:00")
+                {
+                    $trims_status="Need To Apply For Trims";
+                }
+                $report_data_ary[$i]['tstatus'] = $trims_status;
+                $i++;
+            }
+            
+        
+        }
+        $output = '';
     if(sizeof($report_data_ary)>0){
         
         $output.=' <body style="border: 1px solid #ccc"><table ><th colspan="8">Job Plan Details</th><tr><th>Style</th><th>Schedule</th><th>Color</th><th>Docket Number</th><th>Job Number</th><th>Request Time</th><th>Issue Time</th><th>status</th>';
         foreach($report_data_ary as $report){
-
+            
             $output.='<tr><td>'.$report['style'].'</td><td>'.$report['schedule'].'</td><td>'.$report['color'].'</td><td>'.$report['docket'].'</td><td>'.$report['job_no'].'</td><td>'.$report['req_time'].'</td><td>'.$report['issue_time'].'</td><td>'.$report['tstatus'].'</td></tr>';
+            // var_dump( $report_data_ary[$key] ["order_style_no"] );
 
         }
         $output.='</table></body>';
@@ -122,7 +115,17 @@
         </script>";
     }
 
-
+    }else{
+        $url = "board_update_V2_input.php?section_no=".$section;
+        echo"<script>
+            alert('No Data to Download for selected Section:".$section."');
+            window.location = '".$url."';
+        </script>";
+    }
+    
+    // var_dump($report_data_ary);
+    // die();
+    
 
 }
 ?>
