e519f29b5863512d18b374cc514ac33c6ed0ab20
ChandrasekharaPhaniraju Tammana <iamchandu.1992@gmail.com>
2018-09-10 18:40:40 +0530

    #848 responce data save in order_details and shipment_plan

--- sfcs_app/common/config/mo_soap_api_call.php
+++ sfcs_app/common/config/mo_soap_api_call.php
@@ -2,9 +2,9 @@
 /*
 	=========== Create By Chandu =============
 	Created at : 08-09-2018
-	Updated at : 08-09-2018
+	Updated at : 10-09-2018
 	Input : Call SOAP URL.
-	Output : Save the response in mo_details table in m3_inputs database.
+	Output : Save the response in mo_details,order_details&shipment_plan tables in m3_inputs database.
 */
 	include($_SERVER['DOCUMENT_ROOT'].'/template/dbconf.php');
 	error_reporting(E_ALL);
@@ -18,6 +18,7 @@
 		$from = date('Ymd',  strtotime('-1 month'));
 		$result2 = $soap_client->MOData(array('Facility'=>$conf_tool->get('plantcode'),'FromDate'=>$from,'ToDate'=>$to));
 		$i=1;
+		$new_ids = [];
 		echo "From Date:<b>".date('Y-m-d',strtotime($from))."</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;To date:<b>".date('Y-m-d',strtotime($to))."</b><br/>";
 		echo "<table>";
 		echo "<tr><th>S.NO</th><th>MONUMBER</th><th>MOQTY</th><th>STARTDATE</th><th>VPO</th><th>COLORNAME</th><th>COLOURDESC</th><th>SIZENAME</th><th>SIZEDESC</th><th>ZNAME</th><th>ZDESC</th><th>SCHEDULE</th><th>STYLE</th><th>PRODUCT</th><th>PRDNAME</th><th>PRDDESC</th><th>REFERENCEORDER</th><th>REFORDLINE</th><th>MOSTS</th><th>MAXOPERATIONSTS</th><th>COPLANDELDATE</th><th>COREQUESTEDDELDATE</th></tr>";
@@ -48,15 +49,29 @@
 			echo "</tr>";
 			$ins_qry = "
 			INSERT IGNORE INTO `m3_inputs`.`mo_details` 
-            (`MONUMBER`, `MOQTY`, `STARTDATE`, `VPO`, `COLORNAME`, `COLOURDESC`, `SIZENAME`, `SIZEDESC`, `ZNAME`, `ZDESC`, `SCHEDULE`, `STYLE`, `PRODUCT`, `PRDNAME`, `PRDDESC`, `REFERENCEORDER`, `REFORDLINE`, `MOSTS`, `MAXOPERATIONSTS`, `COPLANDELDATE`, `COREQUESTEDDELDATE`) VALUES ('".$value->MONUMBER."','".$value->MOQTY."','".date('Y-m-d',strtotime($value->STARTDATE))."','".$value->VPO."','".$value->COLORNAME."','".$value->COLOURDESC."','".$value->SIZENAME."','".$value->SIZEDESC."','".$value->ZNAME."','".$value->ZDESC."','".$value->SCHEDULE."','".$value->STYLE."','".$value->PRODUCT."','".$value->PRDNAME."','".$value->PRDDESC."','".$value->REFERENCEORDER."','".$value->REFORDLINE."','".$value->MOSTS."','".$value->MAXOPERATIONSTS."','".date('Y-m-d',strtotime($value->COPLANDELDATE))."','".date('Y-m-d',strtotime($value->COREQUESTEDDELDATE))."')
-        ";
-       // echo $ins_qry;
-        $result_time = mysqli_query($link_ui, $ins_qry) or exit("Sql Error update downtime log".mysqli_error($GLOBALS["___mysqli_ston"]));
+            (`MONUMBER`, `MOQTY`, `STARTDATE`, `VPO`, `COLORNAME`, `COLOURDESC`, `SIZENAME`, `SIZEDESC`, `ZNAME`, `ZDESC`, `SCHEDULE`, `STYLE`, `PRODUCT`, `PRDNAME`, `PRDDESC`, `REFERENCEORDER`, `REFORDLINE`, `MOSTS`, `MAXOPERATIONSTS`, `COPLANDELDATE`, `COREQUESTEDDELDATE`) VALUES ('".$value->MONUMBER."','".$value->MOQTY."','".date('Y-m-d',strtotime($value->STARTDATE))."','".$value->VPO."','".$value->COLORNAME."','".$value->COLOURDESC."','".$value->SIZENAME."','".$value->SIZEDESC."','".$value->ZNAME."','".$value->ZDESC."','".$value->SCHEDULE."','".$value->STYLE."','".$value->PRODUCT."','".$value->PRDNAME."','".$value->PRDDESC."','".$value->REFERENCEORDER."','".$value->REFORDLINE."','".$value->MOSTS."','".$value->MAXOPERATIONSTS."','".date('Y-m-d',strtotime($value->COPLANDELDATE))."','".date('Y-m-d',strtotime($value->COREQUESTEDDELDATE))."')";
+			$result = mysqli_query($link_ui, $ins_qry) or exit("Sql Error Insert mo_details".mysqli_error($GLOBALS["___mysqli_ston"]));
+			if($result){
+				$new_ids[] = mysqli_insert_id($link_ui);
+			}
 		}
 		echo "</table>";
+		if(count($new_ids)>0){
+			foreach($new_ids as $idps){
+				$qry_mo_details = "select * from `m3_inputs`.`mo_details` where id = ".$idps;
+				$result_mo_details = mysqli_query($link_ui, $qry_mo_details) or exit("Sql Error Get mo_details".mysqli_error($GLOBALS["___mysqli_ston"]));
+				$mo_details_row = mysqli_fetch_array($result_mo_details);
 
+				$ins_order_details = "INSERT INTO `m3_inputs`.`order_details`(`Facility`, `Customer_Style_No`, `CPO_NO`, `VPO_NO`, `CO_no`, `Style`, `Schedule`, `Manufacturing_Schedule_no`, `MO_Split_Method`, `MO_Released_Status_Y_N`, `GMT_Color`, `GMT_Size`, `GMT_Z_Feature`, `Graphic_Number`, `CO_Qty`, `MO_Qty`, `PCD`, `Plan_Delivery_Date`, `Destination`, `Packing_Method`, `Item_Code`, `Item_Description`, `RM_Color_Description`, `Order_YY_WO_Wastage`, `Wastage`, `Required_Qty`, `UOM`, `MO_NUMBER`, `SEQ_NUMBER`, `time_stamp`) VALUES ('".$conf_tool->get('plantcode')."','','','".$mo_details_row['VPO']."','','".$mo_details_row['STYLE']."','".$mo_details_row['SCHEDULE']."','','','','".$mo_details_row['COLORNAME']."','".$mo_details_row['SIZENAME']."','".$mo_details_row['ZNAME']."','','','".$mo_details_row['MOQTY']."','','".date('Y-m-d',strtotime($mo_details_row['COPLANDELDATE']))."','','','".$mo_details_row['PRODUCT']."','".$mo_details_row['PRDDESC']."','".$mo_details_row['COLOURDESC']."','','','','','".$mo_details_row['MONUMBER']."','','')";
+				$res_order_details = mysqli_query($link_ui, $ins_order_details) or exit("Sql Error Insert Order Details".mysqli_error($GLOBALS["___mysqli_ston"]));
+
+				$ins_shipment_details = "INSERT INTO `m3_inputs`.`shipment_plan`(`Customer_Order_No`, `CO_Line_Status`, `Ex_Factory`, `Order_Qty`, `Mode`, `Destination`, `Packing_Method`, `FOB_Price_per_piece`, `MPO`, `CPO`, `DBFDST`, `Size`, `HMTY15`, `ZFeature`, `MMBUAR`, `Style_No`, `Product`, `Buyer_Division`, `Buyer`, `CM_Value`, `Schedule_No`, `Colour`, `Alloc_Qty`, `Dsptched_Qty`, `BTS_vs_Ord_Qty`, `BTS_vs_FG_Qty`, `time_stamp`) VALUES ('".$mo_details_row['REFERENCEORDER']."','','".$mo_details_row['COPLANDELDATE']."','".$mo_details_row['MOQTY']."','','','','','','','','".$mo_details_row['SIZENAME']."','','".$mo_details_row['ZNAME']."','','".$mo_details_row['STYLE']."','".$mo_details_row['PRODUCT']."','','','','".$mo_details_row['SCHEDULE']."','".$mo_details_row['COLORNAME']."','','','','','')";
+				$res_shipment_details = mysqli_query($link_ui, $ins_shipment_details) or exit("Sql Error Insert Shipment Details".mysqli_error($GLOBALS["___mysqli_ston"]));
+			}
+		}
 	}
 	catch(Exception $e){
 		var_dump($e->getMessage());
 	}
 ?>
+
