27c04092b071e9dae9c55d89e7c3374e1151b793
satishkalla <satishkalla@schemaxtech.com>
2018-09-17 17:00:56 +0530

    #762 simplified logic changes

--- sfcs_app/app/jobs/m3_integrations/m3_vs_sfcs_operation_reporting.php
+++ sfcs_app/app/jobs/m3_integrations/m3_vs_sfcs_operation_reporting.php
@@ -1,98 +1,53 @@
 <?php
 	$start_timestamp = microtime(true);
-	error_reporting(0);
     $include_path=getenv('config_job_path');
-	include($include_path.'\sfcs_app\common\config\config_jobs.php');
-	include($include_path.'\sfcs_app\common\config\rest_api_calls.php');
-    $details=[];
-    $arr1 = [];
-    $arr2 = [];
-    $cono='200';
-    $faci='EKG';
-    $selected_arr = ['OPDS','MFNO','PLG1','PITI','OPNO','MAQT','SCQT'];
-    $date = date('Y-m-d');
-    $sql_mo="SELECT  mo_no,product_sku FROM $bai_pro3.mo_details  where mo_no in (select distinct mo_no from $bai_pro3.m3_transactions where DATE(date_time)='$date') group by mo_no,product_sku";
-    // echo $sql_mo;
+	include( $include_path.'\sfcs_app\common\config\config_jobs.php');
+    include( $include_path.'\sfcs_app\common\config\rest_api_calls.php');
+
+    $mo_details = [];
+    $overall_details = [];
+    $from_date = date('Y-m-d');
+    $to_date = date('Y-m-d');
+    
+    $sql_mo="SELECT  mo_no,product_sku,style,schedule,color,size FROM $bai_pro3.mo_details  where mo_no in (select distinct mo_no from $bai_pro3.m3_transactions where DATE(date_time)>='$from_date' and DATE(date_time)<='$to_date') group by mo_no,product_sku";
     $result_mo=mysqli_query($link, $sql_mo) or exit("Sql Error mo".mysqli_error($GLOBALS["___mysqli_ston"]));
     while($row_mo=mysqli_fetch_array($result_mo))
     {
-        $mo_nos[]=$row_mo['mo_no'];
-        $pr_nos[]=$row_mo['product_sku'];
+        $mo_details[]=['mo_no'=>$row_mo['mo_no'],'product_sku'=>$row_mo['product_sku'],'style'=>$row_mo['style'],'schedule'=>$row_mo['schedule'],'color'=>$row_mo['color'],'size'=>$row_mo['size']];
     }
-    // $mo_nos = ['7512409','1991686','1991678'];
-    // $pr_nos = ['M05083AB%20%20%200190','ASL18SF8%20%20%200026','ACZ06SF8%20%20%200006'];
-    foreach($mo_nos as $key=>$monos)
+    //looping MONO's array
+    foreach($mo_details as $key=>$details)
     {
-        $start = microtime(true);
-        $obj1 = new get_api_call(); 
-        $url="http://eka-mvxsod-01.brandixlk.org:22105/m3api-rest/execute/PMS100MI/SelOperations?CONO=".$cono."&FACI=".$faci."&MFNO=".trim($monos)."&PRNO=".trim($pr_nos[$key]);
+        //calling the API
+        $url=$api_hostname .":".$api_port_no."/m3api-rest/execute/PMS100MI/SelOperations;returncols=MFNO,OPNO,OPDS,MAQT,SCQT?CONO=".$company_no ."&FACI=".$facility_code."&MFNO=".$details['mo_no']."&PRNO=".$details['product_sku'];     
         $url1 = str_replace(' ', '%20', $url);
-        // echo $url1."<br>";
-        $end = microtime(true);
-        $dur = $end - $start;
-        print("API call Execution takes ".$dur." seconds")."\n";
-        $result = $obj1->getCurlAuthRequest($url1);  
-        $decoded = json_decode($result,true);
-        // var_dump($decoded);
-        if($decoded['@type'])
-        {
-		    continue;
-		}
+        $result = $obj->getCurlAuthRequest($url1);      
+        $decoded = json_decode($result,true); 
+        //getting the Namevalue pair from MIRecord       
         $name_values = array_column($decoded['MIRecord'], 'NameValue');
         foreach ($name_values as $key => $value) 
         {
-            foreach ($value as $key1 => $value1) 
-            {
-				if(in_array($value1['Name'] , $selected_arr)) 
-				{
-                    $kk = trim($value1['Name']);
-                    $all_data_array[$kk][] = trim($value1['Value']);
+            //getting the Name and values from name values array
+            $data = array_column($value, 'Value','Name');
+
+            //getting sum of operation code wise data from SFCS table
+            $sql="select sum(good_quantity) as MAQT,sum(rejected_quantity)  as SCQT,op_code as OPNO  from $bai_pro3.mo_operation_quantites where mo_no=".$data['MFNO']." and op_code=".$data['OPNO'];
+            $results_mop=mysqli_query($link, $sql) or exit("Sql Error mo operation quantites".mysqli_error($GLOBALS["___mysqli_ston"]));
+
+            while($row = mysqli_fetch_array($results_mop)){
+                $flag = true;
+                if((float)$data['MAQT'] != (float)$row['MAQT']){
+                    $flag = false;
+                }
+                if((float)$data['SCQT'] != (float)$row['SCQT']){
+                    $flag = false;
+                }
+
+                if($flag == false){
+                    $overall_details[] = ['MFNO'=>$data['MFNO'],'OPNO'=>$data['OPNO'],'OPDS'=>$data['OPDS'],'M3_MAQT'=>$data['MAQT'],'M3_SCQT'=>$data['SCQT'],'SFCS_MAQT'=>$row['MAQT'],'SFCS_SCQT'=>$row['SCQT'],'style'=>$details['style'],'schedule'=>$details['schedule'],'color'=>$details['color'],'size'=>$details['size']];
                 }
             }
         }
-        foreach($all_data_array as $key => $value)
-        {
-            $test[$key]=implode(",",$all_data_array[$key]);
-            $test1[$key]=$all_data_array[$key];
-        }
-        unset($all_data_array);
-        if(sizeof($test1) >0)
-        {
-            $req_fields = ['MAQT','SCQT'];
-            
-            $sql="select good_quantity as MAQT,rejected_quantity as SCQT,mo_no as MONO,op_code as OPNO from $bai_pro3.mo_operation_quantites where op_code in(". $test["OPNO"].") and mo_no in (". $test["MFNO"].")";
-            // echo $sql;
-        
-            $result=mysqli_query($link, $sql) or 
-                    exit("Sql Error 1".mysqli_error($GLOBALS["___mysqli_ston"]));
-            while($row=mysqli_fetch_array($result))
-            {
-                foreach($req_fields as $key => $value)
-                    $new_array[$value][] = $row[$value];
-            }
-            
-        
-            foreach($test1 as $key=>$value)
-            {
-                if($key == 'MAQT' || $key == 'SCQT')
-                {
-                   foreach($value as $key1=>$value1)
-                   {
-                   
-                     $arr1[$test1['MFNO'][$key1]][$key][$test1['OPNO'][$key1]] = $value1;
-                  
-                  }
-                  foreach($new_array[$key] as $key2 => $value2)
-                  {
-                    $arr2[] = $value2;
-                  }
-                }
-            }
-            $mf_nos[] = $test1['MFNO'][0];
-            $details[$test1['MFNO'][0]] = $arr1;
-            unset($arr1);
-        }
-   
     }
     $message='<html> 
     <head> 
@@ -132,8 +87,8 @@
     </style> 
     </head> 
     <body>'; 
-    $message .="Dear all, <br/> Please find the  M3 Quantities and SFCS Quantities";
-    $message.= "<h2>M3 Quantities and SFCS Quantities</h2>"; 
+    $message .="Dear User,";
+    $message.= "<h2>Day end validation report</h2>"; 
     $message.= "<table border=1><tr>
     <th>Sno</th>
     <th>MO NO</th>
@@ -147,58 +102,31 @@
     <th>SFCS Good Quantity</th>
     <th>SFCS Rejected Quantity</th>
     </tr>";
-    if(sizeof($details))
-    {
-        $sno=0;
-        $sql1="select sum(good_quantity) as good_quantity,sum(rejected_quantity) as rejected_quantity,mo_no,op_code,input_job_random from $bai_pro3.mo_operation_quantites where mo_no in (".implode(',',$mf_nos).") 
-            and  op_code in (". $test["OPNO"].") group by mo_no,op_code";
-        // echo $sql1;
-        $result1=mysqli_query($link, $sql1) or exit("Sql Error 2".mysqli_error($GLOBALS["___mysqli_ston"]));
-        while($row=mysqli_fetch_array($result1))
-        {
-            $sql12 = "select style,schedule,group_concat(color) as color,group_concat(size) as size from $bai_pro3.mo_details where mo_no ='".$row['mo_no']."'";
-            // echo $sql12;
-            $result12 = mysqli_query($link,$sql12);
-            while($row12=mysqli_fetch_array($result12))
-            {
-                $style=$row12['style'];
-                $schedule=$row12['schedule'];
-                $color=$row12['color']; 
-                $size=$row12['size'];   
-            }
-            $mono = $row['mo_no'];
-            $opcode = $row['op_code'];
-            $m3_good = $details[$mono][$mono]['MAQT'][trim($opcode)];
-            $m3_rej = $details[$mono][$mono]['SCQT'][trim($opcode)];
-            $sfcs_good = $row['good_quantity'];
-            $sfcs_rej = $row['rejected_quantity'];
-            if($m3_good == $sfcs_good && $m3_rej == $sfcs_rej )
-            {
-
-            }
-            else
-            {
-                $message.= "<tr>
-                <td>".++$sno."</td>
-                <td>".$mono."</td>
-                <td>".$opcode."</td>
-                <td>".$style."</td>
-                <td>".$schedule."</td>
-                <td>".$color."</td>
-                <td>".$size."</td>
-                <td>".$m3_good."</td>
-                <td>".$m3_rej."</td>
-                <td>".$sfcs_good."</td>
-                <td>".$sfcs_rej."</td>
-                </tr>";
-            }
+    if(count($overall_details)>0){
+        foreach($overall_details as $key=>$data){
+            $message.= "<tr>
+            <td>".$key."</td>
+            <td>".$data['MFNO']."</td>
+            <td>".$data['OPNO']."</td>
+            <td>".$data['style']."</td>
+            <td>".$data['schedule']."</td>
+            <td>".$data['color']."</td>
+            <td>".$data['size']."</td>
+            <td>".$data['M3_MAQT']."</td>
+            <td>".$data['M3_SCQT']."</td>
+            <td>".$data['SFCS_MAQT']."</td>
+            <td>".$data['SFCS_SCQT']."</td>
+            </tr>";
         }
+    }else{
+        $message.= "<tr>
+            <td colspan=12><center>No records found<center></td>
+            </tr>";
     }
-
     $message.= "</table><br/><br/>Message Sent Via:".$plant_name."</body> 
     </html>";
     echo $message;
-    $to  ="saiyateesh@gmail.com,"; 
+    $to  ="saiyateesh@gmail.com"; 
     // subject 
     $subject = 'M3 Quantities and SFCS Quantities'; 
     // To send HTML mail, the Content-type header must be set 
@@ -208,15 +136,15 @@
     $headers .= "From: ".$header_name." <".$header_mail.">". "\r\n";
   
     // Mail it 
-    if($sno >0){
+    if(count($overall_details)>0){
         if(mail($to, $subject, $message, $headers)) 
         { 
-            print("mail sent successfully")."\n"; 
+            print("Email Sent Successfully")."\n"; 
+        }else{
+            print("Email Sent Failed")."\n"; 
         }
-    }else{
-            print("mail not send due to no Mismatches")."\n"; 
-
     }
+    
     $end_timestamp = microtime(true);
     $duration = $end_timestamp - $start_timestamp;
     print("Execution took ".$duration." seconds.");
