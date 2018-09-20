702b4881913e7f5e7892d96e8b3a36b2097473e7
ram G <ramkumar@schemaxtech.com>
2018-09-20 16:55:36 +0530

    #922 new function added

--- sfcs_app/app/production/controllers/sewing_job/barcode_new.php
+++ sfcs_app/app/production/controllers/sewing_job/barcode_new.php
@@ -47,11 +47,13 @@
 				<body>';
 
 		$barcode_qry="select * from $bai_pro3.packing_summary_input where order_del_no='".$schedule."' and input_job_no='".$input_job."' order by old_size,tid";			
+		//echo "Qry :".$barcode_qry."</br>";
 		$sql_barcode=mysqli_query($link, $barcode_qry) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
 
 		$seq_num=1;
 		while($barcode_rslt = mysqli_fetch_array($sql_barcode))
 		{
+			$sewing_job_random_id=$barcode_rslt['input_job_no_random'];
 			$barcode=$barcode_rslt['tid'];
 			$color=$barcode_rslt['order_col_des'];
 			$style=$barcode_rslt['order_style_no'];
@@ -68,7 +70,8 @@
 
 
 			$color_code=echo_title("$bai_pro3.bai_orders_db_confirm","color_code","order_col_des='".$color."' and order_del_no",$schedule,$link);
-			$display = get_sewing_job_prefix("prefix","$brandix_bts.tbl_sewing_job_prefix","$bai_pro3.packing_summary_input",$schedule,$color,$input_job,$link);
+			//$display = get_sewing_job_prefix("prefix","$brandix_bts.tbl_sewing_job_prefix","$bai_pro3.packing_summary_input",$schedule,$color,$input_job,$link);
+			$display = get_sewing_job_prefix_inp("prefix","$brandix_bts.tbl_sewing_job_prefix",$input_job,$sewing_job_random_id,$link);
 			// $html.= '<div>
 						// <table>
 							// <tr rowspan=2>
@@ -93,7 +96,8 @@
 			{	
 				$operations=$ops['operation_name'];
 				$opscode=$ops['operation_code'];				
-				$display1 = get_sewing_job_prefix("prefix","$brandix_bts.tbl_sewing_job_prefix","$bai_pro3.packing_summary_input",$schedule,$color,$input_job,$link);
+				//$display1 = get_sewing_job_prefix("prefix","$brandix_bts.tbl_sewing_job_prefix","$bai_pro3.packing_summary_input",$schedule,$color,$input_job,$link);
+				$display1 = get_sewing_job_prefix_inp("prefix","$brandix_bts.tbl_sewing_job_prefix",$input_job,$sewing_job_random_id,$link);
 				$html.= '<div>
 							<div style="margin-left:50px;"><barcode code="'.$barcode.'-'.$opscode.'" type="C39"/ height="0.80" size="0.8" text="1"></div>
 							<table>
