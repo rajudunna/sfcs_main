c0223d039b9487dcb511408de2a2ec15f2bdedc3
Surya Kumari <suryakumari@schemaxtech.com>
2018-10-12 14:43:43 +0530

    #935 column name change

--- sfcs_app/app/masters/inspection_supplier_claim_reasons/insert_inspection_supplier_claim_reasons.php
+++ sfcs_app/app/masters/inspection_supplier_claim_reasons/insert_inspection_supplier_claim_reasons.php
@@ -43,8 +43,8 @@ if (empty($complaint_reason) || empty($complaint_clasification) || empty($compla
 }else{
 	if($tid>0){ 
 		//update
-		$sql = "update $bai_rm_pj1.inspection_complaint_reasons set complaint_reason='$complaint_reason',complaint_clasification='$complaint_clasification',complaint_category='$complaint_category',status='$status' where tid=$tid";
-		//echo $sql;exit;
+		$sql = "update $bai_rm_pj1.inspection_complaint_reasons set complaint_reason='$complaint_reason',complaint_clasification='$complaint_clasification',complaint_category='$complaint_category',status='$status' where tid =$tid";
+		//echo 	$sql;
 		if (mysqli_query($conn, $sql)) {
 			$url=getFullURL($_GET['r'],'save_inspection_supplier_claim_reasons.php','N');
 			
