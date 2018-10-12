1197c5641d26a695d81fb21a018774a248526ed4
Surya Kumari <suryakumari@schemaxtech.com>
2018-10-12 12:11:20 +0530

    #935 data base changes

--- sfcs_app/app/masters/inspection_supplier_claim_reasons/insert_inspection_supplier_claim_reasons.php
+++ sfcs_app/app/masters/inspection_supplier_claim_reasons/insert_inspection_supplier_claim_reasons.php
@@ -43,7 +43,7 @@ if (empty($complaint_reason) || empty($complaint_clasification) || empty($compla
 }else{
 	if($tid>0){
 		//update
-		$sql = "update $bai_rm_pj1.inspection_complaint_reasons set complaint_reason='$complaint_reason',complaint_clasification='$complaint_clasification',complaint_category='$complaint_category',status='$status' where sno=$tid";
+		$sql = "update $bai_rm_pj1.inspection_complaint_reasons set complaint_reason='$complaint_reason',complaint_clasification='$complaint_clasification',complaint_category='$complaint_category',status='$status' where tid=$tid";
 		//echo $sql;exit;
 		if (mysqli_query($conn, $sql)) {
 			$url=getFullURL($_GET['r'],'save_inspection_supplier_claim_reasons.php','N');
