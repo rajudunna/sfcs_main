689cd696896fb9f132849d05ae41cf3f88cfc914
saiyateesh <saiyateesh@gmail.com>
2018-10-17 13:29:46 +0530

    #994

--- sfcs_app/app/dashboards/controllers/IMS/cpanel_main_v2.php
+++ sfcs_app/app/dashboards/controllers/IMS/cpanel_main_v2.php
@@ -554,7 +554,7 @@ $shifts = $_GET['shift'];
                   $sqlred="SELECT SUM(ims_qty) AS Input,SUM(ims_pro_qty) AS Output,ims_doc_no,ims_style,ims_schedule,ims_color,rand_track,input_job_no_ref AS inputjobno,
                     input_job_rand_no_ref AS inputjobnorand,ims_date,pac_tid,acutno FROM bai_pro3.ims_log
                     LEFT JOIN bai_pro3.plandoc_stat_log 
-                    ON ims_log.ims_doc_no=plandoc_stat_log .doc_no  WHERE ims_mod_no='$module' AND ims_status <> 'DONE' GROUP BY inputjobnorand ";
+                    ON ims_log.ims_doc_no=plandoc_stat_log .doc_no  WHERE ims_mod_no='$module' AND ims_status <> 'DONE' GROUP BY inputjobnorand limit $ims_priority_boxes";
 
                 
                  //echo $sqlred;
