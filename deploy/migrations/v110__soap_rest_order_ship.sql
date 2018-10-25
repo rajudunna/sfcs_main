f4b527cd83e194c5425b1c21c204bc916c36cd3a
rajud <rajud@schemaxtech.com>
2018-10-25 16:00:30 +0530

    #1034 query script correction

--- deploy/migrations/v110__soap_rest_order_ship.sql
+++ deploy/migrations/v110__soap_rest_order_ship.sql
@@ -11,5 +11,5 @@ ALTER TABLE `mo_details`
   ADD COLUMN `packing_method` VARCHAR(10) NULL AFTER `product_sku`,
   ADD COLUMN `cpo` VARCHAR(20) NULL AFTER `packing_method`,
   ADD COLUMN `buyer_id` VARCHAR(50) NULL AFTER `cpo`,
-  ADD COLUMN `material_master_status` INT(11) DEFAULT 0 NULL AFTER `buyer_id`,
-  ADD COLUMN `shipment_master_status` INT(11) DEFAULT 0 NULL AFTER `material_master_status`;
+  ADD COLUMN `material_master_status` INT(11) DEFAULT 0 NOT NULL AFTER `buyer_id`, 
+  ADD COLUMN `shipment_master_status` INT(11) DEFAULT 0 NOT NULL AFTER `material_master_status`; 
