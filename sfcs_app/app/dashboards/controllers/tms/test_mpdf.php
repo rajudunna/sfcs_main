c3c45f84cbfdb40560b9dbb88359d82ee78bef2f
ThejaB <theja@schemaxtech.com>
2018-10-25 19:53:54 +0530

    #1084 commit

--- sfcs_app/app/dashboards/controllers/tms/test_mpdf.php
+++ sfcs_app/app/dashboards/controllers/tms/test_mpdf.php
@@ -1,9 +1,11 @@
-<head>
-    <?php
-        include($_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/config/config.php');
-        include($_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/config/functions.php'); 
-        error_reporting(0);
-    ?>
+
+<?php 
+    $html = "";    
+    require_once $_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/lib/mpdf7/vendor/autoload.php'; 
+	include($_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/config/config.php');
+    include($_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/config/functions.php');
+    $html.='
+    <head>
     <link rel="stylesheet" type="text/css" href="../../../../common/css/bootstrap.min.css">
     <title></title>
 </head>
@@ -25,21 +27,16 @@ border: 1px solid black;
 .space{
     margin-bottom: 5px;
 }
-</style>
-<script>
-function printPreview(){
-    
-    //window.open(window.location.href+'&status=1');
-    window.open('test_mpdf.php');
-}
-</script>
-<div class="panel panel-primary">
-    <div class="panel-heading"><center><button onclick="printPreview()" id="printid" style="float:left;color:blue;">Print</button><strong>Job Wise Sewing and Packing Trim Requirement Report - <?= $plant_name ?></strong></center></div>
-    <div class="panel-body">
+</style>';?>
+<?php
+$html.='<div class="panel panel-primary">
+    <div class="panel-heading"><center><strong>Job Wise Sewing and Packing Trim Requirement Report -'. $plant_name .'</strong></center></div>
+    <div class="panel-body">';
+    ?>
         <?php
             //error_reporting(0);
             //include("header.php");
-            $plant_code = $global_facility_code;
+            $plant_code = 'AIP';
             $company_num = $company_no;
             $schedule=$_GET['schedule'];
             $style=$_GET['style'];
@@ -78,7 +75,7 @@ function printPreview(){
                     $display_prefix1 = get_sewing_job_prefix("prefix","$brandix_bts.tbl_sewing_job_prefix","$bai_pro3.packing_summary_input",$schedule,$color,$input_job_no,$link);
                     if($colorrows >0)
                     {
-                        ?>
+                        $html.='
                         <div class="row">
                             <div style="float:left; padding-left: 20px;">
                                 <table width=100%>
@@ -96,34 +93,35 @@ function printPreview(){
                         <div class="row">
                             <div style="float:left; padding-left: 20px;">
                                 <table width=100%>
-                                    <tbody>
+                                    <tbody>';?>
                                     <?php
                                      $j=0;
                                     for($k=0;$k<=$size_count;$k++) 
-                                    { ?>                          
-                                        <tr><td><b>Size:</b></td>
+                                    {                           
+                                        $html.=' <tr><td><b>Size:</b></td>';?>
                                             <?php for($i=$j;$i<$limit;$i++){if($size_code[$i]){
-                                            echo '<td>'.$size_code[$i].'</td>';}
-                                             } ?>
-                                        </tr>
-                                        <tr><td><b>Quantity:</b></td>
+                                            $html.='<td>'.$size_code[$i].'</td>';}
+                                             } 
+                                             $html.='</tr>
+                                        <tr><td><b>Quantity:</b></td>';?>
                                             <?php for($i=$j;$i<$limit;$i++){if($size_code_qty[$i]){
-                                                 echo '<td>'.$size_code_qty[$i].'</td>';
+                                                 $html.='<td>'.$size_code_qty[$i].'</td>';
                                             }
-                                        $j++; } ?>
-                                        </tr>
+                                        $j++; } 
+                                        $html.='</tr>';?>
                                     <?php 
                                         $limit = $limit+$limit1;
                                         if($limit>= ($size_count+$limit1)){
                                             break;
                                         }
                                         
-                                    }  ?>           
-                                    </tbody>
+                                    }             
+                                    $html.='</tbody>
                                 </table>
                             </div>
                         </div>
-                        <br/>
+                        <br/>';
+                        ?>
                         <?php
                         $sql="select order_col_des,size_code,SUM(carton_act_qty) as carton_act_qty from $bai_pro3.packing_summary_input where order_del_no='".$schedule."' and input_job_no='".$input_job_no."' and order_col_des='".$color."' group by size_code";
                         //echo $sql."<br/>";
@@ -164,8 +162,8 @@ function printPreview(){
                                         }
                                     }      
                                 }
-                            }?>
-                            <table width="100%">
+                            }
+                            $html.='<table width="100%">
                                 <thead style="background-color: whitesmoke;">
                                     <tr>
                                         <th>Item Code(SKU)</th>
@@ -181,7 +179,7 @@ function printPreview(){
                                         <th>UOM</th>                
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody>';?>
                                     <?php
                                     if(count($final_data) >0)
                                     {
@@ -223,8 +221,8 @@ function printPreview(){
                                         
                                         if(count($api_selected_valuess_strim)>0)
                                         { 
-                                            ?>
-                                            <tr style="background-color: whitesmoke;"><td colspan=11><center><strong>Sewing Trims</strong></center></td></tr>
+                                           
+                                            $html.='<tr style="background-color: whitesmoke;"><td colspan=11><center><strong>Sewing Trims</strong></center></td></tr>';?>
                                             <?php
                                             foreach($api_selected_valuess_strim as $api_selected_valuess)
                                             {                        
@@ -233,27 +231,27 @@ function printPreview(){
                                                 
                                                 //wastage calculation
                                                 $api_selected_valuess['WASTAGE'] =  (($api_selected_valuess['WITH_WASTAGE']-$api_selected_valuess['WITH_OUT_WASTAGE'])*100)/$api_selected_valuess['WITH_OUT_WASTAGE'];
-                                                ?>
-                                                <tr>
-                                                    <td><?= $api_selected_valuess['MTNO'] ?></td>
-                                                    <td><?= $api_selected_valuess['ITDS'] ?></td>
-                                                    <td><?= $api_selected_valuess['COLOR'] ?></td> 
-                                                    <td><?= $api_selected_valuess['COL_DESC'] ?></td>                          
-                                                    <td><center><?= $api_selected_valuess['SIZE_DESC'] ?><center></td>
-                                                    <td><?=$api_selected_valuess['Z_DESC'] ?></td>
-                                                    <td><?php echo "<span style='float:right;'>".number_format((float)$api_selected_valuess['CNQT'], 4)."</span>"; ?></td>
-                                                    <td><?php echo "<span style='float:right;'>".$api_selected_valuess['WASTAGE']."</span>"; ?></td>
-                                                    <td><?php echo "<span style='float:right;'>".number_format((float)$api_selected_valuess['WITH_WASTAGE'], 2)."</span>"; ?></td>
-                                                    <td><?php echo "<span style='float:right;'>".number_format((float)$api_selected_valuess['WITH_OUT_WASTAGE'], 2)."</span>";?></td>
-                                                    <td><?= $api_selected_valuess['UOM'] ?></td>
-                                                </tr>
+                                                
+                                                $html.="<tr>
+                                                    <td>".$api_selected_valuess['MTNO']."</td>
+                                                    <td>".$api_selected_valuess['ITDS']."</td>
+                                                    <td>".$api_selected_valuess['COLOR']."</td> 
+                                                    <td>".$api_selected_valuess['COL_DESC']."</td>                          
+                                                    <td><center>".$api_selected_valuess['SIZE_DESC']."<center></td>
+                                                    <td>".$api_selected_valuess['Z_DESC']."</td>
+                                                    <td>";?><?php $html.="<span style='float:right;'>".number_format((float)$api_selected_valuess['CNQT'], 4)."</span></td> 
+                                                    <td>";?><?php $html.="<span style='float:right;'>".$api_selected_valuess['WASTAGE']."</span></td>
+                                                    <td>"; ?><?php $html.="<span style='float:right;'>".number_format((float)$api_selected_valuess['WITH_WASTAGE'], 2)."</span></td>
+                                                    <td>"; ?><?php $html.="<span style='float:right;'>".number_format((float)$api_selected_valuess['WITH_OUT_WASTAGE'], 2)."</span></td>
+                                                    <td>".$api_selected_valuess['UOM']."</td>
+                                                </tr>";?>
                                                 <?php 
                                             }
                                         }
                                         if(count($api_selected_valuess_ptrim)>0)
                                         {
-                                            ?>
-                                            <tr style="background-color: whitesmoke;"><td colspan=11><center><strong>Packing Trims</strong></center></td></tr>
+                                            $html.='
+                                            <tr style="background-color: whitesmoke;"><td colspan=11><center><strong>Packing Trims</strong></center></td></tr>';?>
                                             <?php
                                             foreach($api_selected_valuess_ptrim as $api_selected_valuess)
                                             {                               
@@ -262,46 +260,77 @@ function printPreview(){
 
                                                 //wastage calculation
                                                 $api_selected_valuess['WASTAGE'] =  (($api_selected_valuess['WITH_WASTAGE']-$api_selected_valuess['WITH_OUT_WASTAGE'])*100)/$api_selected_valuess['WITH_OUT_WASTAGE'];
-                                                ?>
+                                                $html.="
                                                 <tr>
-                                                    <td><?= $api_selected_valuess['MTNO'] ?></td>
-                                                    <td><?= $api_selected_valuess['ITDS'] ?></td>
-                                                    <td><?=  $api_selected_valuess['COLOR'] ?></td>  
-                                                    <td><?= $api_selected_valuess['COL_DESC'] ?></td>                          
-                                                    <td><center><?= $api_selected_valuess['SIZE_DESC'] ?><center></td>
-                                                    <td><?= $api_selected_valuess['Z_DESC'] ?></td>
-                                                    <td><?php echo "<span style='float:right;'>".number_format((float)$api_selected_valuess['CNQT'], 4)."</span>"; ?></td>
-                                                    <td><?php echo "<span style='float:right;'>".$api_selected_valuess['WASTAGE']."</span>"; ?></td>
-                                                    <td><?php echo "<span style='float:right;'>".number_format((float)$api_selected_valuess['WITH_WASTAGE'], 2)."</span>"; ?></td>
-                                                    <td><?php echo "<span style='float:right;'>".number_format((float)$api_selected_valuess['WITH_OUT_WASTAGE'], 2)."</span>";?></td>
-                                                    <td><?=  $api_selected_valuess['UOM'] ?></td>
-                                                </tr>
-                                                <?php 
+                                                    <td>".$api_selected_valuess['MTNO']."</td>
+                                                    <td>".$api_selected_valuess['ITDS']."</td>
+                                                    <td>".$api_selected_valuess['COLOR']."</td>  
+                                                    <td>".$api_selected_valuess['COL_DESC']."</td>                          
+                                                    <td><center>".$api_selected_valuess['SIZE_DESC'] ."<center></td>
+                                                    <td>".$api_selected_valuess['Z_DESC']."</td>
+                                                    <td>";?><?php $html.="<span style='float:right;'>".number_format((float)$api_selected_valuess['CNQT'], 4)."</span></td>
+                                                    <td>"; ?><?php $html.="<span style='float:right;'>".$api_selected_valuess['WASTAGE']."</span></td>
+                                                    <td>"; ?><?php $html.="<span style='float:right;'>".number_format((float)$api_selected_valuess['WITH_WASTAGE'], 2)."</span></td>
+                                                    <td>"; ?><?php $html.="<span style='float:right;'>".number_format((float)$api_selected_valuess['WITH_OUT_WASTAGE'], 2)."</span></td>
+                                                    <td>".$api_selected_valuess['UOM']."</td>
+                                                </tr>";
                                             }
-                                        }                        
-                                        ?>    
-                                        <br> 
+                                        }     
+                                        $html.='<br>'; ?>    
                                         <?php
                                     }
                                     else
                                     {
-                                        ?>
-                                        <tr>
+                                       
+                                        $html.='<tr>
                                             <td colspan=14><center><strong>No Records Found</strong></center></td>
-                                        </tr>
+                                        </tr>';?>
                                         <?php 
                                     }
-                                    ?>                 
-                                </tbody>   
-                            </table>       
+                                                    
+                                    $html.='</tbody>   
+                            </table>           
                             <hr style="border:1px solid black;">
-                            <br>
+                            <br>';?>
                             <?php
                         }
                     }
                 }
             }
-        ?>
-       
-    </div>
-</div>
+            
+        
+    $html.='</div>
+</div>';?>
+
+
+<?php
+    echo $html;
+	$mpdf = new \Mpdf\Mpdf([
+		'mode' => 'utf-8'
+	]);
+
+	
+    $mpdf->SetHTMLFooter('<table class="footertable" style="width:100%;">
+                            <tbody>
+                                <tr class="footertable">
+                                  <td class="footertable"><hr width=195 class="space"><center><strong>Prepared By(Name/EPF No)</strong><center></td>
+                                  <td class="footertable" style="padding-left:50px;"><hr width=160 class="space"><center><strong>Issuer/EPF No</strong></center></td>
+                                  <td class="footertable" style="padding-left:50px;"><hr width=180 class="space"><center><strong>Receiver Name/EPF No</strong></center></td>
+                                </tr>
+                            </tbody>
+                            </table>
+                            <br/>
+                            <table class="footertable" style="width:100%;">
+                            <tbody >
+                                <tr class="footertable">
+                                  <td class="footertable"><hr width=195 class="dotted space"><center><strong>Signature & Date</strong><center></td>
+                                  <td class="footertable" style="padding-left:50px;"><hr width=170 class="dotted space"><center><strong>Signature & Date</strong></center></td>
+                                  <td class="footertable" style="padding-left:50px;"><hr width=180 class="dotted space"><center><strong>Signature & Date</strong></center></td>
+                                </tr>
+                                </tbody>
+                            </table>');
+	$mpdf->WriteHTML($html); 
+	$mpdf->Output();
+	exit();
+
+?>
