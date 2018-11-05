5c293add82d6043716d00f400d7c37bd4de38d70
ram G <ramkumar@schemaxtech.com>
2018-11-05 19:03:50 +0530

    #1158 git commit

--- sfcs_app/app/jobs/integrations/Schedules_Operation__Masters_Capturing.php
+++ sfcs_app/app/jobs/integrations/Schedules_Operation__Masters_Capturing.php
@@ -16,7 +16,7 @@ while($sql_row=mysqli_fetch_array($result_qry_modetails))
 {
 
     $mo_num=$sql_row['mo_num'];
-    $FG_code=$sql_row['product_sku'];
+    $FG_code=rawurlencode($sql_row['product_sku']);
 
     //getting all work center id in sfcs
     $qry_getworkcenters="SELECT * FROM brandix_bts.tbl_orders_ops_ref";
@@ -25,7 +25,8 @@ while($sql_row=mysqli_fetch_array($result_qry_modetails))
     $workcenters_array=array_column($result_allcenters,'parent_work_center_id');
     
     $url=$api_hostname.":".$api_port_no."/m3api-rest/execute/PMS100MI/SelOperations?CONO=".$company_no."&FACI=".$facility_code."&MFNO=".$mo_num."&PRNO=".$FG_code;
-    $url = str_replace(' ', '%20', $url);
+    //$url = str_replace(' ', '%20', $url);
+    echo "</br>".$url."</br>";
 
     $result = $obj->getCurlAuthRequest($url);
     $decoded = json_decode($result,true);
@@ -85,7 +86,8 @@ while($sql_row=mysqli_fetch_array($result_qry_modetails))
     }
 
     if($workcenter_status_valid){
-       //var_dump($values);
+       echo "</br>".$sql1."</br>";
+       var_dump($values);
         //insertion query for schedule_oprations_master table
        $sql_result1=mysqli_query($link, $sql1 . implode(', ', $values)) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
        if($sql_result1){
