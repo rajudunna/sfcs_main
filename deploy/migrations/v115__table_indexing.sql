862615203153389449db6836e9229b4156d50c5e
rajud <rajud@schemaxtech.com>
2018-11-05 19:34:19 +0530

    #1161 table index script

---
+++ deploy/migrations/v115__table_indexing.sql
@@ -0,0 +1,7 @@
+USE `bai_pro3`;
+ALTER TABLE `mo_operation_quantites` CHANGE 
+`mo_no` `mo_no` INT(11) NULL,
+ CHANGE `ref_no` `ref_no` INT(11) NULL;
+ 
+ USE `m3_inputs`;
+ ALTER TABLE `mo_details` CHANGE `MONUMBER` `MONUMBER` INT(11) NULL; 
