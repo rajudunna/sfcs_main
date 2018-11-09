e28e7ccf6d05a66ecdb729661f3067e3e5110885
saiyateesh <saiyateesh@gmail.com>
2018-11-09 09:37:09 +0530

    #1170

--- sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/scanning_functionality_ajax.php
+++ sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/scanning_functionality_ajax.php
@@ -1263,7 +1263,7 @@ if($barcode_generation == 1)
 						}else{
 							
 							$bulk_insert_post .= '("'.$b_style.'","'. $b_schedule.'","'.$b_colors[$key].'","'.$b_size_code[$key].'","'. $b_sizes[$key].'","'. $sfcs_smv.'","'.$b_tid[$key].'","'.$b_in_job_qty[$key].'","'.$b_in_job_qty[$key].'","'.$b_rep_qty[$key].'","'.$b_rej_qty[$key].'","'.$left_over_qty.'","'. $b_op_id.'","'.$b_doc_num[$key].'","'.date('Y-m-d').'","'.$b_a_cut_no[$key].'","'.$b_inp_job_ref[$key].'","'.$b_job_no.'","'.$b_shift.'","'.$b_module[$key].'")';	
-							//$result_query_001 = $link->query($bulk_insert_post) or exit('bulk_insert_post query error in updating3');
+							$result_query_001 = $link->query($bulk_insert_post) or exit('bulk_insert_post query error in updating3');
 						}
 						//m3 operations............. 
 						if($b_rep_qty[$key] > 0){
@@ -1275,7 +1275,7 @@ if($barcode_generation == 1)
 							if($b_rep_qty[$key] > 0 || $b_rej_qty[$key] >0)
 							{
 								$bulk_insert_post_temp .= '("'.$b_style.'","'. $b_schedule.'","'.$b_colors[$key].'","'.$b_size_code[$key].'","'. $b_sizes[$key].'","'. $sfcs_smv.'","'.$b_tid[$key].'","'.$b_in_job_qty[$key].'","'.$b_in_job_qty[$key].'","'.$b_rep_qty[$key].'","'.$b_rej_qty[$key].'","'.$left_over_qty.'","'. $b_op_id.'","'.$b_doc_num[$key].'","'.date('Y-m-d').'","'.$b_a_cut_no[$key].'","'.$b_inp_job_ref[$key].'","'.$b_job_no.'","'.$b_shift.'","'.$b_module[$key].'","'.$b_remarks[$key].'")';	
-								$result_query_001_temp = $link->query($bulk_insert_post_temp) or exit('bulk_insert_post query error in updating4');
+								//$result_query_001_temp = $link->query($bulk_insert_post_temp) or exit('bulk_insert_post query error in updating4');
 							}
 						}	
 						if($post_ops_code != null)
