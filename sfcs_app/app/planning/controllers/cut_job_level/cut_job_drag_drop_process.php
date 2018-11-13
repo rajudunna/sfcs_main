91b74d16ac1e19b3ffacfc4f88b2efb605fbc021
saiyateesh <saiyateesh@gmail.com>
2018-11-13 12:10:36 +0530

    #1118

--- sfcs_app/app/planning/controllers/cut_job_level/cut_job_drag_drop_process.php
+++ sfcs_app/app/planning/controllers/cut_job_level/cut_job_drag_drop_process.php
@@ -9,47 +9,47 @@
 	$list_db=array();
 	$list_db=explode(";",$list);
 	
-	//var_dump($list_db);
+	// var_dump($list_db);
+	// die();
 	$x=1;
 	
 	for($i=0;$i<sizeof($list_db);$i++)
 	{
 		$items=array();
 		$items=explode("|",$list_db[$i]);
-		//module-doc_no
-		//var_dump($items[0]);
+		
 		if($items[0]=="allItems")
 		{
-	         
 			$sql1="DELETE from $bai_pro3.cutting_table_plan where doc_no=".$items[1];
 			// echo "<br>1=".$sql1."<br>";
 			$re=mysqli_query($link,$sql1) or exit("Sql Error6".mysqli_error());
-			
-			
 		}
 		else
 		{
 			//echo $items[1];
-
-                $sql12="select * from $bai_pro3.cutting_table_plan where doc_no='$items[1]'";
-              // echo $sql12;
-
+            $sql12="select * from $bai_pro3.cutting_table_plan where doc_no='$items[1]'";
+            // echo $sql12;
+			$resultr11211=mysqli_query($link, $sql12) or exit("Sql Error636 == ".$sql12.' == '.mysqli_error($GLOBALS["___mysqli_ston"]));
+			if(mysqli_num_rows($resultr11211)==0)
+			{
+	            $sql12="select * from $bai_pro3.tbl_cutting_table where tbl_id='$items[0]' and status='active'";
 				$resultr112=mysqli_query($link, $sql12) or exit("Sql Error5 == ".$sql12.' == '.mysqli_error($GLOBALS["___mysqli_ston"]));
-				if(mysqli_num_rows($resultr112)==0)
+				//echo $sql12;
+				while($sql_row12=mysqli_fetch_array($resultr112))
 				{
-		            $sql12="select * from $bai_pro3.tbl_cutting_table where tbl_id='$items[0]' and status='active'";
-					$resultr112=mysqli_query($link, $sql12) or exit("Sql Error5 == ".$sql12.' == '.mysqli_error($GLOBALS["___mysqli_ston"]));
-					//echo $sql12;
-					while($sql_row12=mysqli_fetch_array($resultr112))
-					{
-						$tbl_id=$sql_row12["tbl_id"];
-					}
-					$insert_log_query="INSERT INTO $bai_pro3.cutting_table_plan (doc_no,priority,dashboard_ref,cutting_tbl_id,doc_no_ref,username, log_time) VALUES('".$items[1]."', '".$x."','CUT','".$tbl_id."',  '".$items[1]."','".$userName."', NOW())";
-					// echo $insert_log_query."<br>";
-					mysqli_query($link, $insert_log_query) or exit("Sql Error5".mysqli_error($GLOBALS["___mysqli_ston"]));
-				}			
-			$x++;
-				
+					$tbl_id=$sql_row12["tbl_id"];
+				}
+				$insert_log_query="INSERT INTO $bai_pro3.cutting_table_plan (doc_no,priority,dashboard_ref,cutting_tbl_id,doc_no_ref,username, log_time) VALUES('".$items[1]."', '".$x."','CUT','".$tbl_id."',  '".$items[1]."','".$userName."', NOW())";
+				// echo $insert_log_query."<br>";
+				mysqli_query($link, $insert_log_query) or exit("Sql Error5963".mysqli_error($GLOBALS["___mysqli_ston"]));
+			}
+			else
+			{
+                $update_log_query="update $bai_pro3.cutting_table_plan set cutting_tbl_id=".$items[0]." where doc_no=".$items[1]."";
+				// echo $update_log_query."<br>";
+				mysqli_query($link, $update_log_query) or exit("error while updating cutting_table_plan");
+			}			
+			$x++;				
 		}
 		unset($items);
     }
