49c046e5339672f5400628452f769cc62adc143b
ravindra35 <36989030+ravindra35@users.noreply.github.com>
2018-11-21 15:33:34 +0530

    #1136 added backend from hrms to sfcs daily attendance update

--- sfcs_app/app/production/controllers/insert_emp_data_v2.php
+++ sfcs_app/app/production/controllers/insert_emp_data_v2.php
@@ -23,67 +23,27 @@ for($i=0;$i<sizeof($mod_names);$i++)
 	// echo $i."--Absent---".$_POST['aba'.$i]."<br>";
 	// $attenid=$date."-".$mod_names[$i];
 	$sqla="Select * from $bai_pro.pro_attendance where date=\"$date\" and module=\"$mod_names[$i]\" and shift='".$shift."'";
-	echo $sqla."</br>";
+	// echo $sqla."</br>";
 	$sqlresa=mysqli_query($link, $sqla) or exit("Sql Errord $sql1".mysqli_error($GLOBALS["___mysqli_ston"]));
 	if(mysqli_num_rows($sqlresa)==0)
 	{
 		$sql1="INSERT INTO $bai_pro.pro_attendance (date,module,shift) VALUES ('".$date."','$mod_names[$i]','".$shift."')";
-		echo $sql1."</br>";
+		// echo $sql1."</br>";
 		mysqli_query($link, $sql1) or exit("Sql Errore $sql1".mysqli_error($GLOBALS["___mysqli_ston"]));
 		$sql23="update $bai_pro.pro_attendance set present='".$_POST['pra'.$i]."',absent='".$_POST['aba'.$i]."' where date='".$date."' and module='$mod_names[$i]' and shift='".$shift."'";
-		echo $sql23."</br>";
+		// echo $sql23."</br>";
 		mysqli_query($link, $sql23) or exit("Sql Errorf".mysqli_error($GLOBALS["___mysqli_ston"]));
 	}else{
 		$sql22="update $bai_pro.pro_attendance set present='".$_POST['pra'.$i]."',absent='".$_POST['aba'.$i]."' where date='".$date."' and module='$mod_names[$i]' and shift='".$shift."'";
-		echo $sql22."</br>";
+		// echo $sql22."</br>";
 		mysqli_query($link, $sql22) or exit("Sql Errorf".mysqli_error($GLOBALS["___mysqli_ston"]));
 	}
 	
-	
-	// $countattenjump="select count(*) as cnt from $bai_pro.pro_atten_jumper where atten_id='".$date."".$mod_names[$i]."' and module='$mod_names[$i]'";
-	// $rslt=mysqli_query($link, $countattenjump) or exit ("Error while getting count".mysqli_error($GLOBALS["___mysqli_ston"]));
-	// if($row=mysqli_fetch_array($rslt))
-	// {
-	// 	$count=$row['cnt'];
-	// }
-	// if($count==0)
-	// {
-	// 	$sql11="INSERT Ignore INTO $bai_pro.pro_atten_jumper (atten_id, date,module) VALUES ('".$date."".$mod_names[$i]."','".$date."','$mod_names[$i]')";
-	// 	mysqli_query($link, $sql11) or exit("Sql Error88 $sql11".mysqli_error($GLOBALS["___mysqli_ston"]));
-	// }
-	// echo $sql11."</br>";
-
-	// $sql2="update $bai_pro.pro_atten_jumper set avail_$shift='".$_POST['pra'.$i]."',absent_$shift='".$_POST['aba'.$i]."' where atten_id='".$date."".$mod_names[$i]."' and module='$mod_names[$i]'";
-	// mysqli_query($link, $sql2) or exit("Sql Error88 $sql".mysqli_error($GLOBALS["___mysqli_ston"]));
-	// echo $sql2."</br>";
-	
-	// $sql222="Select  jumper from $bai_pro.pro_atten_jumper where atten_id='".$date."".$mod_names[$i]."' and module='$mod_names[$i]'";
-	// $sql_result222=mysqli_query($link, $sql222) or exit ("Sql Error222: $Sql1".mysqli_error($GLOBALS["___mysqli_ston"]));
-	// while($sql_row222=mysqli_fetch_array($sql_result222))
-	// {
-	// $jumper_a=$sql_row222['jumper'];
-	// }
-	
-	// $sql22="update $bai_pro.pro_attendance set present='".$_POST['pra'.$i]."',absent='".$_POST['aba'.$i]."',shift='".$shift."' where date='".$date."' and module='$mod_names[$i]'";
-	// echo $sql22."</br>";
-	// mysqli_query($link, $sql22) or exit("Sql Errorf".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	//update bai_pro.bai_log
-	// $updatebailog="update $bai_pro.bai_log set nop='".$_POST['pra'.$i]."' where bac_date='".$date."' and bac_no='$mod_names[$i]' and bac_shift='$shift'";
-	// mysqli_query($link, $updatebailog) or exit("Error while updating in bai_log".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	//update bai_pro.bai_log_buf
-	// $updatebailogbuf="update $bai_pro.bai_log_buf set nop='".$_POST['pra'.$i]."' where bac_date='".$date."' and bac_no='$mod_names[$i]' and bac_shift='$shift'";
-	// mysqli_query($link, $updatebailogbuf) or exit("Error while updating in bai_log_buf".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	//update bai_pro.grand_rep
-	// $updategrandrep="update $bai_pro.grand_rep set nop='".$_POST['pra'.$i]."',absents='".$_POST['aba'.$i]."' where date='".$date."' and module='$mod_names[$i]' and shift='$shift'";
-	// mysqli_query($link, $updategrandrep) or exit("Error while updating in grand_rep".mysqli_error($GLOBALS["___mysqli_ston"]));
 }
-//   echo "<script type=\"text/javascript\"> setTimeout(\"Redirect()\",0);
-//   		function Redirect() {
-//   			sweetAlert('Attandance Details Sucessfully Updated','','success');
-//   			location.href = \"".getFullURLLevel($_GET['r'], "update_emp_details_v2.php", "0", "N")."\";
-//   			}
-//   		</script>";
+  echo "<script type=\"text/javascript\"> setTimeout(\"Redirect()\",0);
+  		function Redirect() {
+  			sweetAlert('Attandance Details Sucessfully Updated','','success');
+  			location.href = \"".getFullURLLevel($_GET['r'], "update_emp_details_v2.php", "0", "N")."\";
+  			}
+  		</script>";
 ?>
