ee930b58ca0183b61ab2ca0661ae0fd17602b112
rajesh raj <rajesh.nalam@schemaxtech.com>
2018-12-03 18:38:18 +0530

    #1145 ips color issue for clubbing is rectified and comitted

--- sfcs_app/app/dashboards/controllers/IPS/ips_dashboard.php
+++ sfcs_app/app/dashboards/controllers/IPS/ips_dashboard.php
@@ -110,7 +110,6 @@ while($sql_rowx=mysqli_fetch_array($sql_resultx))
 			$result=mysqli_query($link, $sql) or exit("Sql Error5".mysqli_error($GLOBALS["___mysqli_ston"]));
 			while($row=mysqli_fetch_array($result))
 			{
-							
 				$input_job_no_random_ref=$row["input_job_no_random_ref"];
 				$input_trims_status=$row["input_trims_status"];
 				$add_css="behavior: url(border-radius-ie8.htc);  border-radius: 10px;";
@@ -123,6 +122,13 @@ while($sql_rowx=mysqli_fetch_array($sql_resultx))
 				$schedule_no=$row['order_del_no'];
 				$ft_status=$row['ft_status'];
 				$type_of_sewing=$row['type_of_sewing'];
+				//FOR SCHEDULE CLUBBING ensuring for parent docket
+				$parent_doc_query = "SELECT GROUP_CONCAT(org_doc_no) as docs from $bai_pro3.plandoc_stat_log  
+									where doc_no IN ($doc_no_ref) and org_doc_no > 0";
+				$parent_doc_result = mysqli_query($link,$parent_doc_query);
+				if($org_row = mysqli_fetch_array($parent_doc_result))
+					$doc_no_ref = $org_row['docs'];
+
 				if($input_trims_status>1)
 				{
 					$add_css="";
