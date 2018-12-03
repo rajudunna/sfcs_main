87be842642f39151a2c2735857ce88567f2942c7
Bhargav <bhargavg@schemaxtech.com>
2018-12-03 19:00:11 +0530

    #1261 nop and logic updated

--- sfcs_app/app/jobs/planning/generate.php
+++ sfcs_app/app/jobs/planning/generate.php
@@ -1,5 +1,6 @@
 <?php 
 $include_path=getenv('config_job_path');
+
 include($include_path.'\sfcs_app\common\config\config_jobs.php');
 $start_timestamp = microtime(true);
 error_reporting(0);
@@ -7,104 +8,48 @@ error_reporting(0);
 
 <?php
 
+$shifts_array = $conf->get('shifts');
 $date=date("Y-m-d");
 
 $sql="delete from $bai_pro.bai_bac";
-mysqli_query($link, $sql) or exit("Sql Error1".mysqli_error($GLOBALS["___mysqli_ston"]));
+mysqli_query($link, $sql) or exit("Sql Error1-$sql".mysqli_error($GLOBALS["___mysqli_ston"]));
 
-//NEW2011
 $sql="delete from $bai_pro.pro_mod_today";
 mysqli_query($link, $sql) or exit("Sql Error2".mysqli_error($GLOBALS["___mysqli_ston"]));
+
 $sql="delete from $bai_pro.pro_plan_today";
 mysqli_query($link, $sql) or exit("Sql Error3".mysqli_error($GLOBALS["___mysqli_ston"]));
+
 $sql="delete from $bai_pro.pro_style_today";
 mysqli_query($link, $sql) or exit("Sql Error4".mysqli_error($GLOBALS["___mysqli_ston"]));
 
-//NEW2011
-
-
-if(mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "select * from $bai_pro.pro_mod where mod_date=\"$date\""))==0)
-{
-
-$sql="select * from $bai_pro.pro_mod where mod_date=(select max(mod_date) from bai_pro.pro_mod)";
+$sql="select * from $bai_pro3.module_master";
 $sql_result=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
 while($sql_row=mysqli_fetch_array($sql_result))
 {
-	$division=$sql_row['division'];
-	$buyer=$sql_row['buyer'];
-	$delivery=$sql_row['delivery'];
-	$color=$sql_row['color'];
-			
-	$mod_number=$sql_row['mod_no'];
-	$mod_section=$sql_row['mod_sec'];
-	$mod_shifts=$sql_row['mod_shift'];
-	$stat=$sql_row['mod_stat'];
-	$mod_style=$sql_row['mod_style'];
-	$remarks=$sql_row['mod_remarks'];
-	$couple=$sql_row['couple'];
-
-
+	$mod_number=$sql_row['module_name'];
+	$mod_section=$sql_row['section'];
+	$stat=$sql_row['status'];
+	$remarks=$sql_row['module_description'];
+	$couple=count($shifts_array);
 	$ref_code=$date."-".$mod_number;
-
-	$sql="insert ignore into $bai_pro.pro_mod (ref_id) values (\"$ref_code\")";
-	mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	$sql="update $bai_pro.pro_mod set mod_no=$mod_number, mod_date=\"$date\", mod_sec=$mod_section, mod_shift=\"$mod_shifts\", mod_stat=\"$stat\",  mod_style=\"$mod_style\",  mod_lqty=0, mod_remarks=\"$remarks\", division=\"$division\", buyer=\"$buyer\", delivery=\"$delivery\", color=\"$color\", mod_lupdate=\"$date\", couple=$couple where ref_id=\"$ref_code\"";
-	mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	//NEW2011
-	$sql="insert ignore into $bai_pro.pro_mod_today (ref_id) values (\"$ref_code\")";
-	mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	$sql="update $bai_pro.pro_mod_today set mod_no=$mod_number, mod_date=\"$date\", mod_sec=$mod_section, mod_shift=\"$mod_shifts\", mod_stat=\"$stat\",  mod_style=\"$mod_style\",  mod_lqty=0, mod_remarks=\"$remarks\", division=\"$division\", buyer=\"$buyer\", delivery=\"$delivery\", color=\"$color\", mod_lupdate=\"$date\", couple=$couple where ref_id=\"$ref_code\"";
-	mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	//NEW2011
-	
-}
-
-}
-else
-{
-	$sql="delete from $bai_pro.pro_mod where mod_date>=\"".date("Y-m-d")."\"";
-	mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	$sql="select * from $bai_pro.pro_mod where mod_date=(select max(mod_date) from bai_pro.pro_mod)";
-	$sql_result=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	while($sql_row=mysqli_fetch_array($sql_result))
+	for($i=0;$i<sizeof($shifts_array);$i++)
 	{
-		$division=$sql_row['division'];
-		$buyer=$sql_row['buyer'];
-		$delivery=$sql_row['delivery'];
-		$color=$sql_row['color'];
-				
-		$mod_number=$sql_row['mod_no'];
-		$mod_section=$sql_row['mod_sec'];
-		$mod_shifts=$sql_row['mod_shift'];
-		$stat=$sql_row['mod_stat'];
-		$mod_style=$sql_row['mod_style'];
-		$remarks=$sql_row['mod_remarks'];
-		$couple=$sql_row['couple'];
-	
-	
-		$ref_code=$date."-".$mod_number;
-	
 		$sql="insert ignore into $bai_pro.pro_mod (ref_id) values (\"$ref_code\")";
 		mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
 		
-		$sql="update $bai_pro.pro_mod set mod_no=$mod_number, mod_date=\"$date\", mod_sec=$mod_section, mod_shift=\"$mod_shifts\", mod_stat=\"$stat\",  mod_style=\"$mod_style\",  mod_lqty=0, mod_remarks=\"$remarks\", division=\"$division\", buyer=\"$buyer\", delivery=\"$delivery\", color=\"$color\", mod_lupdate=\"$date\", couple=$couple where ref_id=\"$ref_code\"";
+		$sql="update $bai_pro.pro_mod set mod_no=$mod_number, mod_date=\"$date\", mod_sec=$mod_section, mod_shift='$shifts_array[$i], mod_stat=\"$stat\", mod_remarks=\"$remarks\", mod_lupdate=\"$date\", couple=$couple where ref_id=\"$ref_code\"";
 		mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
 		
 		//NEW2011
 		$sql="insert ignore into $bai_pro.pro_mod_today (ref_id) values (\"$ref_code\")";
 		mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
 		
-		$sql="update $bai_pro.pro_mod_today set mod_no=$mod_number, mod_date=\"$date\", mod_sec=$mod_section, mod_shift=\"$mod_shifts\", mod_stat=\"$stat\",  mod_style=\"$mod_style\",  mod_lqty=0, mod_remarks=\"$remarks\", division=\"$division\", buyer=\"$buyer\", delivery=\"$delivery\", color=\"$color\", mod_lupdate=\"$date\", couple=$couple where ref_id=\"$ref_code\"";
+		$sql="update $bai_pro.pro_mod_today set mod_no=$mod_number, mod_date=\"$date\", mod_sec=$mod_section, mod_shift='$shifts_array[$i], mod_stat=\"$stat\", mod_remarks=\"$remarks\", mod_lupdate=\"$date\", couple=$couple where ref_id=\"$ref_code\"";
 		mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-		//NEW2011
-		
-	}
+	}	
 }
+
 
 if(mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "select * from $bai_pro.pro_style where date=\"$date\""))==0)
 {
@@ -204,182 +149,78 @@ else
 	}
 }
 
-if(mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "select * from $bai_pro.pro_plan where date=\"$date\""))==0)
+$sql1="select * from $bai_pro.tbl_freez_plan_log where date='".$date."'";
+$sql_result1=mysqli_query($link, $sql1) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
+if(mysqli_num_rows($sql_result1))
 {
-$sql="select * from $bai_pro.tbl_freez_plan_log where date='".$date."'";
-mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-$sql_result=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-while($sql_row=mysqli_fetch_array($sql_result))
-{
-	$mod_no=$sql_row['mod_no'];
-	$shift=$sql_row['shift'];
-	$plan_eff=$sql_row['plan_eff'];
-	$plan_pro=$sql_row['plan_pro'];
-	$remarks='';
-	$sec_no=$sql_row['sec_no'];
-	$couple=1;
-	$fix_nop=16;
-	
-	$plan_clh=$sql_row['plan_clh'];
-	$plan_sah=$sql_row['plan_sah'];
-	
-	$plan_eff_ex=0;
+	while($sql_row=mysqli_fetch_array($sql_result))
+	{
+		$mod_no=$sql_row['mod_no'];
+		$shift=$sql_row['shift'];
+		$plan_eff=$sql_row['plan_eff'];
+		$plan_pro=$sql_row['plan_pro'];
+		$remarks='';
+		$sec_no=$sql_row['sec_no'];
+		$couple=count($shifts_array);
+		$fix_nop=$sql_row['nop'];	
+		$plan_clh=$sql_row['plan_clh'];
+		$plan_sah=$sql_row['plan_sah'];	
+		$plan_eff_ex=0;
+		$ref_code=$date."-".$mod_no."-".$shift;
 
-
-	$ref_code=$date."-".$mod_no."-".$shift;
-
-	$sql="insert ignore into $bai_pro.pro_plan (plan_tag) values (\"$ref_code\")";
-	mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	$sql="update $bai_pro.pro_plan set mod_no=$mod_no, date=\"$date\", remarks=\"$remarks\", shift=\"$shift\", plan_eff=$plan_eff, plan_pro=$plan_pro, sec_no=$sec_no, act_hours=7.5, couple=$couple, fix_nop=$fix_nop,plan_clh=$plan_clh,plan_sah=$plan_sah, plan_eff_ex=$plan_eff_ex where plan_tag=\"$ref_code\"";
-	mysqli_query($link, $sql) or exit("Sql Error2".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	//NEW2011
-	$sql="insert ignore into $bai_pro.pro_plan_today (plan_tag) values (\"$ref_code\")";
-	mysqli_query($link, $sql) or exit("Sql Error4".mysqli_error($GLOBALS["___mysqli_ston"]));
-	
-	$sql="update $bai_pro.pro_plan_today set mod_no=$mod_no, date=\"$date\", remarks=\"$remarks\", shift=\"$shift\", plan_eff=$plan_eff, plan_pro=$plan_pro, sec_no=$sec_no, act_hours=7.5, couple=$couple, fix_nop=$fix_nop,plan_clh=$plan_clh,plan_sah=$plan_sah, plan_eff_ex=$plan_eff_ex where plan_tag=\"$ref_code\"";
-	mysqli_query($link, $sql) or exit("Sql Error3".mysqli_error($GLOBALS["___mysqli_ston"]));
-	//NEW2011
-	
-}
-
+		$sql="insert ignore into $bai_pro.pro_plan (plan_tag) values (\"$ref_code\")";
+		mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
+		$sql="update $bai_pro.pro_plan set mod_no=$mod_no, date=\"$date\", remarks=\"$remarks\", shift=\"$shift\", plan_eff=$plan_eff, plan_pro=$plan_pro, sec_no=$sec_no, act_hours=7.5, couple=$couple, fix_nop=$fix_nop,plan_clh=$plan_clh,plan_sah=$plan_sah, plan_eff_ex=$plan_eff_ex where plan_tag=\"$ref_code\"";
+		mysqli_query($link, $sql) or exit("Sql Error2".mysqli_error($GLOBALS["___mysqli_ston"]));
+		
+		//NEW2011
+		$sql="insert ignore into $bai_pro.pro_plan_today (plan_tag) values (\"$ref_code\")";
+		mysqli_query($link, $sql) or exit("Sql Error4".mysqli_error($GLOBALS["___mysqli_ston"]));
+		
+		$sql="update $bai_pro.pro_plan_today set mod_no=$mod_no, date=\"$date\", remarks=\"$remarks\", shift=\"$shift\", plan_eff=$plan_eff, plan_pro=$plan_pro, sec_no=$sec_no, act_hours=7.5, couple=$couple, fix_nop=$fix_nop,plan_clh=$plan_clh,plan_sah=$plan_sah, plan_eff_ex=$plan_eff_ex where plan_tag=\"$ref_code\"";
+		mysqli_query($link, $sql) or exit("Sql Error3".mysqli_error($GLOBALS["___mysqli_ston"]));
+		//NEW2011		
+	}
 }
 else
 {
-	$sql="delete from $bai_pro.pro_plan where date>=\"".date("Y-m-d")."\"";
-	$res9=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	if($res9)
-	{
-		print("deleted from  pro_plan table successfully")."\n";
-	}
-	$sql="select * from $bai_pro.pro_plan where date=(select max(date) from bai_pro.pro_plan)";
-	$sql_result=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
+	$sql1="select * from $bai_pro.tbl_freez_plan_log where date=(select max(date) from $bai_pro.tbl_freez_plan_log)";
 	while($sql_row=mysqli_fetch_array($sql_result))
 	{
 		$mod_no=$sql_row['mod_no'];
 		$shift=$sql_row['shift'];
 		$plan_eff=$sql_row['plan_eff'];
 		$plan_pro=$sql_row['plan_pro'];
-		$remarks=$sql_row['remarks'];
+		$remarks='';
 		$sec_no=$sql_row['sec_no'];
-		$couple=$sql_row['couple'];
-		$fix_nop=$sql_row['fix_nop'];
-		
+		$couple=count($shifts_array);
+		$fix_nop=$sql_row['nop'];	
 		$plan_clh=$sql_row['plan_clh'];
-		$plan_sah=$sql_row['plan_sah'];
-		
-		$plan_eff_ex=$sql_row['plan_eff_ex'];
-	
-	
+		$plan_sah=$sql_row['plan_sah'];	
+		$plan_eff_ex=0;
 		$ref_code=$date."-".$mod_no."-".$shift;
-	
+
 		$sql="insert ignore into $bai_pro.pro_plan (plan_tag) values (\"$ref_code\")";
-		$res8=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-		if($res8)
-		{
-			print("inserted into pro_plan table successfully")."\n";
-		}
+		mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
 		$sql="update $bai_pro.pro_plan set mod_no=$mod_no, date=\"$date\", remarks=\"$remarks\", shift=\"$shift\", plan_eff=$plan_eff, plan_pro=$plan_pro, sec_no=$sec_no, act_hours=7.5, couple=$couple, fix_nop=$fix_nop,plan_clh=$plan_clh,plan_sah=$plan_sah, plan_eff_ex=$plan_eff_ex where plan_tag=\"$ref_code\"";
-		$res7=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-		if($res7)
-		{
-			print("updated pro_plan table successfully")."\n";
-		}
+		mysqli_query($link, $sql) or exit("Sql Error2".mysqli_error($GLOBALS["___mysqli_ston"]));
+		
 		//NEW2011
 		$sql="insert ignore into $bai_pro.pro_plan_today (plan_tag) values (\"$ref_code\")";
-		$res6=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-		if($res6)
-		{
-			print("updated pro_plan_today table successfully")."\n";
-		}
+		mysqli_query($link, $sql) or exit("Sql Error4".mysqli_error($GLOBALS["___mysqli_ston"]));
+		
 		$sql="update $bai_pro.pro_plan_today set mod_no=$mod_no, date=\"$date\", remarks=\"$remarks\", shift=\"$shift\", plan_eff=$plan_eff, plan_pro=$plan_pro, sec_no=$sec_no, act_hours=7.5, couple=$couple, fix_nop=$fix_nop,plan_clh=$plan_clh,plan_sah=$plan_sah, plan_eff_ex=$plan_eff_ex where plan_tag=\"$ref_code\"";
-		$res5=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-		if($res5)
-		{
-			print("updated pro_plan_today table successfully")."\n";
-		}
-		//NEW2011
-		
+		mysqli_query($link, $sql) or exit("Sql Error3".mysqli_error($GLOBALS["___mysqli_ston"]));
+		//NEW2011		
 	}
-	
-}
-
-//New 20160519 // KiranG to update based on freezed plan.
-if(mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "select * from $bai_pro.tbl_freez_plan_log where date=\"$date\""))>1)
-{
-	
-	$sql="select * from $bai_pro.tbl_freez_plan_log where date=\"$date\"";
-	$sql_result=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	while($sql_row=mysqli_fetch_array($sql_result))
-	{
-		$mod_no=$sql_row['mod_no'];
-		$shift=$sql_row['shift'];
-		$plan_eff=$sql_row['plan_eff'];
-		$plan_pro=$sql_row['plan_pro'];
-		$sec_no=$sql_row['sec_no'];
-		
-		$plan_clh=$sql_row['plan_clh'];
-		$plan_sah=$sql_row['plan_sah'];
-		
-	
-		$ref_code=$date."-".$mod_no."-".$shift;
-	
-		
-		
-		$sql="update $bai_pro.pro_plan set mod_no=$mod_no, date=\"$date\",  shift=\"$shift\", plan_eff=$plan_eff, plan_pro=$plan_pro,  plan_clh=$plan_clh,plan_sah=$plan_sah where plan_tag=\"$ref_code\"";
-		$res4=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-		if($res4)
-		{
-			print("updated pro_plan table successfully")."\n";
-		}
-		$sql="update $bai_pro.pro_plan_today set mod_no=$mod_no, date=\"$date\",  shift=\"$shift\", plan_eff=$plan_eff, plan_pro=$plan_pro,  plan_clh=$plan_clh,plan_sah=$plan_sah where plan_tag=\"$ref_code\"";
-		$res3=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-		if($res3)
-		{
-			print("updated pro_plan_today table successfully")."\n";
-		}
-		//NEW2011
-	}
-	
-	//Reset Values
-	
-	$sql="select * from $bai_pro.pro_plan_today where plan_tag not in (select plan_tag from bai_pro.tbl_freez_plan_log where date=\"$date\")";
-	$sql_result=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-	while($sql_row=mysqli_fetch_array($sql_result))
-	{
- 		$ref_code=$sql_row['ref_code'];
-		
-		$sql="update $bai_pro.pro_plan_today set plan_eff=0, plan_pro=0,  plan_clh=0,plan_sah=0 where plan_tag=\"$ref_code\"";
-		$res2=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-		if($res2)
-		{
-			print("updated pro_plan_today table successfully")."\n";
-		}
-		
-		$sql="update $bai_pro.pro_plan set plan_eff=0, plan_pro=0,  plan_clh=0,plan_sah=0 where plan_tag=\"$ref_code\"";
-		$res1=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-		if($res1)
-		{
-			print("updated pro_plan table successfully")."\n";
-		}
-	}
-	
-}
-
-
-//To track success of schedule
-
+}	
 $sql="insert into $bai_ict.report_alert_track(report,date) values ('PRO_NEW_DB','".date("Y-m-d H:i:s")."')";
 $res=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
 if($res)
 {
 	print("inserted report_alert_track table successfully")."\n";
 }
-//To track success of schedule
-// $dir = 'cache/';
-// foreach(glob($dir.'*.*') as $v){
-// unlink($v);
-// }
+
 $end_timestamp = microtime(true);
 $duration = $end_timestamp - $start_timestamp;
 print("Execution took ".$duration." milliseconds.");
