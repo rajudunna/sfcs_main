6d0837da50fb002c939a9de5a9d8f85e8197576e
rajud <rajud@schemaxtech.com>
2018-12-12 10:46:39 +0530

    #1289 new menu script

---
+++ deploy/migrations/v138__new_menu.sql
@@ -0,0 +1,3 @@
+INSERT INTO central_administration_sfcs.tbl_menu_list(menu_pid,page_id,fk_group_id,fk_app_id,parent_id,link_type,link_status,link_visibility,link_location,link_description,link_tool_tip,link_cmd) VALUES ('1620','SFCS_9005',8,8,212,1,1,1,'/sfcs_app/app/production/controllers/delete_0F_jobs_ips.php','IPS jobs',6,'remove');
+
+INSERT INTO central_administration_sfcs.rbac_role_menu(menu_pid,menu_description,roll_id) VALUES (1620,'Remove Jobs From IPS',1);
