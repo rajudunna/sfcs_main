d912264a189ff24e5333ee8817798809248e3192
SRINIVASA RAO YADAVALLI <ysrinivas@schemaxtech.com>
2018-12-13 20:24:48 +0530

    recut check

--- sfcs_app/app/dashboards/controllers/recut_dashboards/recut_dashboard_issue.php
+++ sfcs_app/app/dashboards/controllers/recut_dashboards/recut_dashboard_issue.php
@@ -130,15 +130,15 @@ if(isset($_POST['formSubmit']))
                 $result1=mysqli_query($link, $mo_operations_insertion) or die("Error while mo_operations_insertion".mysqli_error($GLOBALS["___mysqli_ston"]));
             }
             //updating bcd and cps log
-            $update_cps_qry = "update $bai_pro3.cps_log set remaining_qty = remaining_qty+$excess_qty where doc_no = $doc_nos and operation_code=15 and size_code='$size'";
-            echo $update_cps_qry;
+            $update_cps_qry = "update $bai_pro3.cps_log set cut_quantity = cut_quantity+$excess_qty where doc_no = $doc_nos and operation_code=15 and size_code='$size'";
+            // echo $update_cps_qry;
             mysqli_query($link, $update_cps_qry) or die("Error while update_cps_qry".mysqli_error($GLOBALS["___mysqli_ston"]));
             $update_bcd_qry = "update $brandix_bts.bundle_creation_data set original_qty=original_qty+$excess_qty where docket_number = $doc_nos and size_id = '$size' and operation_id = 15";
-            echo $update_bcd_qry;
+            // echo $update_bcd_qry;
             mysqli_query($link, $update_bcd_qry) or die("Error while update_bcd_qry".mysqli_error($GLOBALS["___mysqli_ston"]));
         }   
     }
-    die();
+    // die();
     $sql="insert into $bai_pro3.recut_track(doc_no,username,sys_name,log_time,level,status) values(\"".$doc_nos."\",\"".$username."\",\"".$hostname[0]."\",\"".date("Y-m-d H:i:s")."\",\"".$codes."\",\"".$status."\")";
     mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
     $url = '?r='.$_GET['r'];
