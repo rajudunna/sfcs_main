8d27c464cdea9023887e2e97167e69dd9c250a36
ThejaB <theja@schemaxtech.com>
2018-12-13 13:34:45 +0530

    #1132 added new permission

--- sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/scan_input_jobs.php
+++ sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/scan_input_jobs.php
@@ -7,10 +7,20 @@
 	include(getFullURLLevel($_GET['r'],'common/config/functions.php',5,'R'));
 	$has_permission=haspermission($_GET['r']);
 
-	if(in_array($authorized,$has_permission)) { 
+	if(in_array($authorized,$has_permission))
+	{ 
 		$value = 'authorized';
-	} else {
-		$value = 'not_authorized';
+	}
+	else
+	{
+		if (in_array($override_sewing_limitation,$has_permission))
+		{
+			$value = 'authorized';
+		}
+		else
+		{
+			$value = 'not_authorized';
+		}
 	}
 	echo '<input type="hidden" name="user_permission" id="user_permission" value="'.$value.'">';
 
