31fbc75bfddcf617443608de893daa6d0d0315e7
mounika pentakota <mounika@schemaxtech.com>
2018-12-15 15:21:25 +0530

    commit #935

--- sfcs_app/app/masters/Inspection_rejection_reasons/delete_inspection_rej_reasons.php
+++ sfcs_app/app/masters/Inspection_rejection_reasons/delete_inspection_rej_reasons.php
@@ -13,8 +13,21 @@ if (!$conn) {
 $url=getFullURL($_GET['r'],'save_inspection_rej_reasons.php','N');
   
 $delete="delete from bai_rm_pj1.reject_reasons where tid='$rid'";
-$sql_result=mysqli_query($link, $delete) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-    echo "<script>window.location = '".$url."'</script>";
+if (mysqli_query($conn, $delete)) {
+	echo"<script>setTimeout(function () { 
+		swal({
+			title: 'Deleted successfully.',
+			type: 'success',
+			confirmButtonText: 'OK'
+		},
+		function(isConfirm){
+			if (isConfirm) {
+			window.location.href = \"$url\";
+			}
+		}); }, 100);</script>";
+	echo "<script>window.location.href = \"$url\"</script>";
 
-//header('location: index.php?r=L3NmY3NfYXBwL2FwcC9tYXN0ZXJzL2Rvd250aW1lcmVhc29uL2Rvd25fdGltZV9yZWFzb25fYWRkLnBocA==');
+		} else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
 ?>
