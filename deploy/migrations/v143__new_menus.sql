0f03a8f3af3099a8424f6847a56dec18ce9f79ca
rajud <rajud@schemaxtech.com>
2018-12-17 13:19:47 +0530

    #1145 script correction menu_id to menu_pid field

--- deploy/migrations/v143__new_menus.sql
+++ deploy/migrations/v143__new_menus.sql
@@ -1,5 +1,5 @@
 /*1145*/
-INSERT INTO `central_administration_sfcs`.`tbl_menu_list` (`menu_id`,`page_id`, `fk_group_id`, `fk_app_id`, `parent_id`, `link_type`, `link_visibility`, `link_location`, `link_description`, `link_tool_tip`, `link_cmd`) VALUES ('1621', 'SFCS_0355', '8', '4', '109', '0', '1', '', 'Schedule Clubbing(Color Level)', '2', ''); 
+INSERT INTO `central_administration_sfcs`.`tbl_menu_list` (`menu_pid`,`page_id`, `fk_group_id`, `fk_app_id`, `parent_id`, `link_type`, `link_visibility`, `link_location`, `link_description`, `link_tool_tip`, `link_cmd`) VALUES ('1621', 'SFCS_0355', '8', '4', '109', '0', '1', '', 'Schedule Clubbing(Color Level)', '2', ''); 
 UPDATE `central_administration_sfcs`.`tbl_menu_list` SET `parent_id` = '1621' WHERE `menu_pid` = '1462'; 
 UPDATE `central_administration_sfcs`.`tbl_menu_list` SET `parent_id` = '1621' WHERE `menu_pid` = '1463'; 
 UPDATE `central_administration_sfcs`.`tbl_menu_list` SET `parent_id` = '1621' WHERE `menu_pid` = '1520'; 
