ffc39a6614657b41f55526b341fe569d837b7630
rajud <rajud@schemaxtech.com>
2018-12-28 11:36:42 +0530

    #1298 sql script correction

--- deploy/migrations/v147__update_old_records.sql
+++ deploy/migrations/v147__update_old_records.sql
@@ -1,4 +1,4 @@
 /*#1298*/
-UPDATE bai_pro3.m3_transactions SET api_status='fg' WHERE op_code=200;
+UPDATE bai_pro3.m3_transactions SET api_type='fg' WHERE op_code=200;
 
 INSERT INTO bai_pro3.m3_transactions(date_time, mo_no, quantity, reason, remarks, log_user, tran_status_code, module_no, shift, op_code, op_des, ref_no, workstation_id, response_status, m3_ops_code, m3_trail_count, api_type) SELECT date_time, mo_no, quantity, reason, remarks, log_user, tran_status_code, module_no, shift, op_code, op_des, ref_no, workstation_id, 'fail', m3_ops_code, m3_trail_count, 'opn' FROM bai_pro3.m3_transactions WHERE op_code=200;
