8f6458ec7df9092174dc213f83b52ac6828b2917
Ramesh Kalla <rameshk@schemaxtech.com>
2018-12-31 10:01:41 +0530

    #1336 Corrected Excepted Section Reset Concern

--- sfcs_app/app/production/reports/daily_sah_report_V5.php
+++ sfcs_app/app/production/reports/daily_sah_report_V5.php
@@ -1213,7 +1213,7 @@ for (var i = 0; i < value.length; i++) {
     </div>
     <div class="col-md-2">
         <label>Excepted Sections</label>
-        <input type="text" name="exp_sec" id="exp_sec"  class="form-control" value="0" size="6" onkeyup="abc(this.value)" onblur="abc(this.value)" />
+        <input type="text" name="exp_sec" id="exp_sec"  class="form-control" value="<?php  if(isset($_POST['exp_sec'])) { echo $_POST['exp_sec']; } else { echo '0'; } ?>" size="6" onkeyup="abc(this.value)" onblur="abc(this.value)" />
     </div>
     <input type="submit" value="Show" name="submit" id="submit" onclick="return verify_date();"  class="btn btn-info" style="margin-top:22px;">
     <a href="<?= getFullURL($_GET['r'],'daily_sah_report_v6.php','N'); ?>" class="btn btn-warning" style="margin-top:22px;">Buyer Wise SAH Report</a>
