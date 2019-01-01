50deaa707cfab797946c7b5fbc4c4b02e2b69ee8
SRINIVASA RAO YADAVALLI <ysrinivas@schemaxtech.com>
2019-01-01 16:39:59 +0530

    #1349

--- sfcs_app/common/config/m3Updations.php
+++ sfcs_app/common/config/m3Updations.php
@@ -412,8 +412,8 @@
                         {
                             $is_m3 = $row['default_operration'];
                         }
-                        if(strtolower($is_m3) == 'yes')
-                        {
+                        // if(strtolower($is_m3) == 'yes')
+                        // {
                             $inserting_into_m3_tran_log = "INSERT INTO $bai_pro3.`m3_transactions` (`date_time`,`mo_no`,`quantity`,`reason`,`remarks`,`log_user`,`tran_status_code`,`module_no`,`shift`,`op_code`,`op_des`,`ref_no`,`workstation_id`,`response_status`,`m3_ops_code`,`api_type`) 
                             VALUES ('$current_date','$mo_number',$to_update_qty,'$r_reasons[$key]','Normal','$username','',$b_module,'$b_shift',$op_code,'',$id,'$work_station_id','','$main_ops_code','opn')";
                             mysqli_query($link,$inserting_into_m3_tran_log) or exit("While inserting into the m3_transactions".mysqli_error($GLOBALS["___mysqli_ston"]));
@@ -449,7 +449,7 @@
                                 $qry_transactionslog="INSERT INTO $brandix_bts.`transactions_log` (`transaction_id`,`response_message`,`created_by`,`created_at`,`updated_at`) VALUES ('$insert_id','$message','$username','$current_date')"; 
                                 mysqli_query($link,$qry_transactionslog) or exit("While inserting into M3 transaction log".mysqli_error($GLOBALS["___mysqli_ston"]));
                             }
-                        }
+                       // }
                     }
                 }
             }
