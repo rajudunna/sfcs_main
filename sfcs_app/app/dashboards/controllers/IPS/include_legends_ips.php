109865384ad0f11c1f2bf95035545ffc0def4dfd
unknown <you@example.com>
2019-01-02 15:26:08 +0530

    #1354 commit

--- sfcs_app/app/dashboards/controllers/IPS/include_legends_ips.php
+++ sfcs_app/app/dashboards/controllers/IPS/include_legends_ips.php
@@ -26,6 +26,8 @@
 <div style="width:170px;height:45px;margin-top:5px;border:1px solid #FFF;float:left;margin-left:5px;padding:3px;">
 <div class="pink" ></div> Ready To Issue
 </div>
-
+<div style="width:170px;height:45px;margin-top:5px;border:1px solid #FFF;float:left;margin-left:5px;padding:3px;">
+<div class="orange" ></div> Input Partially Reported
+</div>
 <div style="clear: both;"> </div>
 </div>
