e72dda2c01fcd4cd0e4dc4b45b835384326b001b
unknown <you@example.com>
2019-01-02 15:55:27 +0530

    #1354 color legend

--- sfcs_app/app/dashboards/controllers/IPS/include_legends_ips.php
+++ sfcs_app/app/dashboards/controllers/IPS/include_legends_ips.php
@@ -24,11 +24,12 @@
 <div class="white" ></div> Empty Slot
 </div>
 <div style="width:170px;height:45px;margin-top:5px;border:1px solid #FFF;float:left;margin-left:5px;padding:3px;">
-<div class="pink" ></div> Ready To Issue
+<div class="pink" ></div> Ready In Warehouse
 </div>
 <div style="width:170px;height:45px;margin-top:5px;border:1px solid #FFF;float:left;margin-left:5px;padding:3px;">
 <div class="orange" ></div> Input Partially Reported
 </div>
+
 
 
 
