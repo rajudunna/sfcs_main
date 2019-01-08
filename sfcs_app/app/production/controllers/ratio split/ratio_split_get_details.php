ab369a57317f5a428303a5cefb4d28c71cc8efce
rajesh raj <rajesh.nalam@schemaxtech.com>
2019-01-08 12:43:33 +0530

    #1323 interfaces comitted

--- sfcs_app/app/production/controllers/ratio split/ratio_split_get_details.php
+++ sfcs_app/app/production/controllers/ratio split/ratio_split_get_details.php
@@ -1,26 +1,27 @@
-
-
 <?php
+include($_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/config/config_ajax.php');
 
 if(isset($_GET['fetch'])){
-    $doc = $_GET['doc_no'];
+    $doc_no = $_GET['doc_no'];
 
-    if($doc > 0){
+    if($doc_no > 0){
         //verifying for valid docket or not
-        $doc_query = "SELECT do_no from $bai_pro3.packing_summary_input where doc_no = $doc limit 1";
+        $doc_query = "SELECT doc_no from $bai_pro3.packing_summary_input where doc_no = $doc_no limit 1";
         if(mysqli_num_rows(mysqli_query($link,$doc_query)) > 0){
-            $doc_details_query = "SELECT a_plies from $bai_pro3.plandoc_stat_log psl
+            $doc_details_query = "SELECT a_plies,order_style_no,order_del_no,order_col_des from $bai_pro3.plandoc_stat_log psl
                 left join $bai_pro3.bai_orders_db bd ON bd.order_tid = psl.order_tid
-                where doc_no = $doc_no";
+                where doc_no = $doc_no";  
             $row = mysqli_fetch_array(mysqli_query($link,$doc_details_query));
 
             $a_plies  = $row['a_plies'];
             $style    = $row['order_style_no'];
             $schedule = $row['order_del_no'];
-
-            $response_data['a_plies']  = $a_plies;
+            $color    = $row['order_col_des'];
+            
+            $response_data['plies']  = $a_plies;
             $response_data['style']    = $style;
             $response_data['schedule'] = $schedule;
+            $response_data['color']    = $color;
         }else{
             $response_data['found'] = 0;
         }
--- sfcs_app/app/production/controllers/ratio split/ratio_split_interface.php
+++ sfcs_app/app/production/controllers/ratio split/ratio_split_interface.php
@@ -1,21 +1,44 @@
-
+<?php
+    $get_url = getFullURLLevel($_GET['r'],'ratio_split_get_details.php','R');
+?>
 <div class='col-sm-12'>
     <div class='panel panel-primary'>
         <div class='panel-heading'>
             <b>Ratio Splitting </b>
         </div>
         <div class='panel-body'>
-            <div class='col-sm-12'>
-                <label>Enter Docket No : </label><br/>
-                <input name='docket' type='text' class='form-control integer' onchange='load_details(this)'>
-                <hr/>
-            </div>
-            <div class='col-sm-12' id='details_block' style='display : none'>
-                <div class='col-sm-4' style='font-size : 12px'>
-                    <label class='label label-warning'>Docket  : </label><span id='d_doc_no'></span>
+            <div class='row'>
+                <div class='col-sm-2'>
+                    <label>Enter Docket No : </label>
+                    <input name='docket' type='text' class='form-control integer' onchange='load_details(this)'>
                 </div>
-                <div class='col-sm-4'>
-                    <label class='label label-warning'>Plies  : </label><span id='d_org_plies'></span>
+            </div>
+            <div class='row' id='details_block' style='display : none'>
+            <hr/>
+                <div class='col-sm-12' style='font-size : 12px'>
+                    <table class='table table-bordered'> 
+                        <tr class='info'><th>Style</th><th>Schedule</th><th>Color</th>
+                            <th>Doc No</th><th>Actual Plies</th>
+                        </tr>
+                        <tr>
+                            <td id='d_style'></td>
+                            <td id='d_schedule'></td>
+                            <td id='d_color'></td>
+                            <td id='d_doc_no'></td>
+                            <td id='d_plies'></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class='col-sm-12'>
+                    <div class='col-sm-2'>
+                        <label>No of Shades</label>
+                        <input name='shades' type='text' class='form-control' placeholder='shades' onchange='load_shades(this)'>
+                    </div>
+                </div>
+                <div class='col-sm-3'>
+                    <br/>
+                    <table id='shades_table' class='table table-bordered'>
+                    </table>
                 </div>
             </div>
         </div>
@@ -25,35 +48,66 @@
 
 
 <script>
-function load_details(t){
-    clear_data();
-    var doc = Number(t.value);
-    if(doc > 0){
-        $.ajax({
-            url  : '<?= $get_url ?>?fetch=1&doc_no='+doc,
-            type : 'GET',
-            data : form_data
-        }).done(function(res){
-            var data = $.parseJSON(res);
-            $('#details_block').show();
-            $('#d_doc_no').html(doc);
-            $('#d_org_plies').html(data.org_plies);
-        }).fail(function(res){
-            swal('Error Getting Docket','','error');
-            t.value = '';
-        });
+    var count = 0;
+    var a_plies = 0;
+    function load_shades(t){
+        $('#shades_table').html('');
+        var shades = Number(t.value);
+        count = shades; 
+        $('#shades_table').append("<tr class='danger'><td>Shade</td><td>Plies</td></tr>");
+        while(shades-- > 0){
+            $('#shades_table').append("<tr>\
+            <td><input id='shade_"+shades+"' type='text' class='form-control required shades'></td>\
+            <td><input id='plies_"+shades+"' type='text' class='form-control integer plies required'></td>\
+            </tr>");
+        }
+        $('#shades_table').append("<tr><td colspan=2>\
+        <input name='submit' type='submit' class='btn btn-warning btn-sm' value='Submit' onclick='submit_data()'></td>\
+        </tr>");
     }
-}
 
-function clear_data(){
-    $('#details_block').hide();
-    $('#d_doc_no').html('');
-    $('#d_org_plies').html('');
-}
+    function submit_data(){
+        $('.shades').each(function(s){
+            console.log(s.value);
+        });
+
+    }
+
+    function load_details(t){
+        clear_data();
+        var doc = Number(t.value);
+        if(doc > 0){
+            $.ajax({
+                url  : '<?= $get_url ?>?fetch=1&doc_no='+doc,
+                type : 'GET'
+            }).done(function(res){
+                try{
+                    var data = $.parseJSON(res);
+                }catch(e){
+                    return swal('Problem incurred while Fetching Details','','error');
+                }
+
+                if(data.found == '0')
+                    return swal('Sewing Jobs Do Not Exist for the Docket',doc,'error');
+
+                a_plies = data.plies;  
+                $('#details_block').show();
+                $('#d_style').html(data.style);
+                $('#d_schedule').html(data.schedule);
+                $('#d_color').html(data.color);
+                $('#d_doc_no').html(doc);
+                $('#d_plies').html(data.plies);
+               
+            }).fail(function(res){
+                swal('Error Getting Docket','','error');
+                t.value = '';
+            });
+        }
+    }
+
+    function clear_data(){
+        $('#details_block').hide();
+        $('#d_doc_no').html('');
+        $('#d_org_plies').html('');
+    }
 </script>
-
-<?php
-
-
-
-?>
