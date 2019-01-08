d76082e1667e24530c3e30f87f90689df2b00f31
SRINIVASA RAO YADAVALLI <ysrinivas@schemaxtech.com>
2019-01-08 11:28:20 +0530

    #1128

--- sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/scan_input_jobs.php
+++ sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/scan_input_jobs.php
@@ -407,7 +407,7 @@ $(document).ready(function()
 
 										if(data[i].send_qty != 0)
 										{
-											if(Number(data[i].reported_qty) == Number(data[i].send_qty)+Number(data[i].recut_in)+Number(data[i].replace_in))
+											if(Number(data[i].send_qty) == Number(data[i].reported_qty)+Number(data[i].recut_in)+Number(data[i].replace_in))
 											{
 												status = '<font color="red">Already Scanned</font>';
 											}
