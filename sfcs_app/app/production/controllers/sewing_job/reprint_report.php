33d143162423b8e0e1ad245612e79a4413792592
unknown <you@example.com>
2019-01-22 18:00:44 +0530

    #1384

--- sfcs_app/app/production/controllers/sewing_job/reprint_report.php
+++ sfcs_app/app/production/controllers/sewing_job/reprint_report.php
@@ -1,10 +1,11 @@
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<script type="text/javascript" src="../../../../common/js/jquery.min.js"></script>
-<script type="text/javascript" src="../../../../../common/js/datetimepicker_css.js"></script> 
-<link rel="stylesheet" type="text/css" href="../../../../../common/js/style.css">
-<link rel="stylesheet" type="text/css" href="../../../../../common/js/table.css">
+	<link rel="stylesheet" type="text/css" href="../../../../common/css/bootstrap.min.css">
+</head>
+ 
+<meta http-equiv="X-UA-Compatible" content="IE=8,IE=edge,chrome=1" /> 
+<script language="javascript" type="text/javascript" src="../../common/js/datetimepicker_css.js"></script> 
+<link rel="stylesheet" href="style.css" type="text/css" media="all" /> 
 <style type="text/css">
 #div-1a {
  position:absolute;
@@ -17,35 +18,6 @@ table {
     width:33%;
 }
 </style>
-<style type="text/css" media="screen">
-/*====================================================
-	- HTML Table Filter stylesheet
-=====================================================*/
-@import "TableFilter_EN/filtergrid.css";
-
-/*====================================================
-	- General html elements
-=====================================================*/
-body{ 
-	/* margin:15px; padding:15px; border:1px solid #666; */
-	font-family:Arial, Helvetica, sans-serif; font-size:88%; 
-}
-h2{ margin-top: 50px; }
-caption{ margin:10px 0 0 5px; padding:10px; text-align:left; }
-pre{ font-size:13px;  padding:5px; background-color:#f4f4f4; solid #ccc;  }
-.mytable{
-	width:100%; font-size:12px;
-	}
-div.tools{ margin:5px; }
-div.tools input{ background-color:#f4f4f4; outset #f4f4f4; margin:2px; }
-.mytable th{ background-color:#29759c; color:#FFF; padding:2px; solid #ccc; white-space: nowrap;}
-td{ padding:2px; white-space: nowrap;}
-</style>
-<script language="javascript" type="text/javascript" src="TableFilter_EN/actb.js"></script><!-- External script -->
-<script language="javascript" type="text/javascript" src="TableFilter_EN/tablefilter.js"></script>
-
-
-
 <script language="javascript" type="text/javascript">
 
 
@@ -68,7 +40,7 @@ function check_val()
 </script>
 <link href="style.css" rel="stylesheet" type="text/css" />
 
-</head>
+
 
 <body>
 <div class="panel panel-primary">	
@@ -95,11 +67,11 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 <table><tr>
 <!-- <td>
 Start Date: <input id="demo1" onclick="javascript:NewCssCal('demo1','yyyymmdd','dropdown')" type="text" name="sdate" size="8" value="<?php //if(isset($_REQUEST['sdate'])) { echo $_REQUEST['sdate']; } else { echo date("Y-m-d"); } ?>"> End Date: <input id="demo2" onclick="javascript:NewCssCal('demo2','yyyymmdd','dropdown')" type="text" size="8" name="edate" value="<?php //if(isset($_REQUEST['edate'])) { echo $_REQUEST['edate']; } else { echo date("Y-m-d"); } ?>"></td> -->
-                 <td> <div class="col-md-5">
+                 <td> <div class="col-md-6">
 						 <b style='font-size:12px;'>Start Date:</b> <input id="demo1" class="form-control" onclick="javascript:NewCssCal('demo1','yyyymmdd','dropdown')" type="text" data-toggle='datepicker' size="8" name="sdate" value=<?php if(isset($_REQUEST['sdate'])) { echo $_REQUEST['sdate']; } else { echo date("Y-m-d"); } ?>>
 					</div>
-					<div class="col-md-5">
-						 <b style='font-size:12px;padding-left: 55px;'>End Date:</b>   <input id="demo2" class="form-control" onclick="javascript:NewCssCal('demo2','yyyymmdd','dropdown')" type="text" data-toggle='datepicker' size="8" name="edate" value=<?php if(isset($_REQUEST['edate'])) { echo $_REQUEST['edate']; } else { echo date("Y-m-d"); } ?>>
+					<div class="col-md-6">
+						 <b style='font-size:12px;padding-left:2px;'>End Date:</b>   <input id="demo2" class="form-control" onclick="javascript:NewCssCal('demo2','yyyymmdd','dropdown')" type="text" data-toggle='datepicker' size="8" name="edate" value=<?php if(isset($_REQUEST['edate'])) { echo $_REQUEST['edate']; } else { echo date("Y-m-d"); } ?>>
 					</div></td>
 <td style='padding-left:38px;'><input type="checkbox" name="check_list[]" value="1"><b style='font-size:12px;'>Module</b> <input type="checkbox" name="check_list[]" value="2"><b style='font-size:12px;'>Employee</b> <input type="checkbox" name="check_list[]" value="3"><b style='font-size:12px;'>Shift</b></td> 
 <td>
