17296f356d5ac02b7c96d8289c68fcee8fd4a3d8
SRINIVASA RAO YADAVALLI <ysrinivas@schemaxtech.com>
2019-01-24 10:13:30 +0530

    key to i

--- sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/scanning_functionality_ajax.php
+++ sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/scanning_functionality_ajax.php
@@ -1279,7 +1279,7 @@ else if($concurrent_flag == 0)
 							$act_ims_input_qty = $row['ims_qty'];
 						}
 						$act_ims_qty = $pre_ims_qty + $b_rep_qty[$i];
-						$act_ims_qty = min($act_ims_qty,$b_in_job_qty[$key]);
+						$act_ims_qty = min($act_ims_qty,$b_in_job_qty[$i]);
 						//updating the ims_qty when it was there in ims_log
 						$update_query = "update $bai_pro3.ims_log_backup set ims_qty = $act_ims_qty where tid = $updatable_id";
 						$ims_pro_qty_updating = mysqli_query($link,$update_query) or exit("While updating ims_pro_qty in ims_log_log_backup".mysqli_error($GLOBALS["___mysqli_ston"]));
@@ -1307,7 +1307,7 @@ else if($concurrent_flag == 0)
 								$pre_ims_qty = $row['ims_qty'];
 							}
 							$act_ims_qty = $pre_ims_qty + $b_rep_qty[$i] ;
-							$act_ims_qty = min($act_ims_qty,$b_in_job_qty[$key]);
+							$act_ims_qty = min($act_ims_qty,$b_in_job_qty[$i]);
 							//updating the ims_qty when it was there in ims_log
 							$update_query = "update $bai_pro3.ims_log set ims_qty = $act_ims_qty where tid = $updatable_id";
 							mysqli_query($link,$update_query) or exit("While updating ims_qty in ims_log".mysqli_error($GLOBALS["___mysqli_ston"]));
