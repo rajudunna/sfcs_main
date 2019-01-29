be1653ae74bee8d19f8693dab8c8bf8e767d928a
ThejaB <theja@schemaxtech.com>
2019-01-29 16:31:41 +0530

    #1434 module master <<head issues solved

--- sfcs_app/app/masters/modules/view_modules.php
+++ sfcs_app/app/masters/modules/view_modules.php
@@ -26,11 +26,7 @@
 		<th>Module Description</th>		
 		<th>Mapped Cut Table</th>
         <th>Module Color</th>
-<<<<<<< HEAD
-        <th>Module Label</th>
-=======
 		<th>Module Label</th>
->>>>>>> 1461-unable-to-see-the-color-club-dockets-through-plot-job-priority-interface
 		<th>Mini Plant</th>
 		<th>Status</th>
 		<th> Edit / Delete </th>
@@ -78,15 +74,9 @@
             <td>".$row["module_description"]."</td>
             <td>".$mapped_cut_table."</td>
             <td>".$row["color"]."</td>
-<<<<<<< HEAD
-			<td>".$row["label"]."</td>
-			<td>".$mini_plant_name." </td>
-			<td>".$row["status"]." </td>			
-=======
             <td>".$row["label"]."</td>
 			<td>".$mini_plant_name."</td>
 			<td>".$row["status"]." </td>
->>>>>>> 1461-unable-to-see-the-color-club-dockets-through-plot-job-priority-interface
 			<td><a href='$url&rowid=$rowid&module_name=$module_name&section=$section&status=$status&module_description=$module_description&block_priorities=$block_priorities&mini_plant=$mini_plant&mapped_cut_table=$mapped_cut_table&module_color=$color&module_label=$label' class='btn btn-warning btn-xs editor_edit'>Edit</a> / <a href='$url1&rowid1=$rowid&module_name=$module_name&section=$section' class='btn btn-danger btn-xs editor_remove' onclick='return confirm_delete(event,this);'>Delete</a></td>
 			</tr>";
 		}
