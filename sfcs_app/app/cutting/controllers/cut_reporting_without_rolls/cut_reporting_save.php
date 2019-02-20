76d91f606dc08e5c10eff6fb4121daaa8d7fcd7d
rajesh raj <rajesh.nalam@schemaxtech.com>
2019-02-20 16:06:05 +0530

    #1562 transactions removed and comitted

--- sfcs_app/app/cutting/controllers/cut_reporting_without_rolls/cut_reporting_save.php
+++ sfcs_app/app/cutting/controllers/cut_reporting_without_rolls/cut_reporting_save.php
@@ -581,7 +581,6 @@ if($target == 'style_clubbed'){
     $child_docs_result = mysqli_query($link,$child_docs_query);
     while($row = mysqli_fetch_array($child_docs_result)){
         $child_docs[] = $row['doc_no'];
-
     }
     //getting the no of schedules clubbed for the style for equal filling logic
     $child_schedules_query = "SELECT doc_no from $bai_pro3.plandoc_stat_log psl  
@@ -593,13 +592,28 @@ if($target == 'style_clubbed'){
     }
 
     //getting size wise qty of parent docket
-    $doc_qty_query = "SELECT $p_sizes_str,doc_no from $bai_pro3.plandoc_stat_log where doc_no = '$doc_no' 
-                    order by acutno ASC";
-    $doc_qty_result = mysqli_query($link,$doc_qty_query);
-    while($row = mysqli_fetch_array($doc_qty_result)){
-        foreach($sizes_array as $size){
-            if($row['p_'.$size] > 0)
-                $reporting[$size] = ($row['p_'.$size] * $plies);
+    if($plies == $p_plies){
+        $doc_qty_query = "SELECT $p_sizes_str,doc_no,a_plies from $bai_pro3.plandoc_stat_log where org_doc_no = '$doc_no' 
+        order by acutno ASC";
+        $doc_qty_result = mysqli_query($link,$doc_qty_query);
+        while($row = mysqli_fetch_array($doc_qty_result)){
+            $doc = $row['doc_no'];
+            $a_plies = $row['a_plies'];
+            foreach($sizes_array as $size){
+                if($row['p_'.$size] > 0)
+                    $reported[$doc][$size] = ($row['p_'.$size] * $a_plies);
+            }
+        }
+        goto full_plies;
+    }else{
+        $doc_qty_query = "SELECT $p_sizes_str,doc_no from $bai_pro3.plandoc_stat_log where doc_no = '$doc_no' 
+                        order by acutno ASC";
+        $doc_qty_result = mysqli_query($link,$doc_qty_query);
+        while($row = mysqli_fetch_array($doc_qty_result)){
+            foreach($sizes_array as $size){
+                if($row['p_'.$size] > 0)
+                    $reporting[$size] = ($row['p_'.$size] * $plies);
+            }
         }
     }
 
@@ -665,6 +679,7 @@ if($target == 'style_clubbed'){
         $cum_reported = 0;
         $quit_counter = 0;
         do{
+            $left_over[$size] = 0;
             if($quit_counter++ > $THRESHOLD){
                 $response_data['pass'] = 0;
                 force_exit('Threshold Exceeded');
@@ -718,7 +733,7 @@ if($target == 'style_clubbed'){
     //ALL Excess Qty left out to be filled equally 
     foreach($left_over as $size=>$qty){
         if($qty > 0){
-            $docs = $docs_count[$size];
+            $docs = $dockets[$size];
             $splitted = $qty;
             $quit_counter = 0;
             if($qty > $docs){
@@ -749,6 +764,8 @@ if($target == 'style_clubbed'){
             }
         }
     }
+
+    full_plies : 
     //Array Cloning reported into reported2
     foreach($reported as $doc => $size_wise){
         foreach($size_wise as $size => $qty){
