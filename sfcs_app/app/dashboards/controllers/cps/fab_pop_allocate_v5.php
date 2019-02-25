890677a2210d43456c84a9298bada7a12888cec8
ram G <ramkumar@schemaxtech.com>
2019-02-25 11:41:14 +0530

    Git #1581 Issue roll allocation locks

--- sfcs_app/app/dashboards/controllers/cps/fab_pop_allocate_v5.php
+++ sfcs_app/app/dashboards/controllers/cps/fab_pop_allocate_v5.php
@@ -1138,7 +1138,7 @@ if(isset($_POST['allocate']))
 			echo "<td>".$sql_row['ref3']."</td>";
 			echo "<td>".$sql_row['qty_rec']."</td>";
 			echo "<td>".$sql_row['ref5']."</td>";
-			echo "<td>".($sql_row['qty_rec']-$sql_row['qty_issued']+$sql_row['qty_ret'])."</td>";
+			echo "<td>".($sql_row['qty_rec']-$sql_row['qty_issued']+$sql_row['qty_ret']-$sql_row['qty_allocated'])."</td>";
 			echo "<td><input class='form-control float' name=\"issued_new".$doc_ref."[$j]\" type = 'number' min='0' step='any' id=\"issued".$doc_ref."[$j]\" value = '0' onchange='filling($doc_ref,$j,$i);' readonly></input></td>";
 			
 			//echo "</br>Allotment Status".$sql_row['allotment_status']."</br>";
