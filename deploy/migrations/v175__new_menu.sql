d798438eefe520d2f8483dbe8d2078f760806694
rajud <rajud@schemaxtech.com>
2019-03-08 14:17:30 +0530

    #1612 remark corrected in script

--- deploy/migrations/v175__new_menu.sql
+++ deploy/migrations/v175__new_menu.sql
@@ -8,5 +8,5 @@ insert into rbac_role_menu (menu_pid, menu_description, roll_id) values('1648','
 
 INSERT INTO central_administration_sfcs.tbl_menu_list (menu_pid, page_id, fk_group_id, fk_app_id, parent_id, link_type, link_status, link_visibility, link_location, link_description, link_tool_tip, link_cmd) VALUES ('1649','SFCS_00001', '8', '8', '109', '1', '1', '1', '/sfcs_app/app/cutting/controllers/check_list/bundle_check_list.php', 'Bundle Check List', '3', '1'); 
 
-INSERT INTO central_administration_sfcs.rbac_role_menu (menu_pid, menu_description, roll_id) VALUES('1649','MRN Integration','1');
+INSERT INTO central_administration_sfcs.rbac_role_menu (menu_pid, menu_description, roll_id) VALUES('1649','Bundle Check List','1');
 
