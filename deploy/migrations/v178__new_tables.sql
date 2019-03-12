8c6dcb1b544a9c0fc0dcfa4b75c1f94a53ab63b7
rajud <rajud@schemaxtech.com>
2019-03-12 11:18:08 +0530

    #1323 new tables script missed

--- deploy/migrations/v178__new_tables.sql
+++ deploy/migrations/v178__new_tables.sql
@@ -1,23 +1,24 @@
 /*#1323 new tables used in code*/
-create table `bai_pro3`.`shade_split`( 
-   `id` int(10) NOT NULL AUTO_INCREMENT , 
-   `date_time` datetime , 
-   `username` varchar(30) , 
-   `doc_no` int(10) NOT NULL , 
-   `schedule` int(10) NOT NULL , 
-   `shades` varchar(100) NOT NULL , 
-   `plies` varchar(100) NOT NULL , 
-   PRIMARY KEY (`id`)
- );
 
+CREATE TABLE `bai_pro3`.`shade_split` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `date_time` datetime DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `doc_no` int(10) NOT NULL,
+  `schedule` int(10) NOT NULL,
+  `shades` varchar(100) NOT NULL,
+  `plies` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
 
- create table `bai_pro3`.`deleted_sewing_jobs`( 
-   `id` int NOT NULL , 
-   `schedule` int(10) NOT NULL , 
-   `input_job_no_random` varchar(20) NOT NULL , 
-   `size_id` varchar(10) NOT NULL , 
-   `qty` int(5) , 
-   `tid` int(10) , 
-   `type_of_sewing` int(2) , 
-   PRIMARY KEY (`id`)
- );
+CREATE TABLE `bai_pro3`.`deleted_sewing_jobs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `schedule` int(10) NOT NULL,
+  `input_job_no_random` varchar(20) NOT NULL,
+  `size_id` varchar(10) NOT NULL,
+  `qty` int(5) DEFAULT NULL,
+  `tid` int(10) DEFAULT NULL,
+  `type_of_sewing` int(2) DEFAULT NULL,
+  `date_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
