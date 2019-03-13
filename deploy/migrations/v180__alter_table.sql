bf074ff3d987f0a8eefc850e2c216c4664e85157
ravindra35 <36989030+ravindra35@users.noreply.github.com>
2019-03-13 17:49:22 +0530

    #1708 query updated

--- deploy/migrations/v180__alter_table.sql
+++ deploy/migrations/v180__alter_table.sql
@@ -1,2 +1,4 @@
 /*#1708 script*/
-ALTER TABLE bai_pro3.deleted_sewing_jobs ADD COLUMN date_time DATE NULL AFTER type_of_sewing;
+/*
+//this is covered under v181
+ALTER TABLE bai_pro3.deleted_sewing_jobs ADD COLUMN date_time DATE NULL AFTER type_of_sewing;*/
