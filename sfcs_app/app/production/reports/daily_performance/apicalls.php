32e5b349c9bc4ce201b8465cd889876336cc84c6
satishkalla <satishkalla@schemaxtech.com>
2019-03-13 16:35:46 +0530

    #1505 filtering total changes

--- sfcs_app/app/production/reports/daily_performance/apicalls.php
+++ sfcs_app/app/production/reports/daily_performance/apicalls.php
@@ -32,58 +32,23 @@ function getData($date){
 
 	if(count($operation_code)>0){
 		foreach ($operation_code as $key => $value) {	
-			if($value['def_op'] == 'yes'){
-				//columns
-				$get_operations_yes= "select DISTINCT(op_code) from $bai_pro3.m3_transactions where date(date_time) = '$date' and op_code ='".$value['op_code']."'";
-				$result2 = $link->query($get_operations_yes);
-				$op_count = mysqli_num_rows($result2);
-				if($op_count > 0){
-					while($row2 = $result2->fetch_assoc()){
-						$over_all_operations[] = $row2['op_code'];
-						$operations_yes[] = $row2['op_code'];
-					}					
-				}
-			}
-			
-			if($value['def_op'] == 'No'){
-				//columns
-				$get_operations_no= "select DISTINCT(operation_id) from $brandix_bts.bundle_creation_data_temp where date(scanned_date) = '$date' and operation_id ='".$value['op_code']."'";				
-				$result4 = $link->query($get_operations_no);
-				$op_count = mysqli_num_rows($result4);
-				if($op_count){
-					while($row3 = $result4->fetch_assoc()){
-						$over_all_operations[] = $row3['operation_id'];
-						$operations_no[] = $row3['operation_id'];
-					}
+			//columns
+			$get_operations_no= "select DISTINCT(operation_id) from $brandix_bts.bundle_creation_data_temp where date(scanned_date) = '$date' and operation_id ='".$value['op_code']."'";				
+			$result4 = $link->query($get_operations_no);
+			$op_count = mysqli_num_rows($result4);
+			if($op_count){
+				while($row3 = $result4->fetch_assoc()){
+					$over_all_operations[] = $row3['operation_id'];
+					$operations_no[] = $row3['operation_id'];
 				}
 			}
 		}
 	}
 
-	// if(count($operations_yes)>0){
-	// 	$operation_codes_yes = implode(',',$operations_yes);
-	// 	//columns Data
-	// 	$get_data_m3tran= "SELECT mo.style as style,mo.schedule as schedule,mo.color as color,mo.size as size_title,tran.quantity as recevied_qty,tran.op_code as op_code FROM
-	// 	$bai_pro3.m3_transactions AS tran LEFT JOIN $bai_pro3.mo_details AS mo ON tran.`mo_no` = mo.`mo_no`
-	// 	WHERE DATE(tran.date_time) = '$date' AND tran.op_code in ($operation_codes_yes) GROUP BY mo.`style`,mo.`schedule`,mo.`color`,mo.`size`,tran.op_code";	
-	// 	$result3 = $link->query($get_data_m3tran);
-	// 	$op_count1 = mysqli_num_rows($result3);
-	// 	if($op_count1>0){
-	// 		while($row4 = $result3->fetch_assoc()){
-	// 			if($row4['recevied_qty']){
-	// 				$rec_qty = (int)$row4['recevied_qty'];
-	// 				$data =  ['style'=>$row4['style'],'schedule'=>$row4['schedule'],'color'=>$row4['color'],'size'=>$row4['size_title'],$row4['op_code']=>$rec_qty,'op_code'=>$row4['op_code']];	
-	// 				array_push($total_data,$data);
-	// 			}
-				
-	// 		}
-	// 	}
-	// }
-
 	if(count($over_all_operations)>0){
 		$operation_codes_no = implode(',',$over_all_operations);
 		//columns Data
-		$get_data_bcd_temp= "SELECT style,SCHEDULE,color,size_title,recevied_qty,operation_id as op_code FROM brandix_bts.`bundle_creation_data_temp` WHERE DATE(scanned_date) = '$date' AND operation_id in ($operation_codes_no) GROUP BY style,SCHEDULE,color,size_title,operation_id";
+		$get_data_bcd_temp= "SELECT style,SCHEDULE,color,size_title,sum(recevied_qty) as recevied_qty,operation_id as op_code FROM brandix_bts.`bundle_creation_data_temp` WHERE DATE(scanned_date) = '$date' AND operation_id in ($operation_codes_no) GROUP BY style,SCHEDULE,color,size_title,operation_id";
 		$result5 = $link->query($get_data_bcd_temp);
 		$op_count1 = mysqli_num_rows($result5);
 		if($op_count1>0){
--- sfcs_app/app/production/reports/daily_performance/main.js
+++ sfcs_app/app/production/reports/daily_performance/main.js
@@ -52,15 +52,14 @@ class ReactApp extends React.Component {
                         columnsData.push(val);
                     });            
                 }
-                this.setState({ tableData: response.data.data,columnsData:columnsData,loadingimage:false});
-            }else{
-                swal('Warning','No Records found for the selected date','warning'); 
-                this.setState({ tableData:[],columnsData:[],loadingimage:false});
+                this.setState({ tableData: response.data.data,columnsData:columnsData,loadingimage:false,mainData:response.data.data,tempData:[]});
+            }else{ 
+                this.setState({ tableData:[],columnsData:[],loadingimage:false,mainData:[],tempData:[]});
             }
             
         }) .catch((error) => {
             swal('Error','Some thing went wrong, Please try again '+error,'error');     
-            this.setState({ tableData: [],columnsData:[],loadingimage:false});
+            this.setState({ tableData: [],columnsData:[],loadingimage:false,mainData:[],tempData:[]});
         }); 
     }
   
@@ -75,7 +74,7 @@ class ReactApp extends React.Component {
                 element._subRows.forEach(elem1=>{
                     elem1._subRows.forEach(elem2=>{
                         elem2._subRows.forEach(function(elem3){
-                            elem3._subRows.forEach(elem4=>{                                                                                                     
+                            elem3._subRows.forEach(elem4=>{
                                 data.push(elem4);
                             });                            
                         })
@@ -135,7 +134,7 @@ class ReactApp extends React.Component {
     render() {
         const tableData = this.state.tableData;        
         const dynamicColumns = this.state.columnsData;  
-        const loadingimage = this.state.loadingimage;    
+        const loadingimage = this.state.loadingimage;   
 
         let colHeadData = [];
         let coldata = {
@@ -222,6 +221,86 @@ class ReactApp extends React.Component {
                                 sortable= {true}
                                 multiSort= {true}
                                 resizable= {true}
+                                onFilteredChange={(filtered, column) => {  
+                                    if(filtered.length >0){
+                                        filtered.forEach(data =>{
+                                            if(data.id == 'style'){
+                                                if(column.Header == "Style"){             
+                                                    if(this.state.tempData.length > 0){
+                                                        for (var i=0; i < this.state.tempData.length; i++) {
+                                                            if (this.state.tempData[i].style.trim()  === data.value) {
+                                                                this.state.tempData.push(this.state.tempData[i]);
+                                                            }
+                                                        }
+                                                    }else{
+                                                        for (var i=0; i < this.state.mainData.length; i++) {
+                                                            if (this.state.mainData[i].style.trim()  === data.value) {
+                                                                this.state.tempData.push(this.state.mainData[i]);
+                                                            }
+                                                        }
+                                                    }                                                    
+                                                }
+                                            }
+                                            
+                                            if(data.id == 'schedule'){
+                                                if(column.Header == "Schedule"){
+                                                    if(this.state.tempData.length > 0){
+                                                        for (var i=0; i < this.state.tempData.length; i++) {
+                                                            if (this.state.tempData[i].schedule === data.value) {
+                                                                this.state.tempData.push(this.state.tempData[i]);
+                                                            }
+                                                        }
+                                                    }else{
+                                                        for (var i=0; i < this.state.mainData.length; i++) {
+                                                            if (this.state.mainData[i].schedule === data.value) {
+                                                                this.state.tempData.push(this.state.mainData[i]);
+                                                            }
+                                                        }
+                                                    }                                                    
+                                                }
+                                            }
+    
+                                            if(data.id == 'color'){
+                                                if(column.Header == "Color"){
+                                                    if(this.state.tempData.length > 0){
+                                                        for (var i=0; i < this.state.tempData.length; i++) {
+                                                            if (this.state.tempData[i].color.trim()  === data.value ) {
+                                                                this.state.tempData.push(this.state.tempData[i]);
+                                                            }
+                                                        }
+                                                    }else{
+                                                        for (var i=0; i < this.state.mainData.length; i++) {
+                                                            if (this.state.mainData[i].color.trim()  === data.value ) {
+                                                                this.state.tempData.push(this.state.mainData[i]);
+                                                            }
+                                                        }
+                                                    }                                                    
+                                                }
+                                            }
+        
+                                            if(data.id == 'size'){
+                                                if(column.Header == "Size"){
+                                                    if(this.state.tempData.length > 0){
+                                                        for (var i=0; i < this.state.tempData.length; i++) {
+                                                            if (this.state.tempData[i].size.trim()  === data.value ) {
+                                                                this.state.tempData.push(this.state.tempData[i]);
+                                                            }
+                                                        }
+                                                    }else{
+                                                        for (var i=0; i < this.state.mainData.length; i++) {
+                                                            if (this.state.mainData[i].size.trim()  === data.value ) {
+                                                                this.state.tempData.push(this.state.mainData[i]);
+                                                            }
+                                                        }
+                                                    }                                                    
+                                                }
+                                            }
+                                        })
+                                        this.setState({ tableData: this.state.tempData});
+                                    }else{
+                                        this.setState({ tableData: this.state.mainData,tempData:[]});
+                                    }                                                                        
+                                }}
                             />
                         </div>
                     </div>
