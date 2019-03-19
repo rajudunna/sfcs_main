654e2c4467298c9a1917412a14f176e26daa32f8
satishkalla <satishkalla@schemaxtech.com>
2019-03-19 17:51:49 +0530

    #1161 sewing jobs listing issue cleared

--- sfcs_app/app/production/controllers/sewing_job/input_job_mix_ch_report.php
+++ sfcs_app/app/production/controllers/sewing_job/input_job_mix_ch_report.php
@@ -92,26 +92,10 @@
                                         $sql="SELECT type_of_sewing,input_job_no_random,sch_mix,input_job_no,GROUP_CONCAT(DISTINCT tid ORDER BY tid) AS tid,GROUP_CONCAT(DISTINCT doc_no_ref ORDER BY doc_no) AS doc_no_ref,GROUP_CONCAT(DISTINCT m3_size_code order by m3_size_code) AS size_code,group_concat(distinct order_col_des order by order_col_des) as order_col_des,doc_no,group_concat(distinct order_del_no) as order_del_no,GROUP_CONCAT(DISTINCT CONCAT(order_col_des,'$',acutno) ORDER BY doc_no SEPARATOR ',') AS acutno,SUM(carton_act_qty) AS carton_act_qty FROM (SELECT DISTINCT(SUBSTRING_INDEX(order_joins,'J',-1)) AS sch_mix,order_del_no, input_job_no, input_job_no_random, tid, doc_no, doc_no_ref, m3_size_code, order_col_des, acutno, SUM(carton_act_qty) AS carton_act_qty, type_of_sewing FROM $bai_pro3.packing_summary_input WHERE pac_seq_no='$seq_no' and order_del_no in ($schedule) $exp_query GROUP BY order_col_des,order_del_no,input_job_no_random,acutno,m3_size_code order by field(order_del_no,$schedule),field(m3_size_code,'s01','s02','s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24','s25','s26','s27','s28','s29','s30','s31','s32','s33','s34','s35','s36','s37','s38','s39','s40','s41','s42','s43','s44','s45','s46','s47','s48','s49','s50')) AS t GROUP BY input_job_no_random ORDER BY order_col_des,acutno*1, input_job_no*1, field(m3_size_code,'s01','s02','s03','s04','s05','s06','s07','s08','s09','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24','s25','s26','s27','s28','s29','s30','s31','s32','s33','s34','s35','s36','s37','s38','s39','s40','s41','s42','s43','s44','s45','s46','s47','s48','s49','s50')";
                                         // echo $sql."<br>";
                                         $temp=0;
-                                        $temp+=$sql_row['carton_act_qty'];
-                                        $color=$sql_row['order_col_des'];
-                                        $bg_color = get_sewing_job_prefix("bg_color","$brandix_bts.tbl_sewing_job_prefix","$bai_pro3.packing_summary_input",$schedule,$color,$sql_row['input_job_no'],$link);
-                                        
-                                        $sql4="select order_tid from $bai_pro3.bai_orders_db where order_del_no=\"".$sql_row["sch_mix"]."\"";
-                                        $sql_result4=mysqli_query($link, $sql4) or exit("Sql Error44 $sql4".mysqli_error($GLOBALS["___mysqli_ston"]));
-                                        while($sql_row4=mysqli_fetch_array($sql_result4))
-                                        {
-                                            $order_tid=$sql_row4["order_tid"];
-                                        }
-                                        $total_cuts=explode(",",$sql_row['acutno']);
-                                        $cut_jobs_new='';
-                                        for($ii=0;$ii<sizeof($total_cuts);$ii++)
-                                        {
-                                            $arr = explode("$", $total_cuts[$ii], 2);;
-                                            // $col = $arr[0];
-                                            $sql4="select color_code from $bai_pro3.bai_orders_db_confirm where order_del_no=\"".$schedule."\" and order_col_des='".$arr[0]."'";
-                                            //echo $sql4."<br>";
-                                            $sql_result4=mysqli_query($link, $sql4) or exit("Sql Error44 $sql4".mysqli_error($GLOBALS["___mysqli_ston"]));
-                                            while($sql_row4=mysqli_fetch_array($sql_result4))
+                                        $job_no=0;
+                                        $color="";
+                                        $sql_result=mysqli_query($link, $sql) or exit("Sql Error88 = $sql".mysqli_error($GLOBALS["___mysqli_ston"]));
+                                        while($sql_row=mysqli_fetch_array($sql_result))
                                             {
                                                 $temp+=$sql_row['carton_act_qty'];
                                                 if($temp>$job_qty or $color!=$sql_row['order_col_des'] or in_array($sql_row['order_del_no'],$donotmix_sch_list))
@@ -203,7 +187,6 @@
                                                 }
                                                 echo"</tr>";
                                             }
-                                        }
                                    echo ' </table>
                             </form>
                 </div>
