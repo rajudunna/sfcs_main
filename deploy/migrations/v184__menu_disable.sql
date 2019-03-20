7345f2238089527a0e8444fb86b73c4261ec9323
rajud <rajud@schemaxtech.com>
2019-03-20 13:39:02 +0530

    #1735 script corrected

--- deploy/migrations/v184__menu_disable.sql
+++ deploy/migrations/v184__menu_disable.sql
@@ -1,3 +1,3 @@
 /*#1735 menu disable*/
-UPDATE central_administration_sfcs.tbl_menu_list SET link_visibilty = 0 WHERE menu_pid IN (202,175,512,1515);
+UPDATE central_administration_sfcs.tbl_menu_list SET link_visibility = 0 WHERE menu_pid IN (202,175,512,1515);
 
