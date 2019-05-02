e34353a468be12cf156b83a652d8b82d30cbb7c9
padma <padma.bellana@schemaxtech.com>
2019-05-02 19:02:46 +0530

    #1897 code resolved

--- sfcs_app/app/production/controllers/sewing_job/barcode_new_2_1.php
+++ sfcs_app/app/production/controllers/sewing_job/barcode_new_2_1.php
@@ -65,27 +65,7 @@
 
 			$color_code=echo_title("$bai_pro3.bai_orders_db_confirm","color_code","order_col_des='".$color."' and order_del_no",$schedule,$link);
 			//$display = get_sewing_job_prefix("prefix","$brandix_bts.tbl_sewing_job_prefix","$bai_pro3.packing_summary_input",$schedule,$color,$input_job,$link);
-			$display = get_sewing_job_prefix_inp("prefix","$brandix_bts.tbl_sewing_job_prefix",$input_job,$sewing_job_random_id,$link);
-			// $html.= '<div>
-						// <table>
-							// <tr rowspan=2>
-								// <td colspan=10><b>Stab Here:</b></td>
-								// <td colspan=2>
-									// <svg height="25" width="25">
-										// <circle cx="10" cy="10" r="8"  />
-									// </svg>
-								// </td>
-							// </tr>
-							// <br><br>
-							// <tr><td colspan=4><b>Style:</b>'.$barcode_rslt['order_style_no'].'</td><td><b>Schedule:</b>'.$schedule.'</td></tr>
-							// <tr><td colspan=4><b>Job Number:</b>'.$display.'</td><td><b>Size:</b>'.$barcode_rslt['size_code'].'</td></tr>
-							// <tr><td colspan=4><b>Barcode ID:</b>'.$barcode.'</td><td><b>Cut No:</b>'.chr($color_code).leading_zeros($cutno, 3).'</td></tr>
-							// <tr><td colspan=4><b>Color:</b>'.substr($barcode_rslt['order_col_des'],0,30).'</td></tr>
-						 // </table>
-					 // </div><br><br><br>';
-
 			$get_destination="select destination from bai_pro3.bai_orders_db where order_style_no='".$style."' and order_del_no='".$schedule."' and order_col_des='".$color."' ";
-
 			$destination_result=mysqli_query($link, $get_destination)  or exit("Sql Error1".mysqli_error($GLOBALS["___mysqli_ston"]));
 			while($dest_row = mysqli_fetch_array($destination_result))
             {
@@ -94,7 +74,8 @@
 			$display1 = get_sewing_job_prefix_inp("prefix","$brandix_bts.tbl_sewing_job_prefix",$input_job,$sewing_job_random_id,$link);
 			//A dummy sticker for each bundle
 			//echo $detailed_bundle_sticker.'-';
-			if((int)$detailed_bundle_sticker == 1){
+			if((int)$detailed_bundle_sticker == 1)
+			{
 				$html.= '<div>
 							<table width="98%">
 								<tr>
@@ -108,6 +89,9 @@
 								<tr>
 									<td colspan=2><b>Style:</b>'.$barcode_rslt['order_style_no'].'</td>
 									<td colspan=2><b>Schedule:</b>'.$schedule.'</td>
+								</tr>
+								<tr>
+									<td colspan=2><b>Color:</b>'.$color.'</td>
 								</tr>
 								<tr>
 									<td colspan=2><b>Barcode ID:</b>'.trim($barcode).'</td>
@@ -144,35 +128,34 @@
 				$html.= '<div>
 							<table width="98%" style="font-size:4px;">
 							   <tr>	
-							      <td colspan=6>'.str_replace(' ','',$barcode_rslt['order_style_no']).'/'.$schedule.'</td>
-							      <td rowspan="0" style="border: 1px solid black;	border-top-right-radius: 1px 1px; font-size:4px; text-align:center;">
-								    <p style= "font-size: 4px;font-weight: bold;">'.$seq_num.'</p>
-							</td>
-							
+							      <td colspan=6> '.str_replace(' ','',$barcode_rslt['order_style_no']).'/'.$schedule.'/'.substr(str_replace(' ','',$operations),0,18).' - '.$opscode.'</td>
+							      <td rowspan="0" style="border: 1px solid black;	border-top-right-radius: 1px 1px; font-size:4px; text-align:center;width:10%">
+								           <p style= "font-size: 4px;font-weight: bold;">'.$seq_num.'</p>
+							</td>							
 						   </tr>
-						   <tr>
-								<td colspan=3>'.str_replace(' ','',$barcode_rslt['order_style_no']).'/'.$schedule.'/'.substr(str_replace(' ','',$operations),0,18).' - '.$opscode.'</td>
-						  </tr>
+						   
+						    <tr>
+									<td colspan=4>'.$color.'</td>
+							</tr>	
 						   <tr>
 							  <td colspan=8>
 										<div>
 											<barcode code="'.$barcode.'-'.$opscode.'" type="C39"/ height="0.60" size="0.8" text="1">
-										</div><br>
+										</div><br/>
 									<center style="font-size:6px;">'.trim($barcode).'</td>
 							</tr>
-								<tr>
-									<td colspan=8>'.trim($barcode_rslt['size_code']).'/'.trim($destination);
+							<tr>
+									<td colspan=8>'.trim($barcode_rslt['size_code']).' / '.trim($destination);
 
 						if($shade != '')
-							$html.= "/<b>$shade</b></td>";	
+							$html.= " / <b>$shade</b></td>";	
 						else
 							$html.= "</td>";	
 						$html.='</tr> 
-							<tr>	
-									<td colspan=3><b></b>'.chr($color_code).leading_zeros($cutno,3).'/'.$display1.'/'.trim(str_pad($quantity,3,"0", STR_PAD_LEFT)).'</td>
-									
-							</tr>
-
+						<tr>	
+						<td colspan=><b>'.chr($color_code).leading_zeros($cutno,3).'</b> / '.$display1.' / '.trim(str_pad($quantity,3,"0", STR_PAD_LEFT)).'</td>
+						
+			           </tr>
 							</table>
 						</div><br><br><br><br><br>';
 				$update_bundle_print_status="UPDATE $bai_pro3.pac_stat_log_input_job SET bundle_print_status='1', bundle_print_time=now() WHERE tid='".$barcode."'";	
