cf8a91f22f25b3d606ae1fb837277b3d7f201fc9
ravindra35 <36989030+ravindra35@users.noreply.github.com>
2019-05-21 17:33:31 +0530

    #1984 reverted the script

--- deploy/migrations/v202__update_menu_link_name_1984.sql
+++
@@ -1,6 +0,0 @@
-/*#1984 menu link name update*/
-USE central_administration_sfcs;
-
-UPDATE central_administration_sfcs.tbl_menu_list SET link_description = 'Hourly Efficiency Report 02' WHERE menu_pid = '1653';
-
-UPDATE central_administration_sfcs.rbac_role_menu SET menu_description = 'Hourly Efficiency Report 02' WHERE menu_pid = '1653';
