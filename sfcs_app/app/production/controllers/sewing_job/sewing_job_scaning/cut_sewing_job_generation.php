9f8b2576c9f42bd1f43e58638a63da741acfd09f
pushpa <pushpalatha@schemaxtech.com>
2019-05-23 13:00:29 +0530

    #1995_recuts_validation

--- sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/cut_sewing_job_generation.php
+++ sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/cut_sewing_job_generation.php
@@ -343,7 +343,7 @@ if($schedule != "" && $color != "")
     $ratio_query = "SELECT * FROM bai_pro3.bai_orders_db_confirm bd
    LEFT JOIN bai_pro3.cat_stat_log csl ON bd.order_tid = csl.order_tid 
    LEFT JOIN bai_pro3.plandoc_stat_log psl ON csl.tid = psl.cat_ref AND psl.order_tid = bd.order_tid 
-   WHERE csl.category IN ('Body','Front') AND bd.order_del_no='".$schedule."' AND TRIM(bd.order_col_des) =trim('".$color."') AND psl.order_tid <> ''";
+   WHERE csl.category IN ('Body','Front') AND bd.order_del_no='".$schedule."' AND TRIM(bd.order_col_des) =trim('".$color."') AND psl.order_tid <> '' AND psl.remarks='Normal' ";
   //echo $ratio_query;
     $doc_nos = [];
     $view_shows=[];
