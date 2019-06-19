388148715929b11acfd491933e575340c3717902
ravindra35 <36989030+ravindra35@users.noreply.github.com>
2019-06-19 14:00:46 +0530

    #2046 stock report job

--- sfcs_app/app/jobs/rm_warehouse/rm_stock_report.php
+++ sfcs_app/app/jobs/rm_warehouse/rm_stock_report.php
@@ -1,7 +1,8 @@
 <?php
+$start_timestamp = microtime(true);
 $include_path=getenv('config_job_path');
 include($include_path.'\sfcs_app\common\config\config_jobs.php');
-
+set_time_limit(1000000);
 $sql39="truncate $bai_rm_pj1.stock_report_inventory";
 mysqli_query($link, $sql39) or exit("Sql Error1".mysqli_error($GLOBALS["___mysqli_ston"]));
 
@@ -12,5 +13,9 @@ if($res)
 {
 	print("Data Inserted into stock_report_inventory from stock_report ")."\n";
 }
-
+print( memory_get_usage())."\n";
+	
+$end_timestamp = microtime(true);
+$duration = $end_timestamp - $start_timestamp;
+print("Execution took ".$duration." milliseconds.");
 ?>
