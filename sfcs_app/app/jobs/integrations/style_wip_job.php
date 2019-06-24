46576f755b83ba07934fdec47d4815445d467624
ItsMeBhargav <bhargavg@schemaxtech.com>
2019-06-24 13:25:41 +0530

    #2016 status added

--- sfcs_app/app/jobs/integrations/style_wip_job.php
+++ sfcs_app/app/jobs/integrations/style_wip_job.php
@@ -10,34 +10,34 @@ if(mysqli_num_rows($check_data_result) ==0)
 {
 	//echo "Test---1Exist<br>";
 	$delete_data ="DELETE FROM $brandix_bts.open_style_wip";
-	echo $delete_data."<br>";
+	//echo $delete_data."<br>";
 	mysqli_query($link,$delete_data) or exit('Deleteing Data if exist in that date');
 
-	$get_temp_data ="INSERT IGNORE INTO $brandix_bts.open_style_wip (style,SCHEDULE,color,size,operation_code,good_qty,rejected_qty) 
-	SELECT style,SCHEDULE,color,size_title,operation_id,SUM(recevied_qty) AS good_qty,SUM(rejected_qty) AS rejected_qty 
+	$get_temp_data ="INSERT IGNORE INTO $brandix_bts.open_style_wip (style,SCHEDULE,color,size,operation_code,good_qty,rejected_qty,status) 
+	SELECT style,SCHEDULE,color,size_title,operation_id,SUM(recevied_qty) AS good_qty,SUM(rejected_qty) AS rejected_qty,'open' 
 	FROM $brandix_bts.bundle_creation_data_temp GROUP BY style,SCHEDULE,color,size_title,operation_id";
 	mysqli_query($link,$get_temp_data) or exit('Inserting Data in that date');
-	echo $get_temp_data."<br>";
+	//echo $get_temp_data."<br>";
 	
 } 
 else
 {
 	//echo "Test---1 New<br>";
 	$delete_data ="DELETE FROM $brandix_bts.open_style_wip";
-	echo $delete_data."<br>";
+	//echo $delete_data."<br>";
 	mysqli_query($link,$delete_data) or exit('Deleteing Data if exist in that date');
 
-	$get_temp_data ="INSERT IGNORE INTO $brandix_bts.open_style_wip (style,SCHEDULE,color,size,operation_code,good_qty,rejected_qty) 
-	SELECT style,SCHEDULE,color,size_title,operation_id,SUM(recevied_qty) AS good_qty,SUM(rejected_qty) AS rejected_qty 
+	$get_temp_data ="INSERT IGNORE INTO $brandix_bts.open_style_wip (style,SCHEDULE,color,size,operation_code,good_qty,rejected_qty,status) 
+	SELECT style,SCHEDULE,color,size_title,operation_id,SUM(recevied_qty) AS good_qty,SUM(rejected_qty) AS rejected_qty, 'open' 
 	FROM $brandix_bts.bundle_creation_data_temp where DATE(scanned_date)<'".date('Y-m-d')."' GROUP BY style,SCHEDULE,color,size_title,operation_id";
 	mysqli_query($link,$get_temp_data) or exit('Inserting Data in that date');
-	echo $get_temp_data."<br>";
+	//echo $get_temp_data."<br>";
 	
-	$get_temp_data_temp ="INSERT IGNORE INTO $brandix_bts.open_style_wip (style,SCHEDULE,color,size,operation_code,good_qty,rejected_qty) 
-	SELECT style,SCHEDULE,color,size_title,operation_id,0,0 
+	$get_temp_data_temp ="INSERT IGNORE INTO $brandix_bts.open_style_wip (style,SCHEDULE,color,size,operation_code,good_qty,rejected_qty,status) 
+	SELECT style,SCHEDULE,color,size_title,operation_id,0,0 ,'open'
 	FROM $brandix_bts.bundle_creation_data_temp where DATE(scanned_date)='".date('Y-m-d')."' GROUP BY style,SCHEDULE,color,size_title,operation_id";
 	mysqli_query($link,$get_temp_data_temp) or exit('Inserting Data in that date');
-	echo $get_temp_data_temp."<br>";
+	//echo $get_temp_data_temp."<br>";
 }
 
 
