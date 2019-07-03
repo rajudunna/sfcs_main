db1a84e6f094093f09f8deec77e173adee036351
unknown <santoshkumarvanjala15@gmail.com>
2019-07-03 11:41:29 +0530

    #2113 new code

--- sfcs_app/app/production/reports/m3_transcations_reconfirm_report.php
+++ sfcs_app/app/production/reports/m3_transcations_reconfirm_report.php
@@ -27,7 +27,10 @@ include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/
 $view_access=user_acl("SFCS_0068",$username,1,$group_id_sfcs); 
 ?>
              <?php 
-                $sql="SELECT id,ref_no,response_status,m3_bulk_tran_id FROM $bai_pro3.`m3_transactions` WHERE response_status='fail' AND m3_trail_count=4";
+               //  $sql="SELECT id,ref_no,response_status,mo_no,m3_bulk_tran_id FROM $bai_pro3.`m3_transactions` WHERE response_status='fail' AND m3_trail_count=4";
+              $sql="SELECT `bai_pro3`.`m3_transactions`.id,ref_no,response_status,`bai_pro3`.`m3_transactions`.mo_no,m3_bulk_tran_id,style,schedule,color FROM `bai_pro3`.`m3_transactions` LEFT JOIN `bai_pro3`.`mo_details` ON `bai_pro3`.`mo_details`.`mo_no`=`bai_pro3`.`m3_transactions`.`mo_no`
+                    WHERE response_status='fail' AND m3_trail_count=4";
+                
                 mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
                $sql_result=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
                $count=mysqli_num_rows($sql_result);
@@ -37,7 +40,7 @@ $view_access=user_acl("SFCS_0068",$username,1,$group_id_sfcs);
                         <div class="panel-body">
                            <div class="table-responsive">
                                  <table id="example" cellspacing="0" width="100%" class="table table-bordered">
-                                 <tr><th>Ref No</th><th>Response Status</th>';
+                                 <tr><th>Mo No</th><th>Style</th><th>Schedule</th><th>Color</th><th>Response Status</th>';
                            echo '<form action="'.getFullURLLevel($_GET["r"],"m3_transcations_reconfirm_report.php","0","N").'" name="print" method="POST">
                                        <th><input type="checkbox" onClick="checkAll()"/>Select All</th></tr>';
                            
@@ -47,7 +50,22 @@ $view_access=user_acl("SFCS_0068",$username,1,$group_id_sfcs);
                      $id=$sql_row['id'];
                      $m3_bulk_tran_id=$sql_row['m3_bulk_tran_id'];
                      // echo $m3_bulk_tran_id; 
-                    echo "<tr><td>".$sql_row['ref_no']."</td><td>".$sql_row['response_status']."</td>";
+                     if($sql_row['style']==''){
+                        $sql_row['style']="--";
+                     }else{
+                        $sql_row['style'];
+                     }
+                     if($sql_row['schedule']==''){
+                        $sql_row['schedule']="--";
+                     }else{
+                        $sql_row['schedule'];
+                     }
+                     if($sql_row['color']==''){
+                        $sql_row['color']="--";
+                     }else{
+                        $sql_row['color'];
+                     }
+                    echo "<tr><td>".$sql_row['mo_no']."</td><td>".$sql_row['style']."</td><td>".$sql_row['schedule']."</td><td>".$sql_row['color']."</td><td>".$sql_row['response_status']."</td>";
                     echo "<td><input type='checkbox' name='bindingdata[]' value='".$id.'-'.$m3_bulk_tran_id."'></td>";
 
                 }
