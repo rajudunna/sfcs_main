d0fe690eb07f4d384f36b226f576bc65ba4c347f
pushpa <pushpalatha@schemaxtech.com>
2019-07-04 15:52:55 +0530

    #2156_variable_changes

--- deploy/conversion/2156_inputjob_script.php
+++ deploy/conversion/2156_inputjob_script.php
@@ -3,25 +3,25 @@
 include($_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/config/config.php');
 
 $sql2="Alter table `bai_pro3`.`ims_log`   
-  change `input_job_no_ref` `input_job_no_ref` varchar(20) NOT NULL  COMMENT 'reference of input job number'";
+  change `input_job_no_ref` `input_job_no_ref` varchar(25) NOT NULL  COMMENT 'reference of input job number'";
 $sql_result2=mysqli_query($link, $sql2) or exit("Sql Error2--".mysqli_error($GLOBALS["___mysqli_ston"]));
   
 $sql3="Alter table `bai_pro3`.`ims_log_backup`   
-  change `input_job_no_ref` `input_job_no_ref` varchar(11) NOT NULL";
-$sql_result3=mysqli_query($link, $sql4) or exit("Sql Error3--".mysqli_error($GLOBALS["___mysqli_ston"]));
+  change `input_job_no_ref` `input_job_no_ref` varchar(25) NOT NULL";
+$sql_result3=mysqli_query($link, $sql3) or exit("Sql Error3--".mysqli_error($GLOBALS["___mysqli_ston"]));
   
 $sql4="Alter table `bai_pro`.`bai_log`   
-  change `jobno` `jobno` varchar(4) NOT NULL";
+  change `jobno` `jobno` varchar(25) NOT NULL";
 $sql_result4=mysqli_query($link, $sql4) or exit("Sql Error4--".mysqli_error($GLOBALS["___mysqli_ston"]));
   
   
 $sql5="Alter table `brandix_bts`.`bundle_creation_data`   
-  change `input_job_no` `input_job_no` varchar(11) NULL";
+  change `input_job_no` `input_job_no` varchar(25) NULL";
 $sql_result5=mysqli_query($link, $sql5) or exit("Sql Error5--".mysqli_error($GLOBALS["___mysqli_ston"]));
   
   
 $sql6="Alter table `brandix_bts`.`bundle_creation_data_temp`   
-  change `input_job_no` `input_job_no` varchar(11) NULL";
+  change `input_job_no` `input_job_no` varchar(25) NULL";
 $sql_result6=mysqli_query($link, $sql6) or exit("Sql Error6--".mysqli_error($GLOBALS["___mysqli_ston"]));
 
 $sql1="SELECT group_concat(operation_code) as op_code FROM brandix_bts.tbl_orders_ops_ref where category='sewing' ";
@@ -53,10 +53,10 @@ while($sql_row11=mysqli_fetch_array($sql_result11))
 	$sql_result15=mysqli_query($link, $sql15) or exit("Sql Error15--".mysqli_error($GLOBALS["___mysqli_ston"]));
 	
 	$sql16="update bai_pro.bai_log set jobno='$ijno' where SUBSTRING_INDEX(ims_pro_ref, '-', 1)=$pac_tid and operation_id in($op_code) ";
-	$sql_result16=mysqli_query($link, $sql15) or exit("Sql Error16--".mysqli_error($GLOBALS["___mysqli_ston"]));
+	$sql_result16=mysqli_query($link, $sql16) or exit("Sql Error16--".mysqli_error($GLOBALS["___mysqli_ston"]));
 	
 	$sql17="update bai_pro.bai_log_buf set jobno='$ijno' where SUBSTRING_INDEX(ims_pro_ref, '-', 1)=$pac_tid and operation_id in($op_code) ";
-	$sql_result15=mysqli_query($link, $sql17) or exit("Sql Error17--".mysqli_error($GLOBALS["___mysqli_ston"]));
+	$sql_result17=mysqli_query($link, $sql17) or exit("Sql Error17--".mysqli_error($GLOBALS["___mysqli_ston"]));
 
 }
 
