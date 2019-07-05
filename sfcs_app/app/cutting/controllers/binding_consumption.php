dad6e4ec1f9b1f6bdbc9ab722b71cbd1f9454212
unknown <mounikapentakota30@gmail.com>
2019-07-05 17:40:04 +0530

    commit #2062

--- sfcs_app/app/cutting/controllers/binding_consumption.php
+++ sfcs_app/app/cutting/controllers/binding_consumption.php
@@ -5,27 +5,27 @@ include("../../../common/config/config_ajax.php");
 // global $link;
 // global $bai_pro3;
 
-// var_dump($_GET['row_id']);
+
 if(isset($_GET['row_id']))
 {
+    $php_self = explode('/',$_SERVER['HTTP_HOST']);
+    array_pop($php_self);
+    $url_r = base64_encode(implode('/',$php_self)."/sfcs_app/app/cutting/controllers/seperate_docket.php");
+    $url1 = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://".$_SERVER['HTTP_HOST']."/index.php?r=".$url_r;
+
+
     $row_id = $_GET['row_id'];
     $status = $_GET['status'];
-    if($status== 'Allocate'){
-        $status_new = 'Allocated';
+    if($status== 'Reject'){
+        $status_new = 'Close';
     }
     $query = "UPDATE $bai_pro3.binding_consumption set status = '".$status_new."' where id = $row_id";
     $update_query = mysqli_query($link,$query);
 
-    $message = 'success';
-    echo json_encode($message);
-    exit;
-    // test();
+    echo"<script>swal('Successfully Deleted.','','success')</script>";
+	echo"<script>location.href =  '".$url1."';</script>"; 
 }
 
-// function test(){
-//     var_dump($message);
 
-//     return $message;
-// }
 
 ?>
