b82c17560018988337bc5a8412f4a9451f7dd56b
pushpa <pushpalatha@schemaxtech.com>
2019-07-24 11:53:50 +0530

    #2256_Excess_mofilling_val

--- sfcs_app/common/config/mo_filling.php
+++ sfcs_app/common/config/mo_filling.php
@@ -565,7 +565,8 @@
 									$bundle_no = $row1234['tid'];
 									for($kk=0;$kk<sizeof($mo_no);$kk++)
 									{                                                     
-										$last_mo = $mo_no[sizeof($mo_no)];      
+										//$last_mo = $mo_no[sizeof($mo_no)];'
+                                        $last_mo = $mo_no[$kk];										
 										$m_fil=0;
 										$sql12345="SELECT sum(bundle_quantity) as qty FROM $bai_pro3.mo_operation_quantites WHERE mo_no='".$mo_no[$kk]."' and op_code IN ($ops[0]) GROUP BY op_code";
 										$result12345=mysqli_query($link, $sql12345) or die("Error".mysqli_error($GLOBALS["___mysqli_ston"]));
@@ -627,8 +628,8 @@
 											if($ops_m_id[$last_mo][$ops[$l]]>0)
 											{  
 												
-												$sql="INSERT INTO $bai_pro3.`mo_operation_quantites` (`date_time`, `mo_no`, `ref_no`,`bundle_quantity`, `op_code`, `op_desc`) VALUES ('".date("Y-m-d H:i:s")."', '".$last_mo."','".$row1234['tid']."','".$qty."', '".$ops_m_id[$mo_no[$kk]][$ops[$jj]]."', '".$ops_m_id[$last_mo][$ops[$l]]."')";
-												echo $sql;
+												$sql="INSERT INTO $bai_pro3.`mo_operation_quantites` (`date_time`, `mo_no`, `ref_no`,`bundle_quantity`, `op_code`, `op_desc`) VALUES ('".date("Y-m-d H:i:s")."', '".$last_mo."','".$row1234['tid']."','".$qty."', '".$ops_m_id[$last_mo][$ops[$l]]."', '".$ops_m_name[$last_mo][$ops[$l]]."')";
+												//echo $sql;
 												$result1=mysqli_query($link, $sql) or exit('Error Encountered');
 												$res=1;
 											}
