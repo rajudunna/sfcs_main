eab9872ee4187a36dc68a2466810b5eb453bd25e
pavan <pavan.doddi@schemaxtech.com>
2019-08-16 17:45:32 +0530

    #2429 without operation

--- sfcs_app/app/production/controllers/sewing_job/barcode_without_operation2_2.php
+++ sfcs_app/app/production/controllers/sewing_job/barcode_without_operation2_2.php
@@ -69,7 +69,7 @@
 			//$display1 = get_sewing_job_prefix("prefix","$brandix_bts.tbl_sewing_job_prefix","$bai_pro3.packing_summary_input",$schedule,$color,$input_job,$link);
 			$display1 = get_sewing_job_prefix_inp("prefix","$brandix_bts.tbl_sewing_job_prefix",$input_job,$sewing_job_random_id,$link);
 			$html.= '<div>
-			<table width="100%" style="font-size:6px;">
+			<table width="100%" style="font-size:5px;">
 				<tr>	
 					<td colspan=8>'.str_replace(' ','',$barcode_rslt['order_style_no']).'/'.$schedule.'</td>
 					<td rowspan="0" style="border: 1px solid black;	border-top-right-radius: 1px 1px; font-size:4px; text-align:center;width:10%">
@@ -83,7 +83,7 @@
 				<tr>
 					<td colspan=8>
 						<div>
-							<barcode code="'.$barcode.'" type="C39"/ height="1.20" size="0.90" text="1">
+							<barcode code="'.$barcode.'" type="C39"/ height="1.15" size="0.85" text="1">
 						<br/>
 					<center>'.trim($barcode).'</td> 
 					</div>
