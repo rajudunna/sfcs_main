d60d4911abda062f934d5ecf8d5e8100b8196caa
saiyateesh <saiyateesh@gmail.com>
2019-08-20 11:02:07 +0530

    #2394

--- sfcs_app/app/dashboards/controllers/WIP_Dashboard/wip_dashboard_data.php
+++ sfcs_app/app/dashboards/controllers/WIP_Dashboard/wip_dashboard_data.php
@@ -76,65 +76,55 @@ echo json_encode($section_data);
     
 
     $get_input_jobs = "select distinct(input_job_no_random_ref) from $bai_pro3.plan_dashboard_input where input_module =$module";
-    echo  $get_input_jobs;
+   // echo  $get_input_jobs;
     $result_get_input_jobs = $link->query($get_input_jobs);
     while($row = $result_get_input_jobs->fetch_assoc())
     {
-      $input_job = $row['input_job_no_random_ref'];
+      $input_job[] = $row['input_job_no_random_ref'];
+    }  
 
-        $get_style_details = "select style,schedule,mapped_color From $brandix_bts.bundle_creation_data where input_job_no_random_ref = '$input_job' and operation_id=$get_operation";
+        $get_style_details = "select distinct(schedule),style,mapped_color,input_job_no_random_ref From $brandix_bts.bundle_creation_data where input_job_no_random_ref in ('".implode("','",$input_job)."') and operation_id=$get_operation";
+       // echo $get_style_details;
         $result_get_style_details = $link->query($get_style_details);
-        while($row = $result_get_style_details->fetch_assoc()) 
+        while($row1 = $result_get_style_details->fetch_assoc()) 
         {
-            $style = $row['style'];
-            $schedule = $row['schedule'];
-            $color = $row['mapped_color'];
+            $style = $row1['style'];
+            $schedule = $row1['schedule'];
+            $color = $row1['mapped_color'];
+            $job_no = $row1['input_job_no_random_ref'];
 
             $ops_seq_check = "select id,ops_sequence,operation_order from $brandix_bts.tbl_style_ops_master where style='$style' and color = '$color' and operation_code=$get_operation";
             $result_ops_seq_check = $link->query($ops_seq_check);
-            while($row = $result_ops_seq_check->fetch_assoc()) 
+            while($row2 = $result_ops_seq_check->fetch_assoc()) 
             {
-                $ops_seq = $row['ops_sequence'];
-                $seq_id = $row['id'];
-                $ops_order = $row['operation_order'];
+                $ops_seq = $row2['ops_sequence'];
+                $seq_id = $row2['id'];
+                $ops_order = $row2['operation_order'];
             }
 
             $pre_ops_check = "select operation_code from $brandix_bts.tbl_style_ops_master where style='$style' and color = '$color' and ops_sequence = $ops_seq  AND CAST(operation_order AS CHAR) < '$ops_order' AND operation_code not in (10,200) ORDER BY operation_order DESC LIMIT 1";
-             echo $pre_ops_check;
+             //echo $pre_ops_check;
             $result_pre_ops_check = $link->query($pre_ops_check);
             if($result_pre_ops_check->num_rows > 0)
             {
-                while($row = $result_pre_ops_check->fetch_assoc()) 
+                while($row3 = $result_pre_ops_check->fetch_assoc()) 
                 {
-                    $pre_ops_code = $row['operation_code'];
+                    $pre_ops_code = $row3['operation_code'];
                 }
             }
-            $input_code = $pre_ops_code;
-            $output_code = $get_operation;
+            $previous_operation = $pre_ops_code;
+            $present_operation = $get_operation;
 
-            $get_jobs = "select sum(if(operation_id = $input_code,recevied_qty,0)) as input,sum(if(operation_id = $output_code,recevied_qty,0)) as output From $brandix_bts.bundle_creation_data where assigned_module=$module and input_job_no_random_ref = '$input_job' and operation_id in ($input_code,$output_code) and GROUP BY input_job_no_random_ref,size_title HAVING SUM(IF(operation_id = $input_code,original_qty,0)) != SUM(IF(operation_id = $output_code,recevied_qty,0))";
-            echo $get_jobs;
-           // die();
-            //$get_jobs_result = mysqli_query($link,$get_jobs) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
+            $get_jobs = "select sum(if(operation_id = $previous_operation,recevied_qty,0)) as previous_output,sum(if(operation_id = $present_operation,recevied_qty,0)) as present_output From $brandix_bts.bundle_creation_data where assigned_module=$module and input_job_no_random_ref = '$job_no' and operation_id in ($previous_operation,$present_operation) GROUP BY input_job_no_random_ref,size_title HAVING SUM(IF(operation_id = $previous_operation,recevied_qty,0)) != SUM(IF(operation_id = $present_operation,recevied_qty,0))";
+            //echo $get_jobs;
             $get_jobs_result = $link->query($get_jobs);
-            // while($row1 = mysqli_fetch_array($get_jobs_result))
-            // {
-                   
-                  
-            // }
+            while($row4 = mysqli_fetch_array($get_jobs_result))
+            {
+              $previous_output = $row4['previous_output'];
+              $present_output = $row4['present_output'];   
+            }
+
+            $wip = $previous_output - $present_output;
         }
-
-         
     }
-
-   
-
-
-
-       
-
-
-    
-
-   }
 ?>
