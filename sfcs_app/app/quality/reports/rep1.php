12cd3f7c98694616fc024f89604c2c09fea3c947
kranthip <kranthi.potnuru@schemaxtech.com>
2019-08-30 12:44:52 +0530

    #2254 Changes on rejection reasons

--- sfcs_app/app/quality/reports/rep1.php
+++ sfcs_app/app/quality/reports/rep1.php
@@ -9,19 +9,21 @@ $view_access=user_acl("SFCS_0049",$username,1,$group_id_sfcs);
 
 <?php   
 
-//$reasons=array("Miss Yarn","Fabric Holes","Slub","Foreign Yarn","Stain Mark","Color Shade","Panel Un-Even","Stain Mark","Strip Match","Cut Dmg","Stain Mark","Heat Seal","M ment Out","Shape Out","Emb Defects");
+// $reasons=array("Miss Yarn","Fabric Holes","Slub","Foreign Yarn","Stain Mark","Color Shade","Panel Un-Even","Stain Mark","Strip Match","Cut Dmg","Stain Mark","Heat Seal","M ment Out","Shape Out","Emb Defects");
 
-//Rejection Reasons
+// //Rejection Reasons
 // $reasons=array("Miss Yarn","Fabric Holes","Slub","F.Yarn","Stain Mark","Color Shade","Panel Un-Even","Stain Mark","Strip Match","Cut Damage","Stain Mark","Heat Seal","M' ment Out","Shape Out","Others EMB","Heat Seal","Trim","Un Even","Shape Out Leg","Shape Out waist","With out Label","Trim shortage","Sewing Excess","Cut Holes","Slip Stitch’s","Oil Marks","Foil Defects","Embroidery","Print","Sequence","Bead","Dye","wash");
-	$reasons_sql="select * from bai_pro3.bai_qms_rejection_reason";
+
+// $categories=array("Fabric","Fabric","Fabric","Fabric","Fabric","Fabric","Cutting","Cutting","Cutting","Sewing","Sewing","Sewing","Sewing","Sewing","Embellishment","Fabric","Fabric","Sewing","Sewing","Sewing","Sewing","Sewing","Sewing","Machine Damages","Machine Damages","Machine Damages","Embellishment","Embellishment","Embellishment","Embellishment","Embellishment","Embellishment","Embellishment");
+
+	$reasons_sql="select * from bai_pro3.bai_qms_rejection_reason order by reason_order";
 	$sql_res_rej=mysqli_query($link, $reasons_sql) or exit("Sql Error1".mysqli_error($GLOBALS["___mysqli_ston"]));
 	while($sql_row_rej=mysqli_fetch_array($sql_res_rej))
 	{
-		$reasons[$sql_row_rej['sno']]=$sql_row_rej['reason_desc'];
-		$categories[$sql_row_rej['sno']]=$sql_row_rej['reason_cat'];
+		$reasons[$sql_row_rej['reason_code']]=$sql_row_rej['reason_desc'];
+		$categories[$sql_row_rej['reason_code']]=$sql_row_rej['reason_cat'];
 	}
-	// var_dump($reasons);die();
-// $categories=array("Fabric","Fabric","Fabric","Fabric","Fabric","Fabric","Cutting","Cutting","Cutting","Sewing","Sewing","Sewing","Sewing","Sewing","Embellishment","Fabric","Fabric","Sewing","Sewing","Sewing","Sewing","Sewing","Sewing","Machine Damages","Machine Damages","Machine Damages","Embellishment","Embellishment","Embellishment","Embellishment","Embellishment","Embellishment","Embellishment");
+	
 ?>
 
 <script type="text/javascript" src="<?php echo getFullURLLevel($_GET['r'],'common/js/table2CSV.js',3,'R') ?>" ></script>
@@ -82,7 +84,7 @@ if(isset($_POST['filter']))
 	if($team!="ALL"){
 		$sql="select qms_tid,qms_style,qms_schedule,qms_color,substring_index(remarks,\"-\",1) as \"module\",substring_index(substring_index(remarks,\"-\",2),\"-\",-1) as \"shift\",substring_index(remarks,\"-\",-1) as \"form\",qms_size,log_date,ref1,SUBSTRING_INDEX(doc_no,'D',-1) as doc_no from $bai_pro3.bai_qms_db where qms_tran_type=3 and log_date between \"$sdate\" and \"$edate\" and substring_index(substring_index(remarks,\"-\",2),\"-\",-1)=\"$team\" order by log_date,substring_index(remarks,\"-\",1)+0,substring_index(remarks,\"-\",-1),qms_style,qms_schedule,qms_color,qms_size";
 	}
-	// echo "<br>".$sql."<br>";
+	//echo "<br>".$sql."<br>";
 	$sql_result=mysqli_query($link, $sql) or exit("Sql Error2".mysqli_error($GLOBALS["___mysqli_ston"]));
 
 	if(mysqli_num_rows($sql_result) > 0) {
@@ -129,12 +131,11 @@ if(isset($_POST['filter']))
 				$rep_qty=$sql_rowxx['qms_qty'];
 			}		
 			// echo $rep_qty;
-
 			for($i=0;$i<sizeof($temp);$i++)
 			{
 				$temp2=array();
 				$temp2=explode("-",$temp[$i]);
-				// var_dump($temp2);
+				
 				//Define the rejection form a garment or panel
 				//*P=Panel; *G=Garment
 				$form="Panel";
@@ -179,8 +180,8 @@ if(isset($_POST['filter']))
 				$roll_ids=array();
 				$roll_ids[]=-1;
 				
-				$sql_roll_ids="SELECT * FROM $bai_rm_pj1.fabric_cad_allocation WHERE DOC_NO in ('".$doc_ref."') ORDER BY DOC_NO";
-				 //echo "<br>".$sql_roll_ids."<br>";
+				$sql_roll_ids="SELECT * FROM $bai_rm_pj1.fabric_cad_allocation WHERE DOC_NO in (".$doc_ref.") ORDER BY DOC_NO";
+				 // echo "<br>".$sql_roll_ids."<br>";
 				$sql_result_roll_ids=mysqli_query($link, $sql_roll_ids) or exit("Sql Error43".mysqli_error($GLOBALS["___mysqli_ston"]));
 				while($sql_row_roll_ids=mysqli_fetch_array($sql_result_roll_ids))
 				{
@@ -273,6 +274,9 @@ if(isset($_POST['filter']))
 				
 				$x=$temp2[0];
 				//Based on array, fetch the rejection and department category names
+
+				//echo "<td class=\"lef\">".$categories[$x]."</td>";
+				//echo "<td class=\"lef\">".$reasons[$x]."</td>";
 				if($categories[$x])
 				{
 					echo "<td class=\"lef\">".$categories[$x]."</td>";
@@ -289,7 +293,9 @@ if(isset($_POST['filter']))
 				{
 					echo "<td class=\"lef\">Reason deleted</td>";
 				}
+
 				echo "<td>".$temp2[1]."</td>";
+				
 				//Replace Qty.
 				if($rep_qty>=$temp2[1]){
 					echo "<td>".$temp2[1]."</td>";
