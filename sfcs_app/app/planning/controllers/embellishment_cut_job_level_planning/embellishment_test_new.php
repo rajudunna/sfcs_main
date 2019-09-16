a020211ecd7fd001fd5d2515fb4f489158903f34
Mounika <mounikapentakota30@gmail.com>
2019-09-16 10:34:19 +0530

    commit #1626

--- sfcs_app/app/planning/controllers/embellishment_cut_job_level_planning/embellishment_test_new.php
+++ sfcs_app/app/planning/controllers/embellishment_cut_job_level_planning/embellishment_test_new.php
@@ -5,6 +5,7 @@
 
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/config.php',4,'R')); 
+// include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/functions.php',4,'R')); 
 include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/user_acl_v1.php',4,'R'));
 include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'functions.php',1,'R'));
 
