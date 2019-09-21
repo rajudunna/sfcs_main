3b4b9d37544d536809a891a027c587a6a3628619
ItsMeBhargav <bhargavg@schemaxtech.com>
2019-09-21 19:14:40 +0530

    #2494 code committed

--- sfcs_app/app/cutting/controllers/cut_reporting_without_rolls/cut_reporting_save.php
+++ sfcs_app/app/cutting/controllers/cut_reporting_without_rolls/cut_reporting_save.php
@@ -93,7 +93,7 @@ if($plies == 0 && $full_reporting_flag == 1){
                     remarks=CONCAT(remarks,'$','$remarks'),
                     log_date='$date_time',bundle_loc='$bundle_location',leader_name='$team_leader',joints_endbits=CONCAT(joints_endbits,'$','$joints_endbits')";
     echo $insert_query;
-    $update_psl_query = "UPDATE $bai_pro3.plandoc_stat_log set act_cut_status='DONE' 
+    $update_psl_query = "UPDATE $bai_pro3.plandoc_stat_log set act_cut_status='DONE',a_plie=0 
                     where doc_no = $doc_no or org_doc_no = $doc_no";
     $insert_result = mysqli_query($link,$insert_query) or exit('Query Error 0 Cut 1');   
     $update_result = mysqli_query($link,$update_psl_query) or exit('Query Error 0 Cut 2');
