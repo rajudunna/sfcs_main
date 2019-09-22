1884152e570bf792b3dd68a955df168e430e0539
ravindra35 <ravindra@schemaxtech.com>
2019-09-23 00:45:38 +0530

    #2503 material deallocation completed

--- sfcs_app/app/cutting/controllers/lay_plan_preparation/material_deallocation.php
+++ sfcs_app/app/cutting/controllers/lay_plan_preparation/material_deallocation.php
@@ -1,6 +1,6 @@
 <?php
     include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/config.php',4,'R')); 
-    
+    $username = getrbac_user()['uname'];
 ?>
 <div class="panel panel-primary"> 
     <div class="panel-heading">Material Deallocation</div>
@@ -57,7 +57,7 @@
                                 echo "<option value=''>Please Select</option>";
                                 echo "<option value='Approve'>Approve</option>";
                                 echo "</select></td>";
-                                echo "<td><input type='submit' name='submit$i' id='submit-$i' class='btn btn-info' value='Deallocate' disabled='disabled' onclick='UpdateDamageStatus($i);'></td>";
+                                echo "<td><input type='submit' name='submit$i' id='submit-$i' class='btn btn-info' value='Deallocate' disabled='disabled' onclick='Approve_deallocation($i);'></td>";
                                 echo "</tr>";
                             }
                         ?>
@@ -77,7 +77,7 @@
                             </tr>
                         </thead>
                         <?php
-                            $query = "select * from $bai_rm_pj1.material_deallocation_track where status='Approved'";
+                            $query = "select * from $bai_rm_pj1.material_deallocation_track where status='Deallocated'";
                             $sql_result = mysqli_query($link,$query);
                             // echo $query;
                             while($sql_row=mysqli_fetch_array($sql_result))
@@ -107,26 +107,25 @@ if(isset($_POST['formSubmit']))
     $doc_no = $_POST['docket_number'];
    
     $fabric_status_qry="SELECT * FROM $bai_pro3.plandoc_stat_log WHERE doc_no=$doc_no";
-    // echo $fabric_status_qry;
     $fabric_status_qry_result=mysqli_query($link, $fabric_status_qry) or exit("Sql Error0: fabric_status_qry".mysqli_error($GLOBALS["___mysqli_ston"]));
 
-    if(mysqli_num_rows($fabric_status_qry_result)>0){
+    if(mysqli_num_rows($fabric_status_qry_result)>0)
+    {
         while($sql_row0=mysqli_fetch_array($fabric_status_qry_result))
         {
             $fabric_status = $sql_row0['fabric_status'];
         }
         $fab_qry="SELECT * FROM $bai_rm_pj1.fabric_cad_allocation WHERE doc_no=$doc_no";
         $fab_qry_result=mysqli_query($link, $fab_qry) or exit("Sql Error1: fabric_cad_allocation".mysqli_error($GLOBALS["___mysqli_ston"]));
-        if(mysqli_num_rows($fab_qry_result)>0){     
+        if(mysqli_num_rows($fab_qry_result)>0)
+        {     
             if($fabric_status != 5)
             {
-
                 $is_requested="SELECT * FROM $bai_rm_pj1.material_deallocation_track WHERE doc_no=$doc_no and status='Open'";
                 $is_requested_result=mysqli_query($link, $is_requested) or exit("Sql Error0: fabric_status_qry".mysqli_error($GLOBALS["___mysqli_ston"]));
 
                 if(mysqli_num_rows($is_requested_result)==0)
                 {
-                    // if($doc_no != ''){
                     $fab_qry="SELECT * FROM $bai_rm_pj1.fabric_cad_allocation WHERE doc_no=$doc_no";
                     $fab_qry_result=mysqli_query($link, $fab_qry) or exit("Sql Error1: fabric_cad_allocation".mysqli_error($GLOBALS["___mysqli_ston"]));
                     $allocated_qty=0;
@@ -144,20 +143,6 @@ if(isset($_POST['formSubmit']))
                             },2000);
                             </script>";
                     exit();
-                            // $req_no_qry="SELECT * FROM $bai_rm_pj1.material_deallocation_track";
-                            // $req_no_qry_result=mysqli_query($link, $req_no_qry) or exit("Sql Error3: material_deallocation_track".mysqli_error($GLOBALS["___mysqli_ston"]));
-                            // $sno = 0;
-                            // var_dump($req_no_qry);
-
-                            // while($sql_row2=mysqli_fetch_array($req_no_qry_result))
-                            // {
-                            //     // var_dump($sql_row2);
-                            //     $sno++;
-                            //     echo "<table><tr><th>Sno.</th><th>Doc No</th><th>Qty.</th><th>Requested By</th><th>Requested At</th><th>Status</th></tr>";
-                            //     echo "<tr><td>".$sno."</td><td>".$sql_row2['doc_no']."</td><td>".$sql_row2['qty']."</td><td>".$sql_row2['requested_by']."</td><td>".$sql_row2['requested_at']."</td><td></td></tr>";
-                            //     echo "</table>";
-                            // }
-                    // }
                 } 
                 else 
                 {
@@ -167,81 +152,29 @@ if(isset($_POST['formSubmit']))
             {
                 echo "<script>swal('Error','Material is Issued to Cutting','error')</script>";
             }
-        } else {
+        }
+        else{
             echo "<script>swal('Error','Material is Not Yet Allocated','error')</script>";
         }
-        // while($sql_row0=mysqli_fetch_array($fabric_status_qry_result))
-        // {
-        //     $fabric_status = $sql_row0['fabric_status'];
-        // }
-           
-        // if($fabric_status != 5){
-          
-        //     $fab_qry="SELECT * FROM $bai_rm_pj1.fabric_cad_allocation WHERE doc_no=$doc_no";
-        //     $fab_qry_result=mysqli_query($link, $fab_qry) or exit("Sql Error1: fabric_cad_allocation".mysqli_error($GLOBALS["___mysqli_ston"]));
-        //     if(mysqli_num_rows($fab_qry_result)>0){
-        //         while($sql_row1=mysqli_fetch_array($fab_qry_result))
-        //         {
-        //             $store_in_qry="SELECT * FROM $bai_rm_pj1.store_in WHERE tid = ".$sql_row1['roll_id'];
-        //             $store_in_qry_result=mysqli_query($link, $store_in_qry) or exit("Sql Error2: store_in".mysqli_error($GLOBALS["___mysqli_ston"]));
-        //             while($sql_row2=mysqli_fetch_array($store_in_qry_result))
-        //             {
-        //                 $tid=$sql_row2['tid'];
-        //                 $rec_old=$sql_row2['qty_rec'];
-        //                 $temp =0 ;
-            
-        //                 $find_duplicate_in_qry="SELECT * FROM $bai_rm_pj1.store_in WHERE split_roll=".$tid;
-        //                 $find_duplicate_in_qry_result=mysqli_query($link, $find_duplicate_in_qry) or exit("Sql Error3: dup store_in".mysqli_error($GLOBALS["___mysqli_ston"]));
-        //                 while($sql_row3=mysqli_fetch_array($find_duplicate_in_qry_result))
-        //                 {
-        //                     $rec_new =$sql_row3['qty_rec'];
-        //                     $tid_new=$sql_row3['tid'];
-        //                     $delete_in_qry="DELETE FROM $bai_rm_pj1.store_in WHERE tid=".$tid_new;
-        //                     $delete_in_qry_result=mysqli_query($link, $delete_in_qry) or exit("Sql Error4: dup store_in".mysqli_error($GLOBALS["___mysqli_ston"]));
-                            
-        //                     $temp =1;
-            
-        //                 }
-        //                 if($temp!=1){
-        //                     $update_qry1="UPDATE $bai_rm_pj1.store_in set qty_allocated=0,status=0,allotment_status=0 WHERE tid=".$tid;
-        //                     $update_qry1_result=mysqli_query($link, $update_qry1) or exit("Sql Error6: dup store_in".mysqli_error($GLOBALS["___mysqli_ston"]));
-        //                 } else {
-        //                     $update_qry="UPDATE $bai_rm_pj1.store_in set qty_rec=$rec_new+$rec_old,qty_allocated=0,status=0,allotment_status=0  WHERE tid=".$tid;
-        //                     $update_qry_result=mysqli_query($link, $update_qry) or exit("Sql Error5: dup store_in".mysqli_error($GLOBALS["___mysqli_ston"]));
-        //                 }
-        //                 $delete_fab="delete from $bai_rm_pj1.fabric_cad_allocation WHERE doc_no=$doc_no";
-        //                 $delete_fab_result=mysqli_query($link, $delete_fab) or exit("Sql Error2: delete fabric_cad_allocation".mysqli_error($GLOBALS["___mysqli_ston"]));
-            
-        //                 $update_plan_qry="update $bai_pro3.plandoc_stat_log set plan_lot_ref='',print_status='' where doc_no=".$doc_no;
-        //                 $update_plan_qry_fab_result=mysqli_query($link, $update_plan_qry) or exit("Sql Error5: update plan".mysqli_error($GLOBALS["___mysqli_ston"]));
-        //                 echo "<script>swal('sucess','Material De-Allocated Successfully','success')</script>";
-                
-        //             }
-        //         }
-        //     } else {
-        //         echo "<script>swal('Error','Material is Not Yet Allocated','error')</script>";
-        //     }
-        // }   else {
-        //     echo "<script>swal('Error','Material is Issued to Cutting','error')</script>";
-        // }
-    } else {
+    } 
+    else{
         echo "<script>swal('Error','Enter Valid Docket Number','error')</script>";
     }
 }
 ?>
 <script>
 function IssueAction(i)
-    {
-        document.getElementById('submit-'+i).disabled = false;
-    }
-function UpdateDamageStatus(i) {
+{
+    document.getElementById('submit-'+i).disabled = false;
+}
+function Approve_deallocation(i) {
     if($('#submit-'+i).val()) {
         var status = $('#issue_status-'+i).val();
         var row_id = i;
 
         if(status=='Approve') {
             url_path = "<?php echo getFullURL($_GET['r'],'update_deallocation_status.php','N'); ?>";
-            window.open(url_path+"&id="+row_id+"&status="+status);
+            location.href=url_path+"&id="+row_id+"&status="+status;
         }
     }
     
