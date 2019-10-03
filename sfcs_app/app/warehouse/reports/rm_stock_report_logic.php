0ea434ce672534ddab378c756fea2d79daeec120
gayatri <gayatri@schemaxtech.com>
2019-10-03 18:36:29 +0530

    #2684 adding mrn issued

--- sfcs_app/app/warehouse/reports/rm_stock_report_logic.php
+++ sfcs_app/app/warehouse/reports/rm_stock_report_logic.php
@@ -89,7 +89,7 @@ while ($sql_row1 = $stock_report_inventory_result->fetch_assoc())
         while ($row_mrn = $sql_result_mrn->fetch_assoc())
         {
             $qty_issued=$qty_issued-$row_mrn["mrn_qty"];
-            $qty_balance=$qty_rec+$qty_return- $qty_issued;
+            $qty_balance=$qty_rec+$qty_return + $qty_issued;
 		}
 	}
 	
@@ -181,7 +181,7 @@ if($max_id>0){
 			if(mysqli_num_rows($sql_result_mrn)> 0) {
 				while ($row_mrn = $sql_result_mrn->fetch_assoc())
 				{
-					$qty_issued=$qty_issued-$row_mrn["mrn_qty"];
+					$qty_issued=$qty_issued+$row_mrn["mrn_qty"];
 					$qty_balance=$qty_rec+$qty_return- $qty_issued;
 				}
 			}
