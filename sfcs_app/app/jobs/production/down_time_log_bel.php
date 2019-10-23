0ecd617ef93471fb3b53005f28ff2bf22e3f022d
gayatri <gayatri@schemaxtech.com>
2019-10-23 12:41:50 +0530

    #2706 lost hour details,comments deletion

--- sfcs_app/app/jobs/production/down_time_log_bel.php
+++ sfcs_app/app/jobs/production/down_time_log_bel.php
@@ -1,63 +1,9 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<script language="javascript" type="text/javascript" src="../styles/dropdowntabs.js"></script>
-<link rel="stylesheet" href="../styles/ddcolortabs.css" type="text/css" media="all" />
-	
-<meta http-equiv="cache-control" content="no-cache">
-<style type="text/css" media="screen">
-/*====================================================
-	- HTML Table Filter stylesheet
-=====================================================*/
-@import "../TableFilter_EN/filtergrid.css";
-
-/*====================================================
-	- General html elements
-=====================================================*/
-body{
-	margin:15px; padding:15px; border:1px solid #666;
-	font-family:Trebuchet MS, sans-serif; font-size:88%;
-}
-h2{ margin-top: 50px; }
-caption{ margin:10px 0 0 5px; padding:10px; text-align:left; }
-pre{ font-size:13px; margin:5px; padding:5px; background-color:#f4f4f4; border:1px solid #ccc;  }
-.mytable{
-	width:100%; font-size:12px;
-	border:1px solid #ccc;
-}
-div.tools{ margin:5px; }
-div.tools input{ background-color:#f4f4f4; border:2px outset #f4f4f4; margin:2px; }
-th{ background-color:#003366; color:#FFF; padding:2px; border:1px solid #ccc; }
-td{ padding:2px; border-bottom:1px solid #ccc; border-right:1px solid #ccc; }
-table{	background-color: white;}
-</style>
-<script language="javascript" type="text/javascript" src="../TableFilter_EN/actb.js"></script><!-- External script -->
-<script language="javascript" type="text/javascript" src="../TableFilter_EN/tablefilter.js"></script>
-
-<Link rel='alternate' media='print' href=null>
-<Script Language=JavaScript>
-
-function setPrintPage(prnThis){
-
-prnDoc = document.getElementsByTagName('link');
-prnDoc[0].setAttribute('href', prnThis);
-window.print();
-}
-
-</Script>
-
-<?php echo '<link href="'."http://".$_SERVER['HTTP_HOST']."/styles/sfcs_styles.css".'" rel="stylesheet" type="text/css" />'; ?>
-</head>
-
-<body onload="dodisable();">
-
 <?php
 error_reporting(0);
 $start_timestamp = microtime(true);
 $include_path=getenv('config_job_path');
 include($include_path.'\sfcs_app\common\config\config_jobs.php');		
-// echo $prod_status_driver_name."-".$bel_wisdom_username."-".$bel_wisdom_password."<br>";
 $con = odbc_connect("$prod_status_driver_name;Server=$bel_wisdom_server_name;Database=$bel_wisdom_database;", $bel_wisdom_username,$bel_wisdom_password);
-// $con=odbc_connect('down_time_dnss','SFCS_User','yhnmko@987');
 if(!$con)
 {
 	echo "Not Connected";
@@ -67,13 +13,6 @@ else
 	echo "Connected";
 }
 
-// $plant_name="Test";
-
-echo '<div>
-<table id="table1" class="mytable">';
-
-echo "<tr class='tblheading'>
-<th>Plant</th><th>Date</th><th>Buyer</th><th>Style</th><th>Line</th><th>Reason</th><th>Reason Sub1</th><th>Reason Sub2</th><th>Lost Pieces</th><th>Lost SAH</th><th>Lost Clock Hrs</th></tr>";
 $sql_max="select max(date) as max_date from $bai_pro.grand_rep where date < \"".date("Y-m-d")."\"";
 // echo "<br/>".$sql_max;
 $sql_result_max=mysqli_query($link,$sql_max) or exit("Sql Error".mysqli_error());
@@ -108,12 +47,6 @@ while($sql_row=mysqli_fetch_array($sql_result))
 	$shift=$sql_row['shift'];
 	$schedule=$sql_row['schedule'];
 	
-	echo "<tr>";
-	echo "<td>".$plant_name."</td>";
-	echo "<td>".$date."</td>";
-	echo "<td>".$buyer."</td>";
-	echo "<td>".$style."</td>";
-	echo "<td>".$module."</td>";
 	
 	$sql1x="select * from $bai_pro.down_deps where dep_id=".$sql_row['department'];
 	//mysqli_query($sql1x) or exit("Sql Errorx".mysqli_error());
@@ -143,14 +76,10 @@ while($sql_row=mysqli_fetch_array($sql_result))
 		$down_reason="Nil";
 	}
 	
-	echo "<td>".$down_machine."</td>";
-	echo "<td>".$down_problem."</td>";
-	echo "<td>".$down_reason."</td>";
 	
 	if($sql_row['plan_eff'] == 0)
 	{	
-		/*if(strtolower($sql_row['remarks']) == "speed open capacity" or strtolower($sql_row['remarks']) == "open capacity")
-		{*/
+		
 			$sql2="SELECT MAX(DATE) as max_date FROM $bai_pro.pro_plan WHERE plan_eff > 0 AND DATE <= \"".$date."\"  and mod_no=\"$module\" and shift=\"$shift\"";
 			// echo $sql2;
 			$sql_result2=mysqli_query($link,$sql2) or die("Sql Error = ".mysqli_error());
@@ -163,14 +92,12 @@ while($sql_row=mysqli_fetch_array($sql_result))
 				while($sql_row21=mysqli_fetch_array($sql_result21))
 				{
 					$plan_eff=$sql_row21["plan_eff"];
-					//echo "<td>".$plan_eff."%</td>";
 				}
 			}		
 	}
 	else
 	{
 		$plan_eff=$sql_row['plan_eff'];
-		//echo "<td>".$plan_eff."%</td>";
 	}
 	
 	//added newly based on anu's requirement
@@ -188,24 +115,18 @@ while($sql_row=mysqli_fetch_array($sql_result))
 	if($smv > 0)
 	{
 		$lost_pcs=round(($sah_loss*60)/($smv*100),0);
-		echo "<td>".$lost_pcs."</td>";
+		
 	}
 	else
 	{
 		$lost_pcs=0;
-		echo "<td>".$lost_pcs."</td>";
 	}
 	
-	echo "<td>".(number_format(round($sah_loss/100,2),2,".",""))."</td>";
 	
-	echo "<td align=right>".(number_format(round($sql_row['dtime']/60,2),2,".",""))."</td>";
 	
 	$count=0;
 	$mssql_select="SELECT * FROM [$bel_wisdom_database].[dbo].[$bel_wisdom_table] where TID='".$tid."' and plant2='".$plant_name."'";
 	$mssql_result=odbc_exec($con,$mssql_select) or exit(odbc_errormsg($con));
-	
-	
-	echo "<br/> mssql_select ".$mssql_select."<br/>";	
 	
 	
 	$count=odbc_num_rows($mssql_result);
@@ -268,9 +189,7 @@ while($sql_row=mysqli_fetch_array($sql_result))
 		//print("One data row inserted.\n");
 	}  
 	
-	echo "</tr>";
 }
-echo "</table>";
 
 
 $sql_delete_log="select * from $bai_pro.down_log_deleted where flag=1";
@@ -283,7 +202,6 @@ while($delete_row=mysqli_fetch_array($result_delete_log))
 	$delete_tid=$delete_row['tid'];
 	$mssql_select1="SELECT * FROM [$bel_wisdom_database].[dbo].[$bel_wisdom_table] where TID='".$delete_tid."' and plant2='".$plant_name."'";
 	
-	echo "<br/> mssql_select1 ".$mssql_select1;
 	
 	$mssql_result1=odbc_exec($con,$mssql_select1);
 	
@@ -319,16 +237,3 @@ while($delete_row=mysqli_fetch_array($result_delete_log))
 odbc_close($con); 
 ?>
 
-</div>
-
-<script language="javascript" type="text/javascript">
-//<![CDATA[
-var MyTableFilter = {  exact_match: false }
-	setFilterGrid( "table1", MyTableFilter );
-//]]>
-</script>
-
-<script language="javascript"> setTimeout("CloseWindow()",0); function CloseWindow() { window.open('','_self',''); window.close(); }  </script> 
-
-</body>
-</html>
