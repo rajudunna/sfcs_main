aa2daed06b4ee25593ac709730293252ee2c0de2
gayatri <gayatri@schemaxtech.com>
2019-11-26 12:45:09 +0530

    #2902 rms dashboard changed regarding plandoc tbl

--- sfcs_app/app/planning/controllers/remove_shortshipment_jobs/remove_jobs.php
+++ sfcs_app/app/planning/controllers/remove_shortshipment_jobs/remove_jobs.php
@@ -194,6 +194,13 @@ if(isset($_POST['submit']))
 
                 if(sizeof($remove_docs)>0)
                 {
+                    //To remove Jobs in RMS, cwip inWPT
+                    $plan_doc_update="UPDATE $bai_pro3.plandoc_stat_log SET short_shipment_status=".$status." WHERE doc_no in (".implode(",",$remove_docs).")";
+                    $plan_doc_update_resultx=mysqli_query($link, $plan_doc_update) or exit("Sql Error11".mysqli_error($GLOBALS["___mysqli_ston"]));
+                    if($plan_doc_update_resultx) {
+                        $remarks .="WIP,";
+                    }   
+
                     //To remove Jobs in Cut Table Dashboard
                     $cut_chck_qry = "select distinct doc_no as cut_docs from $bai_pro3.cutting_table_plan where doc_no in (".implode(",",$remove_docs).")";
                     $cut_chck_qry_res=mysqli_query($link, $cut_chck_qry) or exit("Sql Error20".mysqli_error($GLOBALS["___mysqli_ston"]));
@@ -210,23 +217,7 @@ if(isset($_POST['submit']))
 
                         }
                     }
-
-                    $rm_chck_qry = "select distinct doc_no as rm_docs from $bai_pro3.plan_dashboard where doc_no in (".implode(",",$remove_docs).")";
-                    $rm_chck_qry_res=mysqli_query($link, $rm_chck_qry) or exit("Sql Error20".mysqli_error($GLOBALS["___mysqli_ston"]));
-                    while($rm_chck_row=mysqli_fetch_array($rm_chck_qry_res))
-                    {
-                        $rm_docs[]="'".$rm_chck_row['rm_docs']."'";
-                    }
-                    
-                    if(sizeof($rm_docs)>0){
-                        $plan_dashboard_update="UPDATE $bai_pro3.plan_dashboard SET short_shipment_status=".$status." WHERE doc_no in (".implode(",",$rm_docs).")";
-                        $plan_dashboard_update_resultx=mysqli_query($link, $plan_dashboard_update) or exit("Sql Error11".mysqli_error($GLOBALS["___mysqli_ston"]));
-                        if($plan_dashboard_update_resultx) {
-                            $remarks .="RMS,";
-
-                        }
-                    }
-                    
+                                        
                     //To remove Jobs in Embellishment Dashboard
                     $emblishment_chck_qry = "select distinct doc_no as emb_docs from $bai_pro3.embellishment_plan_dashboard where doc_no in (".implode(",",$remove_docs).")";
                     $emblishment_chck_qry_res=mysqli_query($link, $emblishment_chck_qry) or exit("Sql Error20".mysqli_error($GLOBALS["___mysqli_ston"]));
