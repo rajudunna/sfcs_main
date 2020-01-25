f32a842fd27c125d1ace47ff1e637efae33bc644
ItsMeBhargav <bhargavg@schemaxtech.com>
2020-01-25 11:49:10 +0530

    #3166 committed

---
+++ sfcs_app/app/inspection/common/php/headers.php
@@ -0,0 +1,19 @@
+<?php
+// Rolls and Trims Inspection
+$roll_status=array("Approved","Rejected","Partial Rejected","Hold");
+// Supplier Cliam Request Form
+
+// Supplier Performance Update
+//Array Details
+$construction_array=array("","Single Jersey","Double Jersey","1*1 Rib","2*2 Rib","Lace");
+$syp_array=array("","Solid","Yarn DYE","Print");
+$defects_array=array("","Miss Yarn","Stain","Slub","Holes");
+$test_report_array=array("N/A","Pass","Fail");
+$per_report_array=array("N/A","Yes","No");
+$source_cc_report_array=array("N/A","Yes","No");
+$swatch_array=array("","Supplier","WareHouse");
+// inspection Summary Report
+//Authorised to update/confirm reports
+
+
+?>
