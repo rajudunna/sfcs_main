6867ffe63aa6b03e34291f0f22a875cfd5fc4825
RAVI SHANKAR <ravishankar@schemaxtech.com>
2020-01-27 12:51:17 +0530

    #3142 ops seq changed

--- sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/functions_scanning_ij.php
+++ sfcs_app/app/production/controllers/sewing_job/sewing_job_scaning/functions_scanning_ij.php
@@ -182,7 +182,7 @@ function getjobdetails($job_number)
         die();
     }
 
-    $get_ops_query = "SELECT DISTINCT tm.operation_code FROM $brandix_bts.tbl_style_ops_master tm LEFT JOIN $brandix_bts.tbl_orders_ops_ref tr ON tr.id=tm.operation_name WHERE tm.style ='$job_number[1]' AND tm.color='$maped_color' AND tr.category = 'sewing' ORDER BY operation_order";
+    $get_ops_query = "SELECT DISTINCT tm.operation_code,tr.operation_code as opcode,tr.operation_name FROM $brandix_bts.tbl_style_ops_master tm LEFT JOIN $brandix_bts.tbl_orders_ops_ref tr ON tr.id=tm.operation_name WHERE tm.style ='$job_number[1]' AND tm.color='$maped_color' AND tr.category = 'sewing' and display_operations='yes' ORDER BY operation_order";
 
     $ops_query_result=mysqli_query($link,$get_ops_query);
     while ($row = mysqli_fetch_array($ops_query_result))
@@ -190,6 +190,7 @@ function getjobdetails($job_number)
         
         $ops_get_code[] = $row['operation_code'];
         //$result_array['ops_get_code'][] = $row['operation_code'];
+		$result_array['ops_get_code'][$row['operation_name']] = $row['opcode'];
 
     }
 
@@ -200,7 +201,7 @@ function getjobdetails($job_number)
     $ops_query_result1=$link->query($to_display_values);
     while ($row1 = $ops_query_result1->fetch_assoc())
     {
- $result_array['ops_get_code'][$row1['operation_name']] = $row1['operation_code'];
+ // $result_array['ops_get_code'][$row1['operation_name']] = $row1['operation_code'];
     }
     // echo $display_code;
 
