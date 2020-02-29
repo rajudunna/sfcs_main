cf86022831177b838e724e08e51a852893b2e913
Mounika <mounikapentakota30@gmail.com>
2020-02-29 14:18:55 +0530

    commit #3332

--- sfcs_app/app/dashboards/controllers/cps/allocation.php
+++ sfcs_app/app/dashboards/controllers/cps/allocation.php
@@ -624,8 +624,8 @@ if(isset($_POST['allocate_new']))
 					if($issued_ref[$j] > 0)
 					{
 						// $roll_splitting = roll_splitting_function($tid_ref[$j],$val_ref[$j],$issued_ref[$j]);
-						$sql="update bai_rm_pj1.store_in set allotment_status=1 where tid=".$tid_ref[$j];
-    					mysqli_query($link, $sql) or exit("Sql Error3: $sql".mysqli_error($GLOBALS["___mysqli_ston"]));
+						$sql111="update bai_rm_pj1.store_in set allotment_status=1 where tid=".$tid_ref[$j];
+    					mysqli_query($link, $sql111) or exit("Sql Error3: $sql".mysqli_error($GLOBALS["___mysqli_ston"]));
 						if(strtolower($roll_splitting) == 'yes')
 						{
 							
@@ -638,23 +638,27 @@ if(isset($_POST['allocate_new']))
 
 								if($balance_qty>0){
 									//getting new rolls details
-									$qry_rolldetails="SELECT lot_no,ref1,ref2,ref3,remarks,log_user, status, ref4, ref5, ref6, roll_status, shrinkage_length, shrinkage_width, shrinkage_group, rejection_reason,barcode_number,ref_tid FROM $bai_rm_pj1.store_in WHERE tid=".$tid_ref[$j];
+									$qry_rolldetails="SELECT lot_no,ref1,ref2,ref3,remarks,log_user, status, ref4, ref5, ref6, roll_status, shrinkage_length, shrinkage_width, shrinkage_group, rejection_reason,barcode_number,ref_tid,qty_allocated FROM $bai_rm_pj1.store_in WHERE tid=".$tid_ref[$j];
 									$result__rolldetials=mysqli_query($link, $qry_rolldetails);
 									$row_rolldetials=mysqli_fetch_assoc($result__rolldetials);
-									$qry_newroll="insert into bai_rm_pj1.store_in(lot_no,ref1,ref2,ref3,qty_rec, date, remarks, log_user, status, ref4, ref5, ref6, roll_status, shrinkage_length, shrinkage_width, shrinkage_group, rejection_reason, split_roll,ref_tid,barcode_number) values('".$row_rolldetials["lot_no"]."','".$row_rolldetials["ref1"]."','".$row_rolldetials["ref2"]."','".$row_rolldetials["ref3"]."','".$balance_qty."','".$current_date."','".$row_rolldetials["remarks"]."','".$row_rolldetials["log_user"]."','".$row_rolldetials["status"]."','".$row_rolldetials["ref4"]."','".($row_rolldetials["ref5"]-$issued_ref[$j])."','".$row_rolldetials["ref6"]."','".$row_rolldetials["roll_status"]."','".$row_rolldetials["shrinkage_length"]."','".$row_rolldetials["shrinkage_width"]."','".$row_rolldetials["shrinkage_group"]."','".$row_rolldetials["rejection_reason"]."','".$tid_ref[$j]."','".$row_rolldetials["ref_tid"]."','".$row_rolldetials["barcode_number"]."')";
+									$qry_newroll="insert into $bai_rm_pj1.store_in(lot_no,ref1,ref2,ref3,qty_rec, date, remarks, log_user, status, ref4, ref5, ref6, roll_status, shrinkage_length, shrinkage_width, shrinkage_group, rejection_reason, split_roll,ref_tid,barcode_number,qty_allocated,allotment_status) values('".$row_rolldetials["lot_no"]."','".$row_rolldetials["ref1"]."','".$row_rolldetials["ref2"]."','".$row_rolldetials["ref3"]."','".$balance_qty."','".$current_date."','".$row_rolldetials["remarks"]."','".$row_rolldetials["log_user"]."','".$row_rolldetials["status"]."','".$row_rolldetials["ref4"]."','".($row_rolldetials["ref5"]-$issued_ref[$j])."','".$row_rolldetials["ref6"]."','".$row_rolldetials["roll_status"]."','".$row_rolldetials["shrinkage_length"]."','".$row_rolldetials["shrinkage_width"]."','".$row_rolldetials["shrinkage_group"]."','".$row_rolldetials["rejection_reason"]."','".$tid_ref[$j]."','".$row_rolldetials["ref_tid"]."','".$row_rolldetials["barcode_number"]."','".$row_rolldetials["qty_allocated"]."','".$row_rolldetials["allotment_status"]."')";
 									mysqli_query($link, $qry_newroll) or exit("Sql Error3: $qry_newroll".mysqli_error($GLOBALS["___mysqli_ston"]));
 									$new_tid=mysqli_insert_id($link);
-									$sql22="update bai_rm_pj1.store_in set barcode_number='".$facility_code."-".$new_tid."' where tid=".$new_tid;
+									$sql22="update $bai_rm_pj1.store_in set barcode_number='".$facility_code."-".$new_tid."' where tid=".$new_tid;
+									//Uncheck this
+									mysqli_query($link, $sql22) or exit("Sql Error3: $sql".mysqli_error($GLOBALS["___mysqli_ston"]));
+
+									$sql22="update $bai_rm_pj1.fabric_cad_allocation set roll_id='".$new_tid."' where roll_id=".$tid_ref[$j];
 									//Uncheck this
 									mysqli_query($link, $sql22) or exit("Sql Error3: $sql".mysqli_error($GLOBALS["___mysqli_ston"]));
 								}
-								$sql="update bai_rm_pj1.store_in set qty_rec=".$issued_ref[$j]." where tid=".$tid_ref[$j];
+								$sql="update $bai_rm_pj1.store_in set qty_rec=".$issued_ref[$j].",qty_allocated=0 where tid=".$tid_ref[$j];
 								//Uncheck this
 								mysqli_query($link, $sql) or exit("Sql Error3: $sql".mysqli_error($GLOBALS["___mysqli_ston"]));
 							}
 
-							$sql="update $bai_rm_pj1.store_in set status=2, allotment_status=2 where tid=".$tid_ref[$j];
-								$sql_result=mysqli_query($link,$sql) or exit("Sql Error".mysqli_error());
+							$sql="update $bai_rm_pj1.store_in set status=2,allotment_status=2 where tid=".$tid_ref[$j];
+							$sql_result=mysqli_query($link,$sql) or exit("Sql Error".mysqli_error());
 						}
                     }
                     
