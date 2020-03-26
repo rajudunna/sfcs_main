e06ab7a48f7f8eb7c9cb76bffca737fcfa87bc0f
gayatri <gayatri@schemaxtech.com>
2020-03-26 12:59:51 +0530

    #3374 changes

--- deploy/conversion/3374_tbl_carton_ready.php
+++ deploy/conversion/3374_tbl_carton_ready.php
@@ -6,7 +6,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/config/config.php');
 
 $get_code51 = "TRUNCATE `bai_pro3`.`tbl_carton_ready`";
 $bcd_result51 = mysqli_query($link,$get_code51);
-
+$count_val = 0;$count_value=0;
 $get_styles="select distinct(style) as style_val,color from brandix_bts.tbl_style_ops_master group by style_val,color";
 $style_result = mysqli_query($link,$get_styles);
 while($row1 = mysqli_fetch_array($style_result))
@@ -23,52 +23,72 @@ while($row1 = mysqli_fetch_array($style_result))
 		$final_op_code=mysqli_fetch_array($result_last_opn_sewing);
 		$sewing_last_opn = $final_op_code['operation_code'];
 	}
-	echo $style."--".$color."---".$sewing_last_opn."<br/>";
-	$get_mo="SELECT mo_no FROM `bai_pro3`.`mo_details` WHERE style = '$style'  AND color = '$color' ";
-	$mo_result = mysqli_query($link,$get_mo);
-	while($row2 = mysqli_fetch_array($mo_result))
+	if($sewing_last_opn)
 	{
-		$mo_no_vals[]=$row2['mo_no'];
-	}
-	$mo_no_values = "'" . implode ( "', '", $mo_no_vals ) . "'";
-	$moq_query = "SELECT mo_no,sum(if(op_code=$sewing_last_opn,good_quantity,0)) as output,sum(if(op_code=200,good_quantity,0)) as carton from bai_pro3.mo_operation_quantites where mo_no in ($mo_no_values) group by mo_no";
-	echo "<b>".$moq_query."</b><br/>";
-	$moq_result = mysqli_query($link,$moq_query);
-	while($row = mysqli_fetch_array($moq_result))
-	{
-		$mo_no = $row['mo_no'];
-		$output = $row['output'];
-		$carton = $row['carton'];
-		$code = $sewing_last_opn;
-
-		if($carton==0)
+		echo $style."--".$color."---".$sewing_last_opn."<br/>";
+		$get_mo="SELECT mo_no FROM `bai_pro3`.`mo_details` WHERE style = '$style'  AND color = '$color' ";
+		// echo $get_mo;
+		$mo_result = mysqli_query($link,$get_mo);
+		while($row2 = mysqli_fetch_array($mo_result))
 		{
-			$remain=$output;
+			$mo_no_vals[]=$row2['mo_no'];
 		}
-		else
+		$result = array_filter($mo_no_vals); 
+		// var_dump($result,count($mo_no_vals));
+		if(count($mo_no_vals)>0)
 		{
-			$remain = $output-$carton;
-		}
-		
-		if($output > 0)
-		{
-			$insert_qry = "INSERT INTO `bai_pro3`.`tbl_carton_ready` (`operation_id`, `mo_no`, `remaining_qty`, `cumulative_qty`) VALUES ($code, $mo_no, $remain, $output); ";
-			$qty_inserting = mysqli_query($link,$insert_qry) or exit("While inserting mo_operation_quantites".$insert_qry."<br/>".mysqli_error($GLOBALS["___mysqli_ston"]));
-			if($qty_inserting)
+			$mo_no_values = "'" . implode ( "', '", $mo_no_vals ) . "'";
+			$moq_query = "SELECT mo_no,sum(if(op_code=$sewing_last_opn,good_quantity,0)) as output,sum(if(op_code=200,good_quantity,0)) as carton from bai_pro3.mo_operation_quantites where mo_no in ($mo_no_values) and mo_no != '' group by mo_no";
+			echo "<b>".$moq_query."</b><br/>";
+			$moq_result = mysqli_query($link,$moq_query);
+			while($row = mysqli_fetch_array($moq_result))
 			{
-				echo "</br><h4 style='color:blue;'>successfully updated : </h4>".$insert_qry;
+				$mo_no = $row['mo_no'];
+				$output = $row['output'];
+				$carton = $row['carton'];
+				$code = $sewing_last_opn;
+
+				if($carton==0)
+				{
+					$remain=$output;
+				}
+				else
+				{
+					$remain = $output-$carton;
+				}
+				
+				if($output > 0)
+				{
+					if($remain < 0 )
+					{
+						//to insert negative values
+						$mo_val = $mo_no."//".$remain;
+						$mo_num_qry = "INSERT INTO `bai_pro3`.`bai_mod_db` (`mod_id`) VALUES ('$mo_val');";
+						mysqli_query($link,$mo_num_qry);
+						$remain = 0;
+					}
+					$insert_qry = "INSERT INTO `bai_pro3`.`tbl_carton_ready` (`operation_id`, `mo_no`, `remaining_qty`, `cumulative_qty`) VALUES ($code, '$mo_no', $remain, $output); ";
+					// $qty_inserting = mysqli_query($link,$insert_qry) or exit("While inserting mo_operation_quantites".$insert_qry."<br/>".mysqli_error($GLOBALS["___mysqli_ston"]));
+					$qty_inserting = mysqli_query($link,$insert_qry);
+					if($qty_inserting)
+					{
+						$count_val++;
+						echo "<h4 style='color:blue;'>successfully updated : </h4>".$count_val."-".$insert_qry."</br>";
+					}
+					else
+					{
+						$count_value++;
+						echo "<h4 style='color:red;'>Failed to updated : </h4>".$count_value."-".$insert_qry."</br>";
+					}	
+				}
+
 			}
-			else
-			{
-				echo "</br><h4 style='color:red;'>Failed to updated : </h4>".$insert_qry;
-			}	
 		}
-
+		unset($mo_no_vals);
+		unset($mo_no_values);
+		unset($sewing_last_opn);
 	}
-	unset($mo_no_vals);
-	unset($mo_no_values);
-	unset($sewing_last_opn);
 }
 
-echo "Successful";
+echo "Script Completed Successfully";
 ?>
