016fb25c2fbb63e194a8af233662641d39835d1e
Mounika <mounikapentakota30@gmail.com>
2020-04-01 15:53:21 +0530

    commit 3591

--- sfcs_app/common/config/functions_dashboard.php
+++ sfcs_app/common/config/functions_dashboard.php
@@ -2,7 +2,7 @@
 function get_ips_operation_code($link,$style,$color){
 	
     $data=[];
-    $qry_ips_ops_mapping = "SELECT tsm.operation_code AS operation_code ,tor.operation_name AS operation_name FROM brandix_bts.tbl_style_ops_master tsm LEFT JOIN brandix_bts.tbl_orders_ops_ref tor ON tor.id=tsm.operation_name WHERE style='$style' AND color='$color' AND tor.display_operations='yes' AND tor.category='sewing' GROUP BY tsm.operation_code ORDER BY tsm.operation_order*1 LIMIT 1";
+    $qry_ips_ops_mapping = "SELECT tsm.operation_code AS operation_code ,tor.operation_name AS operation_name FROM brandix_bts.tbl_style_ops_master tsm LEFT JOIN brandix_bts.tbl_orders_ops_ref tor ON tor.id=tsm.operation_name WHERE style='$style' AND color='$color' AND tor.display_operations='yes' AND tor.category='sewing' GROUP BY tsm.operation_code ORDER BY tsm.operation_order LIMIT 1";
     $result_qry_ips_ops_mapping = $link->query($qry_ips_ops_mapping);
     while($sql_row1=mysqli_fetch_array($result_qry_ips_ops_mapping))
     {
