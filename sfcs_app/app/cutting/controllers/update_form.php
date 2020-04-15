5268738e1cf5f32112e4232b426f30fd39de1160
Mounika <mounikapentakota30@gmail.com>
2020-04-15 19:31:28 +0530

    commit mrn data issue sorted #3607

--- sfcs_app/app/cutting/controllers/update_form.php
+++ sfcs_app/app/cutting/controllers/update_form.php
@@ -363,20 +363,20 @@ if(sizeof($_GET["lots"]) > 0)
 		{
 			$bgcolor="#dgffdf";
 		}
-		$mrn_iss_qty = 0;
-		$sql111="select sum(iss_qty) as iss_qty1 from $bai_rm_pj2.mrn_out_allocation where lable_id='".$sql_row1["tid"]."'";
-		$sql_result221=mysqli_query($link, $sql111) or exit("Sql Error5".mysqli_error($GLOBALS["___mysqli_ston"]));
-		while($sql_row221=mysqli_fetch_array($sql_result221))
-		{	
-			$mrn_iss_qty=$sql_row221['iss_qty1'];
-		}
-		echo "<td bgcolor=\"$bgcolor\">".($sql_row1["qty_issued"]+$mrn_iss_qty)."</td>";
+		// $mrn_iss_qty = 0;
+		// $sql111="select sum(iss_qty) as iss_qty1 from $bai_rm_pj2.mrn_out_allocation where lable_id='".$sql_row1["tid"]."'";
+		// $sql_result221=mysqli_query($link, $sql111) or exit("Sql Error5".mysqli_error($GLOBALS["___mysqli_ston"]));
+		// while($sql_row221=mysqli_fetch_array($sql_result221))
+		// {	
+		// 	$mrn_iss_qty=$sql_row221['iss_qty1'];
+		// }
+		echo "<td bgcolor=\"$bgcolor\">".($sql_row1["qty_issued"])."</td>";
 		echo "<td bgcolor=\"$bgcolor\">".$sql_row1["qty_allocated"]."</td>";
 		echo "<td>".$sql_row1["qty_ret"]."</td>";
 		
 
 
-		echo "<td><input type=\"hidden\" name=\"lotbal[]\" value=\"".(($sql_row1["qty_rec"]+$sql_row1["qty_ret"])-($sql_row1["qty_issued"]+$sql_row1["qty_allocated"]+$mrn_iss_qty))."\"  />".(($sql_row1["qty_rec"]+$sql_row1["qty_ret"])-($sql_row1["qty_issued"]+$sql_row1["qty_allocated"]+$mrn_iss_qty))."</td>";
+		echo "<td><input type=\"hidden\" name=\"lotbal[]\" value=\"".(($sql_row1["qty_rec"]+$sql_row1["qty_ret"])-($sql_row1["qty_issued"]+$sql_row1["qty_allocated"]))."\"  />".(($sql_row1["qty_rec"]+$sql_row1["qty_ret"])-($sql_row1["qty_issued"]+$sql_row1["qty_allocated"]))."</td>";
 		echo "<td><input type=\"text\"  name=\"issqty[]\" class='float' onfocus=if(this.value==0){this.value=''} onblur=if(this.value==''){this.value=0;} value=\"0\" $readonly onkeyup=\"DataCheck();\"/></td>";
 		echo "</tr>";
 		}
