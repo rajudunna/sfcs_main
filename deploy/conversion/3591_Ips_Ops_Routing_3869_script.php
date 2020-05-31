1bc4ed21007be6bf23c990e677f04383cff4f24b
Mounika <mounikapentakota30@gmail.com>
2020-05-31 13:42:39 +0530

    commit #3869 previous operation for script

--- deploy/conversion/3591_Ips_Ops_Routing_3869_script.php
+++ deploy/conversion/3591_Ips_Ops_Routing_3869_script.php
@@ -8,7 +8,7 @@ $scanning_query=" select operation_code from $brandix_bts.tbl_ims_ops where appi
 $scanning_result=mysqli_query($link, $scanning_query)or exit("scanning_error".mysqli_error($GLOBALS["___mysqli_ston"]));
 while($sql_row=mysqli_fetch_array($scanning_result))
 {
-    $operation_code_routing=$sql_row['operation_code'];
+    $ips_ops_routing=$sql_row['operation_code'];
 }
 $appilication_out = "IMS_OUT";
 $checking_output_ops_code_out = "SELECT operation_code from $brandix_bts.tbl_ims_ops where appilication='$appilication_out'";
@@ -24,11 +24,14 @@ else
 {
     $output_ops_code_out = 130;
 }
-// $style = 'CNB2406H';
-// $color = '900MP1-WHITE';
-// $schedule = '672359';
+//uncomment this for developer testing
+// $style = 'CNB1396G';
+// $color = '026JKZ-HICKORY PLAID BLACK';
+// $schedule = '667111';
 
 // $get_style_color_query="SELECT order_style_no as style,order_col_des as color,order_del_no as schedule FROM $bai_pro3.packing_summary_input where order_style_no='$style' and order_col_des='$color' and order_del_no='$schedule' limit 1";
+
+//uncomment this query during developer testing
 $get_style_color_query="SELECT order_style_no as style,order_col_des as color,order_del_no as schedule FROM $bai_pro3.packing_summary_input GROUP BY order_style_no,order_col_des,order_del_no";
 echo $get_style_color_query.'<br/>';
 $get_style_color_query_result=mysqli_query($link, $get_style_color_query) or exit("Sql Error212".mysqli_error($GLOBALS["___mysqli_ston"]));
@@ -37,13 +40,33 @@ while($row=mysqli_fetch_array($get_style_color_query_result))
     $style=$row['style'];
     $color=$row['color'];
     $schedule=$row['schedule'];
-    if($operation_code_routing == 'Auto') {
+    if($ips_ops_routing == 'Auto') {
         $qry_ips_ops_mapping = "SELECT tsm.operation_code AS operation_code FROM brandix_bts.tbl_style_ops_master tsm LEFT JOIN brandix_bts.tbl_orders_ops_ref tor ON tor.id=tsm.operation_name WHERE style='$style' AND color='$color' AND tor.display_operations='yes' AND tor.category='sewing' GROUP BY tsm.operation_code ORDER BY tsm.operation_order LIMIT 1";
         $result_qry_ips_ops_mapping = $link->query($qry_ips_ops_mapping);
         while($sql_row1=mysqli_fetch_array($result_qry_ips_ops_mapping))
         {
             $operation_code_routing=$sql_row1['operation_code'];
         }
+    } else{
+        $operation_code_routing = $ips_ops_routing;
+    }
+     //*To get previous Operation
+    $ops_sequence_check = "select id,ops_sequence,ops_dependency,operation_order from $brandix_bts.tbl_style_ops_master where style='$style' and color = '$color' and operation_code=$output_ops_code_out";
+    //echo $ops_sequence_check;b_op_id
+    $result_ops_sequence_check = $link->query($ops_sequence_check);
+    while($row2 = $result_ops_sequence_check->fetch_assoc()) 
+    {
+        $ops_seq = $row2['ops_sequence'];
+        $seq_id = $row2['id'];
+        $ops_order = $row2['operation_order'];
+    }
+
+    $pre_operation_check = "select operation_code from $brandix_bts.tbl_style_ops_master where style='$style' and color = '$color' AND ops_sequence = '$ops_seq' AND CAST(operation_order AS CHAR) < '$ops_order' and operation_code NOT IN  (10,200) ORDER BY operation_order DESC LIMIT 1";
+    // echo  $pre_operation_check;
+    $result_pre_operation_check = $link->query($pre_operation_check);
+    while($row23 = $result_pre_operation_check->fetch_assoc()) 
+    {
+        $previous_operation = $row23['operation_code'];
     }
 
     $get_ims="SELECT operation_id FROM $bai_pro3.ims_log where ims_style='$style' and ims_color='$color' and ims_schedule='$schedule' limit 1";
@@ -94,22 +117,34 @@ while($row=mysqli_fetch_array($get_style_color_query_result))
                 $schedule =$bcd_row['schedule'];
                 $remarks =$bcd_row['remarks'];
                 echo $bundle_number.'bundle_number<br/>';
-                $get_bcd_ims="SELECT send_qty,recevied_qty,rejected_qty FROM $brandix_bts.bundle_creation_data where bundle_number=$bundle_number and operation_id=$output_ops_code_out";
-                // echo $get_bcd_ims."<br>";
-                $get_bcd_ims_result=mysqli_query($link, $get_bcd_ims) or exit("Sql Error242".mysqli_error($GLOBALS["___mysqli_ston"]));
-                while($bcd_ims_row=mysqli_fetch_array($get_bcd_ims_result))
-                {
-                    $send_qty_out =$bcd_ims_row['send_qty'];
-                    $recevied_qty_out =$bcd_ims_row['recevied_qty'];
-                    $rejected_qty_out =$bcd_ims_row['rejected_qty'];
-                }
-                $ims_status = '';
-                if($send_qty_out > 0){
-                    if($send_qty_out == ($recevied_qty_out+$rejected_qty_out)){
-                        $ims_status = 'DONE';
-                    }
-                }
 
+                
+                $get_qty_details1="select sum(if(operation_id = $previous_operation,send_qty,0)) as input,sum(if(operation_id = $previous_operation,rejected_qty,0)) as input_rej,sum(if(operation_id = $output_ops_code_out,recevied_qty,0)) as output,sum(if(operation_id = $output_ops_code_out,rejected_qty,0)) as output_rej From $brandix_bts.bundle_creation_data where  bundle_number=$bundle_number";
+                //echo $get_qty_details1;
+                $get_qty_result1=mysqli_query($link,$get_qty_details1) or exit("barcode status Error3".mysqli_error($GLOBALS["___mysqli_ston"]));
+                while($qty_details1=mysqli_fetch_array($get_qty_result1))
+                {
+                    $previous_ops_send_qty = $qty_details1['input'];
+                    $previous_ops_rej_qty = $qty_details1['input_rej'];
+                    $recevied_qty_out =$qty_details1['output'];
+                    $ims_operation_out_qty = $qty_details1['output'] + $qty_details1['output_rej'];
+                }
+                
+                if($previous_ops_rej_qty > 0)
+                {
+                    $input_final_qty = $previous_ops_send_qty - $previous_ops_rej_qty;
+                }
+                else
+                {
+                    $input_final_qty = $previous_ops_send_qty;
+                } 
+
+                $ims_status = '';
+                if($input_final_qty == $ims_operation_out_qty)
+                {
+                    $ims_status = 'DONE';
+                }
+           
                 $bundle_op_id=$bundle_number."-".$operation_id."-".$input_job_no.'-'.$remarks;
 
                 $cat_ref=0;
@@ -144,9 +179,15 @@ while($row=mysqli_fetch_array($get_style_color_query_result))
                 // echo $get_rep_ims_bkp.'<br/>';
                 $get_rep_ims_bkp_result=mysqli_query($link, $get_rep_ims_bkp) or exit("Sql Error22".mysqli_error($GLOBALS["___mysqli_ston"]));
                 if(mysqli_num_rows($get_rep_ims_bkp_result) > 0) {
-                    $update_ims_bkp = "update $bai_pro3.ims_log_backup set ims_qty = $recevied_qty,ims_pro_qty = $recevied_qty_out,operation_id = $operation_id,bai_pro_ref='".$bundle_op_id."' where pac_tid = $bundle_number";
-                    // echo $update_ims_bkp.'<br/>';
-                    mysqli_query($link,$update_ims_bkp) or exit("While updating status in ims_log_backup1".mysqli_error($GLOBALS["___mysqli_ston"]));
+                    if($recevied_qty > 0){
+                        $update_ims_bkp = "update $bai_pro3.ims_log_backup set ims_qty = $recevied_qty,ims_pro_qty = $recevied_qty_out,operation_id = $operation_id,bai_pro_ref='".$bundle_op_id."' where pac_tid = $bundle_number";
+                        // echo $update_ims_bkp.'<br/>';
+                        mysqli_query($link,$update_ims_bkp) or exit("While updating status in ims_log_backup1".mysqli_error($GLOBALS["___mysqli_ston"]));
+                    } else {
+                        $ims_log_delete="delete from $bai_pro3.ims_log_backup where operation_id=$ims_operation and pac_tid=$bundle_number";
+                        // echo $ims_log_delete.'<br/>';
+                        mysqli_query($link,$ims_log_delete) or exit("While Delete".mysqli_error($GLOBALS["___mysqli_ston"]));
+                    }
                     
                 }
             }
