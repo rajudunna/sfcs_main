f7c629a2d96b995def7d5ab673b567382a814e92
unknown <sekhar.vungarala@schemaxtech.com>
2020-07-09 14:47:52 +0530

    #4207-1.5 ui itegration

--- template/helper.php
+++ template/helper.php
@@ -150,7 +150,7 @@ function hasviewpermission($r)
     $user = getrbac_user()['uname'];
     GLOBAL $link_ui;
     $r = base64_decode($r);
-    $r = "/".trim($r, "/");
+    // $r = "/".trim($r, "/");
 
     $query = "SELECT * FROM rbac_role_menu LEFT JOIN tbl_menu_list ON rbac_role_menu.menu_pid=tbl_menu_list.menu_pid LEFT JOIN rbac_users ON rbac_role_menu.roll_id = rbac_users.role_id WHERE rbac_users.user_name='".$user."' AND tbl_menu_list.link_location='".$r."'";
 
