b1bbb2f51e3eb807e5b03de3b10d6331f88609fe
unknown <sekhar.vungarala@schemaxtech.com>
2020-07-20 01:25:53 +0530

    #4207-1.5-ui integration

--- template/sidemenu.php
+++ template/sidemenu.php
@@ -39,8 +39,8 @@
                     }
                     ?>
                     <?php
-                    // role_id 1 is super user and 30 is super admin
-                    if (getrbac_user()['role_id'] == '1' || '30') {
+                    // role_id 1 is super user 
+                    if (getrbac_user()['role_id'] == '1') {
                         $ma = 'User Access';
                         global $link_ui;
                         $query = "SELECT * FROM tbl_menu_list WHERE parent_id=(SELECT menu_pid FROM tbl_menu_list WHERE link_description='" . $ma . "')";
