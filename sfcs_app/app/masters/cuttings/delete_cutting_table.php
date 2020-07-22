78995bddff7714f8b28d65ca1765b7eb5b194571
RAVI SHANKAR <ravishankar@schemaxtech.com>
2020-07-22 11:40:44 +0530

    #4476 CUTTING TEAM LEAD NAME CHANGES

---
+++ sfcs_app/app/masters/cuttings/delete_cutting_table.php
@@ -0,0 +1,20 @@
+<script src="/sfcs_app/common/js/jquery-2.1.3.min.js"></script>
+  <script src="/sfcs_app/common/js/sweetalert-dev.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+<?php
+$tbl_id=$_GET['tid'];
+
+include($_SERVER['DOCUMENT_ROOT'].'/sfcs_app/common/config/config.php');
+$conn=$link;
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+	$url=getFullURL($_GET['r'],'cutting_table_add.php','N');
+	
+$delete="delete from $mdm.`tbl_leader_name` where id='$tbl_id'";
+$sql_result=mysqli_query($link, $delete) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
+    echo "<script>window.location = '".$url."'</script>";
+
+//header('location: index.php?r=L3NmY3NfYXBwL2FwcC9tYXN0ZXJzL2N1dHRpbmcvY3V0dGluZ190YWJsZV9hZGQucGhw');
+?>
