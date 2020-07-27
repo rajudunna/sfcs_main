465cf48d69d6f6f1588368ce75fd6851a281d7b6
Yateesh <yateesh@schemaxtech.com>
2020-07-27 13:59:25 +0530

    #4406_mrn_request_file_commit

--- sfcs_app/app/warehouse/reports/mrn_request_form_V2.php
+++ sfcs_app/app/warehouse/reports/mrn_request_form_V2.php
@@ -405,8 +405,8 @@ function button_disable()
 
 			if(strlen($doc_no) > 0)
 			{
-				//$sql_doc="SELECT group_concat(tran_pin) as tid FROM bai_rm_pj1.fabric_cad_allocation where doc_no=".$doc_no." group by doc_no";
-				$sql_doc="SELECT group_concat(roll_id) as tid FROM $bai_rm_pj1.fabric_cad_allocation where doc_no='".$doc_no."' group by doc_no";
+				//$sql_doc="SELECT group_concat(tran_pin) as tid FROM wms.fabric_cad_allocation where doc_no=".$doc_no." group by doc_no";
+				$sql_doc="SELECT group_concat(roll_id) as tid FROM $wms.fabric_cad_allocation where doc_no='".$doc_no."' AND plant_code='".$plant_code."' group by doc_no";
 				//echo "<option value=\"0\" selected>".$sql_doc."</option>";
 				$sql_result_doc=mysqli_query($link, $sql_doc) or die("Error".$sql_doc.mysqli_error($GLOBALS["___mysqli_ston"]));
 				while($sql_row_doc=mysqli_fetch_array($sql_result_doc))
@@ -416,7 +416,7 @@ function button_disable()
 
 				if(strlen($tid) > 0)
 				{
-					$sql_tid="SELECT distinct(lot_no) FROM $bai_rm_pj1.store_in where tid in (".$tid.")";
+					$sql_tid="SELECT distinct(lot_no) FROM $wms.store_in where tid in (".$tid.") AND plant_code='".$plant_code."'";
 					//echo "<option value=\"0\" selected>".$sql_tid."</option>";
 					$sql_result_tid=mysqli_query($link, $sql_tid) or die("Error".$sql_tid.mysqli_error($GLOBALS["___mysqli_ston"]));
 					while($sql_row_tid=mysqli_fetch_array($sql_result_tid))
@@ -433,7 +433,7 @@ function button_disable()
 
 				if(strlen($lot_ref) > 0)
 				{
-					$sql13="select distinct batch_no as batch from $bai_rm_pj1.sticker_report where lot_no in (".str_replace(";",",",$lot_ref).") group by batch_no";
+					$sql13="select distinct batch_no as batch from $wms.sticker_report where lot_no in (".str_replace(";",",",$lot_ref).") AND plant_code='".$plant_code."' group by batch_no";
 					$sql_result13=mysqli_query($link, $sql13) or die("Error".$sql13.mysqli_error($GLOBALS["___mysqli_ston"]));
 					while($sql_row13=mysqli_fetch_array($sql_result13))
 					{
@@ -459,7 +459,7 @@ function button_disable()
 
 			//To check the supplier approved quantity for issue availability 
 			//CR# 376 // kirang // 2015-06-18 // Supplier Agreed Quantity Formula Has revised as per the discussion.
-			$sql14="select COALESCE(SUM(supplier_replace_approved_qty))+COALESCE(SUM(supplier_claim_approved_qty)) as qty from $bai_rm_pj1.inspection_complaint_db where reject_batch_no=\"".$batch."\" and plant_code='".$plant_code."'"; 
+			$sql14="select COALESCE(SUM(supplier_replace_approved_qty))+COALESCE(SUM(supplier_claim_approved_qty)) as qty from $wms.inspection_complaint_db where reject_batch_no=\"".$batch."\" and plant_code='".$plant_code."'"; 
 
 			//echo "<br/>".$sql14."<br/>";
 
@@ -556,8 +556,8 @@ function button_disable()
 
 			$appr_qty=0;
 			//CR# 376 // kirang // 2015-06-18 // Supplier Agreed Quantity Formula Has revised as per the discussion.
-			//$sql14="select sum(supplier_replace_approved_qty) as qty from bai_rm_pj1.inspection_complaint_db where reject_batch_no in (".$batch_ref_implode.")";
-			$sql14="select COALESCE(SUM(supplier_replace_approved_qty))+COALESCE(SUM(supplier_claim_approved_qty)) as qty from $bai_rm_pj1.inspection_complaint_db where reject_batch_no in ('".$inp_5."') and plant_code='".$plant_code."'";
+			//$sql14="select sum(supplier_replace_approved_qty) as qty from wms.inspection_complaint_db where reject_batch_no in (".$batch_ref_implode.")";
+			$sql14="select COALESCE(SUM(supplier_replace_approved_qty))+COALESCE(SUM(supplier_claim_approved_qty)) as qty from $wms.inspection_complaint_db where reject_batch_no in ('".$inp_5."') and plant_code='".$plant_code."'";
 
 			//echo "<br/>".$sql14."<br>";
 
