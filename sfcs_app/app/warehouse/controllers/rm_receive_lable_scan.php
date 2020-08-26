1937c15a08bb2224cf4a340e9f0b84e7a3411442
Yateesh <yateesh@schemaxtech.com>
2020-08-26 09:30:24 +0530

    plant_code_commit

--- sfcs_app/app/warehouse/controllers/rm_receive_lable_scan.php
+++ sfcs_app/app/warehouse/controllers/rm_receive_lable_scan.php
@@ -2,7 +2,7 @@
     <head>
         <?php 
             include($_SERVER['DOCUMENT_ROOT']."/sfcs_app/common/config/config.php");
-            $plant_code = 'EKG';
+            $plant_code = $_SESSION['plantCode'];
 	        $username = $_SESSION['userName'];
         ?>
     </head>
