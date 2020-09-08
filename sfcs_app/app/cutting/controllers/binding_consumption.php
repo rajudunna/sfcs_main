6ff5ec84a1081f4f5d8b661742794dbadd371530
unknown <you@example.com>
2020-09-08 20:16:35 +0530

    #5012 commit

--- sfcs_app/app/cutting/controllers/binding_consumption.php
+++ sfcs_app/app/cutting/controllers/binding_consumption.php
@@ -11,11 +11,13 @@ if(isset($_GET['row_id']))
     $php_self = explode('/',$_SERVER['HTTP_HOST']);
     array_pop($php_self);
     $url_r = base64_encode(implode('/',$php_self)."/sfcs_app/app/cutting/controllers/seperate_docket.php");
-    $url1 = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://".$_SERVER['HTTP_HOST']."/index.php?r=".$url_r;
+    $url1 = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://".$_SERVER['HTTP_HOST']."/index-no-navi.php?r=".$url_r;
 
 
     $row_id = $_GET['row_id'];
     $status = $_GET['status'];
+    $plant_code = $_GET['plantcodename'];
+$username = $_GET['username'];
     if($status== 'Reject'){
         $status_new = 'Close';
     }
@@ -25,8 +27,8 @@ if(isset($_GET['row_id']))
 	
 	$deletemain="delete from $bai_pro3.binding_consumption where id = '$row_id'";
 	$deletemain_query = mysqli_query($link,$deletemain);
-    // $query = "UPDATE $bai_pro3.binding_consumption set status = '".$status_new."',status_at= '".date("Y-m-d H:i:s")."' where id = $row_id";
-    // $update_query = mysqli_query($link,$query);
+    $query = "UPDATE $pps.binding_consumption set status = '".$status_new."',status_at= '".date("Y-m-d H:i:s")."',updated_user= '".$username."',updated_at=NOW() where id = $row_id and plant_code='$plant_code'";
+    $update_query = mysqli_query($link,$query);
 
     echo"<script>swal('Successfully Deleted.','','success')</script>";
 	echo"<script>location.href =  '".$url1."';</script>"; 
