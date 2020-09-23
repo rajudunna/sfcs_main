265c6ec838a50775339c649d360aa0347626b962
Ramani <ramani@schemaxtech.com>
2020-09-23 14:18:14 +0530

    #5182 commit

--- sfcs_app/app/cutting/controllers/closed_docket.php
+++ sfcs_app/app/cutting/controllers/closed_docket.php
@@ -18,6 +18,12 @@ if(isset($_GET['date']))
     while($sql_row=mysqli_fetch_array($sql_result))
     {
         $i = $sql_row['id'];
+        $docket_query = "select * from $pps.binding_consumption_items where parent_id='$i' and plant_code='".$plant_code."'";
+                                    $docket_query_result = mysqli_query($link_new,$docket_query);
+                                    while($sql_row1=mysqli_fetch_array($docket_query_result))
+                                    {
+                                        $docket_number = $sql_row1['doc_no'];
+                                    }
         $path = '../../../sfcs_app/app/cutting/controllers/lay_plan_preparation/book3_print_binding.php'; 
         $index+=1;
         $response_table.= "<tr><td data-toggle='modal' data-target='#myModal$i'><input type='hidden' id='row_id-$i' value='$i'><span class='label label-info fa fa-list fa-xl' >&nbsp;&nbsp;&nbsp;$index</span></td>";
@@ -26,7 +32,7 @@ if(isset($_GET['date']))
         $response_table.= "<td>".$sql_row['color']."</td>";
         $response_table.= "<td>".$sql_row['tot_bindreq_qty']."</td>";
         // $response_table.= "<td>".$sql_row['tot_bindreq_qty']."</td>";
-        $response_table.= "<td><a href=\"$path?binding_id=$i\" onclick=\"Popup1=window.open('$path?binding_id=$i','Popup1','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes, width=920,height=400, top=23'); if (window.focus) {Popup1.focus()} return false;\" class='btn btn-warning btn-xs'><i class='fa fa-print'></i>&nbsp;Print</a></td>";
+        $response_table.= "<td><a href=\"$path?binding_id=$i&docket_number=$docket_number\" onclick=\"Popup1=window.open('$path?binding_id=$i&plant_code=$plant_code&username=$username&docket_number=$docket_number','Popup1','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes, width=920,height=400, top=23'); if (window.focus) {Popup1.focus()} return false;\" class='btn btn-warning btn-xs'><i class='fa fa-print'></i>&nbsp;Print</a></td>";
         $response_table.= "</tr>";
     }
     if($index == 0){
