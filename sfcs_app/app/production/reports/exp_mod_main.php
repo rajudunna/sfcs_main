6f7d83060458154d936bac5620cacaa20d8b3be0
unknown <sekhar.vungarala@schemaxtech.com>
2020-09-29 11:44:57 +0530

    #5164-weekly style progress

--- sfcs_app/app/production/reports/exp_mod_main.php
+++ sfcs_app/app/production/reports/exp_mod_main.php
@@ -1,38 +1,36 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/config.php',3,'R'));
-include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/user_acl_v1.php',3,'R'));
-$exp_mod_week_data_old = getFullURL($_GET["r"],"exp_mod_week_data_old.php","N");
-$exp_mod_shift_data = getFullURL($_GET["r"],"exp_mod_shift_data.php","N");
-$view_access=user_acl("SFCS_0072",$username,1,$group_id_sfcs); 
+include($_SERVER['DOCUMENT_ROOT'] . '/' . getFullURLLevel($_GET['r'], 'common/config/config.php', 3, 'R'));
+include($_SERVER['DOCUMENT_ROOT'] . '/' . getFullURLLevel($_GET['r'], 'common/config/user_acl_v1.php', 3, 'R'));
+$exp_mod_week_data_old = getFullURL($_GET["r"], "exp_mod_week_data_old.php", "N");
+$exp_mod_shift_data = getFullURL($_GET["r"], "exp_mod_shift_data.php", "N");
+$view_access = user_acl("SFCS_0072", $username, 1, $group_id_sfcs);
 ?>
 
 <script>
-function verify(){
+	function verify() {
 		var from = document.getElementById('dat1').value;
 		var to = document.getElementById('dat2').value;
-		if( from > to){
-			sweetAlert('End date must not be greater than start date','','warning');
+		if (from > to) {
+			sweetAlert('End date must not be greater than start date', '', 'warning');
 			return false;
 		}
 		return true;
-
-<script >
-function verify_date()
-{
-	var val1 = $('#dat1').val();
-	var val2 = $('#dat2').val();
-	// d1 = new Date(val1);
-	// d2 = new Date(val2);
-	if(val1 > val2){
-		sweetAlert('Start Date Should  be less than End Date','','warning');
-		return false;
 	}
-	else
-	{
-	    return true;
 
+
+	function verify_date() {
+		var val1 = $('#dat1').val();
+		var val2 = $('#dat2').val();
+		// d1 = new Date(val1);
+		// d2 = new Date(val2);
+		if (val1 > val2) {
+			sweetAlert('Start Date Should  be less than End Date', '', 'warning');
+			return false;
+		} else {
+			return true;
+
+		}
 	}
-}
 </script>
 
 <!-- <style type="text/css">
@@ -55,80 +53,79 @@ function verify_date()
 <div class="panel panel-primary">
 	<div class="panel-heading">Weekly Style Progress</div>
 	<div class="panel-body">
-		<form method="post" class="form_inline" action=<?php getFullURLLevel($_GET['r'],'exp_mod_main.php',0,'N') ?>>
-		<div class="row">
-			<div class="col-md-3"><label>Start Date </label><input type="text" data-toggle='datepicker' class="form-control" id="dat1" name="dat1" size=8  value="<?php  if(isset($_GET['dat1'])) { echo $_GET['dat1']; } else { echo date("Y-m-d"); } ?>"/></div>
-			<div class="col-md-3"><label>End Date </label><input type="text" data-toggle='datepicker'class="form-control" id="dat2" name="dat2" size=8 value="<?php  if(isset($_GET['dat2'])) { echo $_GET['dat2']; } else { echo date("Y-m-d"); } ?>"/></div>
-			<div class="col-md-1"><label>Section</label>
+		<form method="post" class="form_inline" action=<?php getFullURLLevel($_GET['r'], 'exp_mod_main.php', 0, 'N') ?>>
+			<div class="row">
+				<div class="col-md-3">
 				<?php
+			$sadte = (isset($_POST['dat1'])) ? $_POST['dat1'] : date("Y-m-d"); // Start date
+			$eadte = (isset($_POST['dat2'])) ? $_POST['dat2'] : date("Y-m-d"); // End date
+
+			?>
+					<label>Start Date </label>
+				<input type="text" data-toggle='datepicker' class="form-control" id="dat1" name="dat1" size=8 value="<?= $sadte ?>" /></div>
+				<div class="col-md-3">
+					<label>End Date </label>
+					<input type="text" data-toggle='datepicker' class="form-control" id="dat2" name="dat2" size=8 value="<?= $eadte?>" /></div>
+				<div class="col-md-1"><label>Section</label>
+					<?php
 					echo "<select name=\"sec\" class='form-control'>";
-					$sql="SELECT sec_id as secid FROM $bai_pro3.sections_db WHERE sec_id NOT IN (0,-1) ORDER BY sec_id";
-					$result17=mysqli_query($link, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
-					while($sql_row=mysqli_fetch_array($result17))
-					{
-						$sql_sec=$sql_row["secid"];
-						if($_GET['sec'] == $sql_sec){
-							echo "<option value=\"".$sql_sec."\" selected>".$sql_sec."</option>";
+					$sql = "SELECT sec_id as secid FROM $bai_pro3.sections_db WHERE sec_id NOT IN (0,-1) ORDER BY sec_id";
+					$result17 = mysqli_query($link, $sql) or exit("Sql Error" . mysqli_error($GLOBALS["___mysqli_ston"]));
+					while ($sql_row = mysqli_fetch_array($result17)) {
+						$sql_sec = $sql_row["secid"];
+						if ($_GET['sec'] == $sql_sec) {
+							echo "<option value=\"" . $sql_sec . "\" selected>" . $sql_sec . "</option>";
 						} else {
-							echo "<option value=\"".$sql_sec."\" >".$sql_sec."</option>";
+							echo "<option value=\"" . $sql_sec . "\" >" . $sql_sec . "</option>";
 						}
-							
 					}
 					echo "</select>";
-				?>
-			</div>
-			<div class="col-md-2"><label>Category</label><select name="cat" class="form-control">
-				<?php
-					$categories = ['Weekly', 'Shift'];
-					foreach ($categories as $key => $value) {
-						if($_GET['cat'] == $value){
-							echo "<option value=\"".$value."\" selected>".$value."</option>";
-						}else {
-							echo "<option value=\"".$value."\" >".$value."</option>";
+					?>
+				</div>
+				<div class="col-md-2"><label>Category</label><select name="cat" class="form-control">
+						<?php
+						$categories = ['Weekly', 'Shift'];
+						foreach ($categories as $key => $value) {
+							if ($_GET['cat'] == $value) {
+								echo "<option value=\"" . $value . "\" selected>" . $value . "</option>";
+							} else {
+								echo "<option value=\"" . $value . "\" >" . $value . "</option>";
+							}
 						}
-					}
-				?>
-				</select>
+						?>
+					</select>
+				</div>
+				<div class="col-md-1"><br /><input type="submit" onclick='return verify_date()' class="btn btn-primary" NAME="submit" value="Show" /></div>
 			</div>
-			<div class="col-md-1"><br/><input type="submit" onclick='return verify_date()' class="btn btn-primary" NAME="submit" value="Show" /></div>
-		</div>
-		<br/><hr/>
+			<br />
+			<hr />
 		</form>
 
-<!-- <span id="msg" style="display:none;"><h4>Please Wait.. While Processing The Data..<h4></span> -->
-<?php
+		<!-- <span id="msg" style="display:none;"><h4>Please Wait.. While Processing The Data..<h4></span> -->
+		<?php
 
-if(isset($_POST["submit"]))
-{
-	$start=$_POST['dat1'];
-	$end=$_POST['dat2'];
-	$sec=$_POST['sec'];
-	$cat=$_POST['cat'];
-	// echo $_POST['cat'];
-	// echo $start;
-	if($cat == "Weekly")
-	{
-		// header("Location:exp_mod_week_data_old.php?dat1=$start&dat2=$end&sec=$sec&cat=$cat");
-		echo '<script>
-				window.location.href="'.$exp_mod_week_data_old.'&dat1='.$start.'&dat2='.$end.'&sec='.$sec.'&cat='.$cat.'";
+		if (isset($_POST["submit"])) {
+			$start = $_POST['dat1'];
+			$end = $_POST['dat2'];
+			$sec = $_POST['sec'];
+			$cat = $_POST['cat'];
+			// echo $_POST['cat'];
+			// echo $start;
+			if ($cat == "Weekly") {
+				// header("Location:exp_mod_week_data_old.php?dat1=$start&dat2=$end&sec=$sec&cat=$cat");
+				echo '<script>
+				window.location.href="' . $exp_mod_week_data_old . '&dat1=' . $start . '&dat2=' . $end . '&sec=' . $sec . '&cat=' . $cat . '";
 		</script>';
-		
-
-	}
-	elseif($cat == "Shift")
-	{
-		// header("Location:exp_mod_shift_data.php?dat1=$start&dat2=$end&sec=$sec&cat=$cat");	
-		echo '<script>
-			window.location.href="'.$exp_mod_shift_data.'&dat1='.$start.'&dat2='.$end.'&sec='.$sec.'&cat='.$cat.'";
+			} elseif ($cat == "Shift") {
+				// header("Location:exp_mod_shift_data.php?dat1=$start&dat2=$end&sec=$sec&cat=$cat");	
+				echo '<script>
+			window.location.href="' . $exp_mod_shift_data . '&dat1=' . $start . '&dat2=' . $end . '&sec=' . $sec . '&cat=' . $cat . '";
 		</script>';
-		// header("Location:'.$exp_mod_shift_data.'?dat1='.$start.'&dat2='.$end.'&sec='.$sec.'&cat='.$cat.'");
-	}
-	else
-	{
-		header("Location:http://bainet");
-	}
-}
+				// header("Location:'.$exp_mod_shift_data.'?dat1='.$start.'&dat2='.$end.'&sec='.$sec.'&cat='.$cat.'");
+			} else {
+				header("Location:http://bainet");
+			}
+		}
 
-?>
-</div>
-
+		?>
+	</div>
