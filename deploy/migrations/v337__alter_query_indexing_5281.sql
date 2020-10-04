ccc5769e1302ddcb0fffb009c50e916f802f99e2
Subrahmanyam <subrahmanyam.a93@gmail.com>
2020-10-04 18:32:39 +0530

    #5281 Alter Query Indexing

---
+++ deploy/migrations/v337__alter_query_indexing_5281.sql
@@ -0,0 +1,3 @@
+/*#5283  Alter Query Indexing*/
+
+ALTER TABLE m3_inputs.order_details ADD COLUMN STATUS INT(11) DEFAULT 0 NULL COMMENT 'cat_stat_log sync status 0 - no, 1 - yes' AFTER time_stamp, ADD KEY STATUS (STATUS);
