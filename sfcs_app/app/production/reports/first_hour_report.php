57a4d38d2dc946ec2646ac513ba855d3bde80906
Yateesh <yateesh@schemaxtech.com>
2020-10-13 19:54:58 +0530

    #5273_firsthour_commit

--- sfcs_app/app/production/reports/first_hour_report.php
+++ sfcs_app/app/production/reports/first_hour_report.php
@@ -1,5 +1,6 @@
 <?php 
-    include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/config.php',3,'R'));
+	include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/config.php',3,'R'));
+	include($_SERVER['DOCUMENT_ROOT'].'/'.getFullURLLevel($_GET['r'],'common/config/enums.php',3,'R'));
 $plant_code=$_SESSION['plantCode'];
 ?>
 	<title>First Hour Output</title>
@@ -98,40 +99,56 @@ if(isset($_POST['submit']))
 	
 	
     //get last hour
-    $get_end_hour="SELECT HOUR(plant_end_time) as last_time FROM $pms.plant WHERE plant_code='$plant_code' AND is_active=1";
+	$get_end_hour="SELECT HOUR(plant_end_time) as last_time, HOUR(plant_start_time) as start_time FROM $pms.plant WHERE plant_code='$plant_code' AND is_active=1";
     $sql_result123=mysqli_query($link_new, $get_end_hour) or exit("Sql Error2.1111".mysqli_error($GLOBALS["___mysqli_ston"]));
 	while($rows123=mysqli_fetch_array($sql_result123))
     {
-        $last_time = $rows123['plant_end_time'];
+		$last_time = $rows123['last_time'];
+		$start_time= $rows123['start_time'];
     }
-    
-    $time_value_pm = $last_time - 8;
-	$start_time_pm = $time_value_pm;
-    $end_time_pm = $time_value_pm + 1.29;
-	$sql22121="SELECT HOUR(plant_start_time) as start_time FROM $pms.plant WHERE plant_code='$plant_code' AND is_active=1"; 
-	$sql_result22121=mysqli_query($link_new, $sql22121) or exit("Sql Error2".mysqli_error($GLOBALS["___mysqli_ston"]));
-	while($rows1231=mysqli_fetch_array($sql_result22121))
+	$first_hour_am=$start_time + 1;
+	//To get shifts count
+	$shifts_count="SELECT COUNT(shift_code) AS shift FROM $pms.`shifts` WHERE plant_code='$plant_code' AND is_active=1";
+	$shift_result=mysqli_query($link_new, $shifts_count) or exit("Sql Error2".mysqli_error($GLOBALS["___mysqli_ston"]));
+	while($shift_row=mysqli_fetch_array($shift_result))
 	{
-		$start_time_am=$rows1231['plant_start_time'];
+       $shift_count=$shift_row['shift'];
+	}	
+	
+	$shift_query="";
+	if($shift_count > 1)
+	{
+		$get_shifttime=($last_time - $start_time)/$shift_count;
+		$start_time_pm=$start_time+$get_shifttime;
+		$endhour_pm=$start_time_pm + 1;
+		$shift_query="OR (HOUR(created_at) BETWEEN ('".$start_time_pm."') AND ('".$endhour_pm."'))"; 
 	}
-	$end_time_am= $start_time_am + 1.29;
-
-	$sql=$get_details="SELECT GROUP_CONCAT(DISTINCT style) AS style,schedule,color,sum(good_quantity) as qty,resource_id,shift,date(created_at) as bac_date FROM $pts.transaction_log WHERE plant_code='$plant_code' AND ((TIME(created_at) BETWEEN ('".$start_time_am."') AND ('".$end_time_am."')) OR (TIME(created_at) BETWEEN ('".$start_time_pm."') AND ('".$end_time_pm."'))) AND is_active=1 GROUP BY style,schedule,color,resource_id,shift";
+	
+	$type=TaskTypeEnum::PLANNEDSEWINGJOB;
+	$sql=$get_details="SELECT GROUP_CONCAT(DISTINCT style) AS style,schedule,color,sum(good_quantity) as qty,resource_id,shift,date(created_at) as bac_date FROM $pts.transaction_log WHERE plant_code='$plant_code' AND ((HOUR(created_at) BETWEEN ('".$start_time."') AND ('".$first_hour_am."')) $shift_query) AND (DATE(created_at) BETWEEN ('".$fdate."') AND ('".$tdate."')) AND parent_job_type='$type'  AND is_active=1 GROUP BY style,schedule,color,resource_id,shift";
 	$sql_result=mysqli_query($link_new, $sql) or exit("Sql Error".mysqli_error($GLOBALS["___mysqli_ston"]));
 	while($sql_row=mysqli_fetch_array($sql_result))
 	{
 		//To get workstation description
-		$query = "select workstation_description from $pms.workstation where plant_code='$plant_code' and workstation_id = '".$sql_row['resource_id']."' AND is_active=1";
+		$query = "select workstation_description,workstation_code,section_id from $pms.workstation where plant_code='$plant_code' and workstation_id = '".$sql_row['resource_id']."' AND is_active=1";
 		$query_result=mysqli_query($link_new, $query) or exit("Sql Error at workstation_description".mysqli_error($GLOBALS["___mysqli_ston"]));
 		while($des_row=mysqli_fetch_array($query_result))
 		{
 		  $workstation_description = $des_row['workstation_description'];
+		  $workstation_code = $des_row['workstation_code'];
+		  $section_id = $des_row['section_id'];
 		}
 		
 		$color=$sql_row['color'];
 		$qty=$sql_row['qty'];
 		
-	
+		//To get section
+		$get_sections="SELECT section_name FROM $pms.sections WHERE section_id='$section_id' AND plant_code='$plant_code' AND is_active=1";
+	    $sections_result=mysqli_query($link_new, $get_sections) or exit("Sql Error at get_sections".mysqli_error($GLOBALS["___mysqli_ston"]));
+		while($sec_row=mysqli_fetch_array($sections_result))
+		{
+           $section_name=$sec_row['section_name'];
+		}
 		//TO caliculate act hours
 		$get_acthours="SELECT * FROM $pms.plant WHERE plant_code='$plant_code' AND is_active=1"; 
 		$sql_get_acthours=mysqli_query($link_new, $get_acthours) or exit("Sql sql_get_acthours".mysqli_error($GLOBALS["___mysqli_ston"]));
@@ -140,11 +157,11 @@ if(isset($_POST['submit']))
 		   $start_time=$actrows['plant_start_time'];
 		   $end_time=$actrows['plant_end_time'];
 		   $diff_time=$end_time-$start_time;
-		   $act_hrs=$diff_time-0.5;
+		   $act_hrs=$diff_time;
 		}
 
 		//To get nop,plan_eff,planned_qty
-		$get_planned_details="SELECT sum(planned_qty) as plan_qty,planned_eff,capacity_factor,(capacity_factor*1) AS clh FROM $pps.`monthly_production_plan` LEFT JOIN $pps.`monthly_production_plan_upload_log` ON monthly_production_plan_upload_log.`monthly_pp_up_log_id`=monthly_production_plan.`pp_log_id` WHERE row_name='$workstation_description' AND planned_date='".$sql_row['bac_date']."' AND plant_code='$plant_code' AND is_active=1";
+		$get_planned_details="SELECT sum(planned_qty) as plan_qty,planned_eff,capacity_factor,(capacity_factor*1) AS clh FROM $pps.`monthly_production_plan` WHERE row_name='$workstation_code' AND planned_date='".$sql_row['bac_date']."' AND plant_code='$plant_code'";
 		$get_planned_details_result=mysqli_query($link_new, $get_planned_details) or exit("Sql Error at get_planned_details_result".mysqli_error($GLOBALS["___mysqli_ston"]));
 		while($planned_row=mysqli_fetch_array($get_planned_details_result))
 		{
@@ -153,12 +170,11 @@ if(isset($_POST['submit']))
 		   $plan_qty=$planned_row['plan_qty'];
 		   $clh=$planned_row['clh'];
 		}
-		
+
 		//caliculation for plan_out
 		$plan_out=round($plan_qty/$act_hrs,0);
 		//To get SMV
-		$get_smv="SELECT smv FROM $pps.`monthly_production_plan` LEFT JOIN $pps.`monthly_production_plan_upload_log` ON monthly_production_plan_upload_log.`monthly_pp_up_log_id`=monthly_production_plan.`pp_log_id` WHERE style='".$sql_row['style']."' AND color='$color' AND plant_code='$plant_code'
-		AND is_active=1";
+		$get_smv="SELECT smv FROM $pps.`monthly_production_plan` WHERE product_code='".$sql_row['style']."' AND colour='$color' AND plant_code='$plant_code'";
 		$query_result1=mysqli_query($link_new, $get_smv) or exit("Sql Error at workstation_description".mysqli_error($GLOBALS["___mysqli_ston"]));
 		while($smv_row=mysqli_fetch_array($query_result1))
 		{
@@ -184,7 +200,7 @@ if(isset($_POST['submit']))
 		}
 		echo "<tr>";
 		echo "<td>".$sql_row['bac_date']."</td>";
-		echo "<td></td>";
+		echo "<td>".$section_name."</td>";
 		echo "<td>".$sql_row['shift']."</td>";	
 		echo "<td>".$workstation_description."</td>";
 		echo "<td>".$sql_row['style']."</td>";
