75255cd918ce4557f36c138691c4aeac3c43ed9a
Yateesh <yateesh@schemaxtech.com>
2020-10-24 13:38:33 +0530

    #5273_carton_out_report_commit

--- sfcs_app/app/production/reports/sawing_out_report.php
+++ sfcs_app/app/production/reports/sawing_out_report.php
@@ -130,19 +130,16 @@ if (isset($_POST['submit'])) {
 
     // if($sch=="")
     // {
-    $psj = TaskTypeEnum::PLANNEDSEWINGJOB;
+    $PCRT = BarcodeType::PCRT;
     $psej = TaskTypeEnum::PLANNEDSEWEMBELLISHMENTJOB;
-    $sql = "SELECT barcode,parent_job,shift,created_at as date FROM $pts.`transaction_log` WHERE plant_code='$plant_code' AND DATE(created_at) BETWEEN '$dat1' AND '$dat2' AND parent_job_type IN ('$psj','$psej')";
-    $sql = $sch ? $sql . " AND schedule='$sch'" : $sql;
-    // }
-    // else if($sch !="")
-    // {
-    // $sql="SELECT tl.barcode_id as barcode_id,tl.parent_ext_ref_id as parent_ext_ref_id,tl.created_at as created_at FROM $pts.`transaction_log` tl
-    // LEFT JOIN $pts.`fg_barcode` fb ON fb.`barcode_id`=tl.`barcode_id`
-    // LEFT JOIN $pts.`finished_good` fg ON fg.`finished_good_id`=fb.`finished_good_id`
-    // WHERE DATE(tl.`created_at`) BETWEEN '$dat1' AND '$dat2' AND fg.`schedule`='$sch'";
-    // }
-    // echo $sql;
+    if($sch == '')
+    {
+        $sql = "SELECT barcode,parent_job,shift,style,schedule,color,size,sum(good_quantity) as quantity,created_at as date FROM $pts.`transaction_log` WHERE plant_code='Q01' AND DATE(created_at) BETWEEN '$dat1' AND '$dat2' AND parent_job_type='$PCRT' GROUP BY style,schedule,color,size";
+    } else
+    {
+        $sql = "SELECT barcode,parent_job,shift,style,schedule,color,size,sum(good_quantity) as quantity,created_at as date FROM $pts.`transaction_log` WHERE plant_code='Q01' AND DATE(created_at) BETWEEN '$dat1' AND '$dat2' AND parent_job_type='$PCRT' AND schedule='$sch' GROUP BY style,schedule,color,size";
+    }
+
     $sql_result = mysqli_query($link, $sql) or exit("Error While Getting transaction log" . $sql . mysqli_error($GLOBALS["___mysqli_ston"]));
 
     if (mysqli_num_rows($sql_result) > 0) {
@@ -160,72 +157,20 @@ if (isset($_POST['submit'])) {
 						<th>Qty</th>
 					</tr>
 					<?php
-while ($sql_row = mysqli_fetch_array($sql_result)) {
-            $barcode = $sql_row['barcode'];
-            $parent_job = $sql_row['parent_job'];
-            $shift = $sql_row['shift'];
-            $date = $sql_row['date'];
-            //getting barcode id
-            $sql_barcode_qry = "Select barcode_id from $pts.barcode where barcode='$barcode' AND plant_code='$plant_code'";
-            $sql_result_det = mysqli_query($link, $sql_barcode_qry) or exit("Sql Error getting barcode id" . $sql_barcode_qry . mysqli_error($GLOBALS["___mysqli_ston"]));
-            while ($sql_row_id = mysqli_fetch_array($sql_result_det)) {
-                $barcode_id = $sql_row_id['barcode_id'];
-            }
-            //getting parent_ext_ref_id
-            $sql_ext_ref_qry = "select jm_jg_header_id from $pps.jm_jg_header where job_number='$parent_job' AND plant_code='$plant_code'";
-            $sql_result_data = mysqli_query($link, $sql_ext_ref_qry) or exit("Sql Error getting jm_jg_header_id" . mysqli_error($GLOBALS["___mysqli_ston"]));
-            while ($sql_row_ref = mysqli_fetch_array($sql_result_data)) {
-                $parent_ext_ref_id = $sql_row_ref['jm_jg_header_id'];
-            }
-
-            //getting finished good id
-            $get_finshgood_qry = "SELECT finished_good_id FROM $pts.`fg_barcode` WHERE barcode_id='$barcode_id' AND plant_code='$plant_code'";
-            $get_finshgood_qry_result = mysqli_query($link, $get_finshgood_qry) or exit("Sql Error finished_good_id" . mysqli_error($GLOBALS["___mysqli_ston"]));
-            while ($get_finshgood_qry_row = mysqli_fetch_array($get_finshgood_qry_result)) {
-                $finished_good_id = $get_finshgood_qry_row['finished_good_id'];
-                //getting style,schedule,color,size
-                $get_det_qry = "SELECT style,schedule,color,size FROM $pts.`finished_good` WHERE finished_good_id='$finished_good_id' AND plant_code='$plant_code'";
-                $get_det_qry_result = mysqli_query($link, $get_det_qry) or exit("Sql Error getting details" . mysqli_error($GLOBALS["___mysqli_ston"]));
-                while ($get_det_qry_row = mysqli_fetch_array($get_det_qry_result)) {
-                    $style = $get_det_qry_row['style'];
-                    $schedule = $get_det_qry_row['schedule'];
-                    $color = $get_det_qry_row['color'];
-                    $size = $get_det_qry_row['size'];
-                    //getting task job id
-                    $get_taskjobid_qry = "SELECT task_jobs_id FROM $tms.`task_jobs` WHERE task_job_reference='$parent_ext_ref_id' AND plant_code='$plant_code'";
-                    $get_taskjobid_qry_result = mysqli_query($link, $get_taskjobid_qry) or exit("Sql Error" . mysqli_error($GLOBALS["___mysqli_ston"]));
-                    while ($get_taskjobid_qry_result_row = mysqli_fetch_array($get_taskjobid_qry_result)) {
-                        $task_jobs_id = $get_taskjobid_qry_result_row['task_jobs_id'];
-                    }
-                    //getting max operation
-                    $qrytoGetMaxOperation = "SELECT operation_code FROM $tms.`task_job_transaction` WHERE task_jobs_id='" . $task_jobs_id . "' AND plant_code='$plant_code' AND is_active=1 ORDER BY operation_seq DESC LIMIT 0,1";
-                    $maxOperationResult = mysqli_query($link_new, $qrytoGetMaxOperation) or exit('Problem in getting operations data for job');
-                    if (mysqli_num_rows($maxOperationResult) > 0) {
-                        while ($minOperationResultRow = mysqli_fetch_array($maxOperationResult)) {
-                            $maxOperation = $minOperationResultRow['operation_code'];
-                        }
-                    }
-
-                    //getting quantity
-                    $get_quant_qry = "select sum(good_quantity) as quantity from $tms.`task_job_transaction` WHERE task_jobs_id='" . $task_jobs_id . "' AND plant_code='$plant_code' AND is_active=1 and operation_code=$maxOperation";
-                    $get_quant_qry_result = mysqli_query($link_new, $get_quant_qry) or exit("attributes data not found for job " . mysqli_error($GLOBALS["___mysqli_ston"]));
-                    while ($get_quant_qry_row = mysqli_fetch_array($get_quant_qry_result)) {
-                        $quantity = $get_quant_qry_row['quantity'];
-                    }
-
+                    
+            while ($sql_row = mysqli_fetch_array($sql_result)) {
+                if($sql_row['quantity'] > 0)
+                {
                     echo "<tr>";
-                    echo "<td>" . $barcode . "</td>";
-                    echo "<td>" . $date . "</td>";
-                    echo "<td>" . $style . "</td>";
-                    echo "<td>" . $schedule . "</td>";
-                    echo "<td>" . $color . "</td>";
-                    echo "<td>" . $size . "</td>";
-                    echo "<td>" . $quantity . "</td>";
+                    echo "<td>" . $sql_row['barcode'] . "</td>";
+                    echo "<td>" . $sql_row['date'] . "</td>";
+                    echo "<td>" . $sql_row['style'] . "</td>";
+                    echo "<td>" . $sql_row['schedule'] . "</td>";
+                    echo "<td>" . $sql_row['color'] . "</td>";
+                    echo "<td>" . $sql_row['size'] . "</td>";
+                    echo "<td>" . $sql_row['quantity'] . "</td>";
                     echo "</tr>";
-
                 }
-            }
-
         }
         echo "</table>
 			</div>";
