95b893ef0cbf36ff7470058a42fd18d703cd78b3
Yateesh <yateesh@schemaxtech.com>
2020-11-05 15:24:32 +0530

    commit

--- sfcs_app/app/production/controllers/sewing_job/input_job_mix_ch_report.php
+++ sfcs_app/app/production/controllers/sewing_job/input_job_mix_ch_report.php
@@ -29,14 +29,17 @@
 
         $url1 = getFullURLLevel($_GET['r'],'print_input_sheet.php',0,'R');
         $url2 = getFullURLLevel($_GET['r'],'print_input_sheet_mm.php',0,'R');
+        //To get Sub PO Description
+        $result_po_des=getPoDetaials($sub_po,$plant_code);
+        $subpo_des=$result_po_des['po_description'];
 
         echo '<br>
             <div class="panel panel-primary">
-                <div class="panel-heading">Sewing Jobs for Schedule - '.$schedule.' & Sub Po - '.$sub_po.'</div>
+                <div class="panel-heading">Sewing Jobs for Schedule - '.$schedule.' & Sub Po - '.$subpo_des.'</div>
                 <div class="panel-body">
                     <div class="col-md-12 ">';       
                         echo "<a class='btn btn-warning' href='$url1?&style=$style&schedule=$schedule&color=$color&mpo=$mpo&sub_po=$sub_po&flag=1' onclick=\"return popitup2('$url1?&style=$style&schedule=$schedule&color=$color&mpo=$mpo&sub_po=$sub_po&flag=1')\" target='_blank'><i class=\"fa fa-print\" aria-hidden=\"true\"></i>&nbsp;&nbsp;&nbsp;Print Ratio Sheet - Job Wise</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-                        echo "<a class='btn btn-warning' href='$url2?schedule=$schedule&seq_no=$seq_no' onclick=\"return popitup2('$url2?schedule=$schedule&seq_no=$seq_no')\" target='_blank'><i class=\"fa fa-print\" aria-hidden=\"true\"></i>&nbsp;&nbsp;&nbsp;Print Ratio Sheet - Split Wise</a>";
+                        echo "<a class='btn btn-warning' href='$url2?style=$style&schedule=$schedule&color=$color&mpo=$mpo&sub_po=$sub_po' onclick=\"return popitup2('$url2?style=$style&schedule=$schedule&color=$color&mpo=$mpo&sub_po=$sub_po')\" target='_blank'><i class=\"fa fa-print\" aria-hidden=\"true\"></i>&nbsp;&nbsp;&nbsp;Print Ratio Sheet - Split Wise</a>";
                     echo '</div>
                 </div>
             </div>';
