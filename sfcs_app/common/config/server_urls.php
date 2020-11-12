301a3df33c76a55e625d6d53a13bc0130bce8cfc
Kumar <ramkumar@schemaxtech.com>
2020-11-12 16:20:57 +0530

    #5457 git committ for service URL

--- sfcs_app/common/config/server_urls.php
+++ sfcs_app/common/config/server_urls.php
@@ -1,11 +1,11 @@
 <?php
 
-$TMS_SERVER_IP="http://192.168.0.34:3340";
-$PTS_SERVER_IP="http://192.168.0.34:3341";
-$SMS_SERVER_IP="http://192.168.0.34:3339";
-$PPS_SERVER_IP="http://192.168.0.34:3336";
-$PDF_SERVER_IP="http://192.168.0.34:8800";
-$DOCKET_SERVER_IP="http://192.168.0.34:4203";
+$TMS_SERVER_IP="http://192.168.0.155:3340";
+$PTS_SERVER_IP="http://192.168.0.155:3341";
+$SMS_SERVER_IP="http://192.168.0.155:3339";
+$PPS_SERVER_IP="http://192.168.0.155:3336";
+$PDF_SERVER_IP="http://192.168.0.155:8800";
+$DOCKET_SERVER_IP="http://192.168.0.155:4203";
 $KEY_LOCK_IP="http://192.168.0.155:8001/auth/realms/sfcs/protocol/openid-connect/token";
 
 $schedulerIp="http://192.168.0.110:8089";
