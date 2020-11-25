bfcdd21df7a931988471e4aa85a4af6c5ac666ef
Ramani <ramani@schemaxtech.com>
2020-11-25 19:16:37 +0530

    #5552 commit

--- sfcs_app/app/warehouse/controllers/scanV2.php
+++ sfcs_app/app/warehouse/controllers/scanV2.php
@@ -15,7 +15,7 @@ if(isset($_POST['submit']))
 {
 	$plant_code=$_POST['plantcode'];
 	$user_name=$_POST['username'];
-	$url="/sfcs_app/app/warehouse/controllers/rm_receive_lable_scan.php?plantcode=".$plantcode."&username=".$username;
+	$url="/sfcs_app/app/warehouse/controllers/rm_receive_lable_scan.php?plantcode=".$plant_code."&username=".$username;
 	echo "<script>
 	window.open('$url', '_blank');
 	</script>";
