d3424bfffdf01ffd1894af94667446499a646c5d
VenkatalakshmiP <venkatalakshmi.pedireddy@schemaxtech.com>
2020-12-30 23:10:35 +0530

    #5707 changes

--- deploy/migrations/v119__size_column_script.sql
+++ deploy/migrations/v119__size_column_script.sql
@@ -1,4 +1,4 @@
-Alter table `oms`.`oms_products_info` change `size_name` `size_name` varchar(50);
+Alter table `oms`.`oms_products_info` change `size_name` `size_name` varchar(30);
 
 Alter table `wms`.`trims_status` change `size` `size` varchar(30);
 
