e82066526338987144a6c6d88d8c0fb23f9abf33
VenkatalakshmiP <venkatalakshmi.pedireddy@schemaxtech.com>
2020-12-30 11:47:29 +0530

    #5707 migration file

---
+++ deploy/migrations/v119_size_column_change_in_pps_pts_tms_wms_oms.sql
@@ -0,0 +1,65 @@
+Alter table `oms`.`oms_products_info`   
+  change `product_sku` `product_sku` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+
+Alter table `pps`.`finished_good`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+
+Alter table `pps`.`jm_docket_cg_bundle`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+
+
+Alter table `pps`.`jm_docket_component_panel`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+
+Alter table `pps`.`jm_product_bundle`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+
+Alter table `pps`.`jm_product_logical_bundle`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `pps`.`jm_aplb`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `pps`.`mp_mo_qty`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `pps`.`jm_pack_container_line`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `pps`.`pm_container_line`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `pps`.`lp_ratio_size`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `pps`.`lp_ratio_group_bundle`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `pps`.`add_cut_request_mo`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+
+Alter table `pts`.`finished_good`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `pts`.`transaction_log`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `pts`.`rejection_header`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+
+Alter table `tms`.`bundle_transfer_log`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `wms`.`trims_status`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
+Alter table `oms`.`oms_products_info`   
+  change `size` `size` varchar(30) CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL
+
